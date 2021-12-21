102af6fee1783e0c932d9b8d0c995e21d2d9341c
Aldrin Mathew <admin@aldrinmathew.com>
2021-12-22 00:38:27 +0530

    Added `run` argument
    - Added `run` argument to pass a filepath that contains a single file or
      a group of files to compile

--- src/qat.cpp
+++ src/qat.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 
-int main(int count, char **args) {
-    if(count == 1) {
-        std::cout << "No command or arguments provided" << std::endl;
-    } else {
+#include "utilities/types.cpp"
 
+int main(int count, my::StringConstantList args)
+{
+    if (count == 1)
+    {
+        std::cout << "No command or arguments provided!\n"
+                  << "Run \"qat help\" for documentation on the available commands" << std::endl;
+        return 0;
+    }
+    else
+    {
+        if (args[1] == "run")
+        {
+            if (count != 3)
+            {
+                std::cout << "Filepath not provided!\n"
+                          << "Do \"qat run path/to/my/file.qat\" to compile your application\n"
+                          << "Run \"qat help\" for documentation on the available commands" << std::endl;
+                return 0;
+            }
+            else
+            {
+                /// TODO: 
+            }
+        }
     }
 }

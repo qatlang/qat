9d404f7e002de9698927905434891712046abe1e
Aldrin Mathew <admin@aldrinmathew.com>
2022-01-11 21:45:23 +0530

    Updated namespaces for helper types
    - Helper types are now under qat::utilities namespace
    
    - Updated Include Guard names

--- src/utilities/types.hpp
+++ src/utilities/types.hpp
@@ -1,21 +1,24 @@
-#ifndef MY_TYPES_HPP
-#define MY_TYPES_HPP
+#ifndef QAT_UTILITIES_TYPES_HPP
+#define QAT_UTILITIES_TYPES_HPP
 
 // Type aliases and definitions for simplicity and readability
 
-namespace my
+namespace qat
 {
-    /// A reference to a character array can be treated as a string
-    using StringConstant = const char *;
+    namespace utilities
+    {
+        /// A reference to a character array can be treated as a string
+        using StringConstant = const char *;
 
-    /// A reference to an array of character arrays can be treated as a list of string
-    using StringConstantList = const char **;
+        /// A reference to an array of character arrays can be treated as a list of string
+        using StringConstantList = const char **;
 
-    /// A reference to a character array can be treated as a string
-    using String = char *;
+        /// A reference to a character array can be treated as a string
+        using String = char *;
 
-    /// A reference to an array of character arrays can be treated as a list of string
-    using StringList = char **;
+        /// A reference to an array of character arrays can be treated as a list of string
+        using StringList = char **;
+    }
 }
 
 #endif

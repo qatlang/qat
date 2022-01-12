e6b25beca736f991f74b1e08f2810569304f99c4
Aldrin Mathew <admin@aldrinmathew.com>
2022-01-12 22:23:01 +0530

    Added line & character number in Tokens
    - Tokens analysed by the lexer will now have the line number and
      character number associated with them.
    
    - This is for reporting and debugging purposes.
    
    - Changed implementation of Token constructors and functions to
      facilitate the new members.

--- src/lexer/token.hpp
+++ src/lexer/token.hpp
@@ -1,8 +1,51 @@
+/**
+ * Qat Programming Language : Copyright 2022 : Aldrin Mathew
+ * 
+ * AAF INSPECTABLE LICENSE - 1.0
+ * 
+ * This project is licensed under the AAF Inspectable License 1.0. 
+ * You are allowed to inspect the source of this project(s) free of 
+ * cost, and also to verify the authenticity of the product.
+ * 
+ * Unless required by applicable law, this project is provided 
+ * "AS IS", WITHOUT ANY WARRANTIES OR PROMISES OF ANY KIND, either 
+ * expressed or implied. The author(s) of this project is not 
+ * liable for any harms, errors or troubles caused by using the 
+ * source or the product, unless implied by law. By using this 
+ * project, or part of it, you are acknowledging the complete terms 
+ * and conditions of licensing of this project as specified in AAF 
+ * Inspectable License 1.0 available at this URL: 
+ * 
+ * https://github.com/aldrinsartfactory/InspectableLicense/
+ * 
+ * This project may contain parts that are not licensed under the 
+ * same license. If so, the licenses of those parts should be 
+ * appropriately mentioned in those parts of the project. The 
+ * Author MAY provide a notice about the parts of the project that 
+ * are not licensed under the same license in a publicly visible 
+ * manner.
+ * 
+ * You are NOT ALLOWED to sell, or distribute THIS project, it's 
+ * contents, the source or the product or the build result of the 
+ * source under commercial or non-commercial purposes. You are NOT 
+ * ALLOWED to revamp, rebrand, refactor, modify, the source, product 
+ * or the contents of this project.
+ * 
+ * You are NOT ALLOWED to use the name, branding and identity of this 
+ * project to identify or brand any other project. You ARE however 
+ * allowed to use the name and branding to pinpoint/show the source 
+ * of the contents/code/logic of any other project. You are not 
+ * allowed to use the identification of the Authors of this project 
+ * to associate them to other projects, in a way that is deceiving 
+ * or misleading or gives out false information.
+ */
+
 #ifndef QAT_LEXER_TOKEN_HPP
 #define QAT_LEXER_TOKEN_HPP
 
 #include <string>
-#include "token_type.hpp"
+#include "./token_type.hpp"
+#include "./lexer.hpp"
 
 namespace qat
 {
@@ -16,11 +59,13 @@ namespace qat
         class Token
         {
         private:
-            explicit Token(
-                TokenType _type,
-                std::string _value = "") : type(_type), value(_value) {}
+            explicit Token(TokenType _type, long long _line, long long _character)
+                : type(_type), value(""), hasValue(false), line(_line), character(_character) {}
 
-        public:            
+            explicit Token(TokenType _type, std::string _value, long long _line, long long _character)
+                : type(_type), value(_value), hasValue(true), line(_line), character(_character) {}
+
+        public:
             /**
              * @brief Tokens that represents a value. Integer literal, Double literal,
              * Identifiers are some examples of this kind of token
@@ -29,9 +74,10 @@ namespace qat
              * this is TokenType::identifier
              * @param _value Value of the token - In the case of an identifier token,
              * this is the name of the identifier
+             * @param lexer Pointer to the Lexer instance to get the line and character numbers
              * @return Token 
              */
-            static Token valued(TokenType _type, std::string _value);
+            static Token valued(TokenType _type, std::string _value, Lexer *lexer);
 
             /**
              * @brief Tokens that are by default, recognised by the language. These tokens
@@ -39,9 +85,10 @@ namespace qat
              * 
              * @param _type Type of the token - In the case of an Identifier token,
              * this is TokenType::identifier
+             * @param lexer Pointer to the Lexer instance to get the line and character numbers
              * @return Token 
              */
-            static Token normal(TokenType _type);
+            static Token normal(TokenType _type, Lexer *lexer);
 
             /**
              * @brief Type of the token. Can mostly refer to symbols that
@@ -56,9 +103,22 @@ namespace qat
             std::string value;
 
             /**
-             * @brief Whether this token represents a value 
+             * @brief Whether this token represents a value. This is necessary as the value can
+             * just be an empty string and cannot be considerd as no value in that case.
              */
             bool hasValue = false;
+
+            /**
+             * @brief Line number of the token
+             * 
+             */
+            long long line;
+
+            /**
+             * @brief Character in the line the token corresponds to
+             * 
+             */
+            long long character;
         };
     }
 }

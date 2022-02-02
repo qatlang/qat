d64f62ee2be6ed718db96e7aa544a5a9972c7879
Aldrin Mathew <admin@aldrinmathew.com>
2022-02-02 08:54:07 +0530

    Added `NodeType` values
    - Added NodeType values for staticDeclaration, saySentence,
      ifElseSentence, giveSentence, exposeSpace, defineObjectType,
      closeSpace, reassignment, variableExpression, thisExpression,
      toConversion, unaryExpression, ternaryExpression, functionCall,
      binaryExpression, allocateOnHeap, memberFunctionCall,
      memberVariableExpression, sizeOf
    
    - Added `filePosition` field to `Node` class to represent the beginning
      position of an AST member in a file
    
    - Renamed `doubleLiteral` to `floatLiteral` to apply to multiple kinds
      of float datatypes

--- src/AST/node.hpp
+++ src/AST/node.hpp
@@ -1,24 +1,26 @@
 #ifndef QAT_AST_NODE_HPP
 #define QAT_AST_NODE_HPP
 
-#include "llvm/IR/Value.h"
-#include "node_type.hpp"
 #include "../IR/generator.hpp"
+#include "../utilities/file_position.hpp"
+#include "node_type.hpp"
+#include "llvm/IR/Value.h"
 
-namespace qat
-{
-    namespace AST
-    {
-        class Node
-        {
-        public:
-            virtual ~Node() = default;
+namespace qat {
+namespace AST {
+class Node {
+public:
+  Node(utilities::FilePosition _filePosition) : filePosition(_filePosition) {}
 
-            virtual llvm::Value *generate(IR::Generator *generator) = 0;
+  virtual ~Node(){};
 
-            virtual NodeType nodeType() = 0;
-        };
-    }
-}
+  virtual llvm::Value *generate(IR::Generator *generator){};
+
+  virtual NodeType nodeType(){};
+
+  utilities::FilePosition filePosition;
+};
+} // namespace AST
+} // namespace qat
 
 #endif
--- src/AST/node_type.hpp
+++ src/AST/node_type.hpp
@@ -1,21 +1,36 @@
 #ifndef QAT_AST_NODETYPE_HPP
 #define QAT_AST_NODETYPE_HPP
 
-namespace qat
-{
-    namespace AST
-    {
-        enum class NodeType
-        {
-            functionPrototype,
-            functionDefinition,
-            functionParagraph,
-            localDeclaration,
-            integerLiteral,
-            doubleLiteral,
-            stringLiteral,
-        };
-    }
+namespace qat {
+namespace AST {
+enum class NodeType {
+  functionPrototype,
+  functionDefinition,
+  localDeclaration,
+  integerLiteral,
+  floatLiteral,
+  stringLiteral,
+  staticDeclaration,
+  saySentence,
+  ifElseSentence,
+  giveSentence,
+  exposeSpace,
+  defineObjectType,
+  closeSpace,
+  reassignment,
+  variableExpression,
+  thisExpression,
+  toConversion,
+  unaryExpression,
+  ternaryExpression,
+  functionCall,
+  binaryExpression,
+  allocateOnHeap,
+  memberFunctionCall,
+  memberVariableExpression,
+  sizeOf,
+};
 }
+} // namespace qat
 
 #endif

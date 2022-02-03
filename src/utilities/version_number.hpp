33cf17d787456de75768c477c7ccb2de1111f0e5
Aldrin Mathew <admin@aldrinmathew.com>
2022-02-03 08:48:13 +0530

    Updated LICENSE Manuscript
    - Updated LICENSE Manuscript and added it to files that didn't contain
      it.

--- src/utilities/version_number.hpp
+++ src/utilities/version_number.hpp
@@ -1,3 +1,45 @@
+/**
+ * Qat Programming Language : Copyright 2022 : Aldrin Mathew 
+ *
+ * AAF INSPECTABLE LICENCE - 1.0
+ * 
+ * This project is licensed under the AAF Inspectable Licence 1.0. 
+ * You are allowed to inspect the source of this project(s) free of 
+ * cost, and also to verify the authenticity of the product.
+ * 
+ * Unless required by applicable law, this project is provided 
+ * "AS IS", WITHOUT ANY WARRANTIES OR PROMISES OF ANY KIND, either 
+ * expressed or implied. The author(s) of this project is not 
+ * liable for any harms, errors or troubles caused by using the 
+ * source or the product, unless implied by law. By using this 
+ * project, or part of it, you are acknowledging the complete terms 
+ * and conditions of licensing of this project as specified in AAF 
+ * Inspectable Licence 1.0 available at this URL: 
+ * 
+ * https://github.com/aldrinsartfactory/InspectableLicence/
+ * 
+ * This project may contain parts that are not licensed under the 
+ * same licence. If so, the licences of those parts should be 
+ * appropriately mentioned in those parts of the project. The 
+ * Author MAY provide a notice about the parts of the project that 
+ * are not licensed under the same licence in a publicly visible 
+ * manner.
+ * 
+ * You are NOT ALLOWED to sell, or distribute THIS project, its 
+ * contents, the source or the product or the build result of the 
+ * source under commercial or non-commercial purposes. You are NOT 
+ * ALLOWED to revamp, rebrand, refactor, modify, the source, product 
+ * or the contents of this project.
+ * 
+ * You are NOT ALLOWED to use the name, branding and identity of this 
+ * project to identify or brand any other project. You ARE however 
+ * allowed to use the name and branding to pinpoint/show the source 
+ * of the contents/code/logic of any other project. You are not 
+ * allowed to use the identification of the Authors of this project 
+ * to associate them to other projects, in a way that is deceiving 
+ * or misleading or gives out false information. 
+ */
+
 #ifndef QAT_UTILITIES_VERSION_NUMBER_HPP
 #define QAT_UTILITIES_VERSION_NUMBER_HPP
 
@@ -11,13 +53,13 @@ class VersionNumber {
   VersionNumber(const std::string version);
 
   VersionNumber(const unsigned int _major, const unsigned int _minor,
-                const unsigned int _patch, std::string _prerelease,
-                std::string _build)
+                const unsigned int _patch, const std::string _prerelease,
+                const std::string _build)
       : major(_major), minor(_minor), patch(_patch), prerelease(_prerelease),
         build(_build) {}
 
   VersionNumber(const unsigned int _major, const unsigned int _minor,
-                const unsigned int _patch, std::string _prerelease)
+                const unsigned int _patch, const std::string _prerelease)
       : major(_major), minor(_minor), patch(_patch), prerelease(_prerelease),
         build(llvm::None) {}
 

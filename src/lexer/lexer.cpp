94c77d57faa5896da3eb1afd0dfc2363f2ccc7fe
Aldrin Mathew <admin@aldrinmathew.com>
2022-02-03 18:00:11 +0530

    Fixed #5 - Integer tokenised as Float
    - Fixed error that integer literals were tokenised as float literals

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -1,43 +1,43 @@
 /**
- * Qat Programming Language : Copyright 2022 : Aldrin Mathew 
+ * Qat Programming Language : Copyright 2022 : Aldrin Mathew
  *
  * AAF INSPECTABLE LICENCE - 1.0
- * 
- * This project is licensed under the AAF Inspectable Licence 1.0. 
- * You are allowed to inspect the source of this project(s) free of 
+ *
+ * This project is licensed under the AAF Inspectable Licence 1.0.
+ * You are allowed to inspect the source of this project(s) free of
  * cost, and also to verify the authenticity of the product.
- * 
- * Unless required by applicable law, this project is provided 
- * "AS IS", WITHOUT ANY WARRANTIES OR PROMISES OF ANY KIND, either 
- * expressed or implied. The author(s) of this project is not 
- * liable for any harms, errors or troubles caused by using the 
- * source or the product, unless implied by law. By using this 
- * project, or part of it, you are acknowledging the complete terms 
- * and conditions of licensing of this project as specified in AAF 
- * Inspectable Licence 1.0 available at this URL: 
- * 
+ *
+ * Unless required by applicable law, this project is provided
+ * "AS IS", WITHOUT ANY WARRANTIES OR PROMISES OF ANY KIND, either
+ * expressed or implied. The author(s) of this project is not
+ * liable for any harms, errors or troubles caused by using the
+ * source or the product, unless implied by law. By using this
+ * project, or part of it, you are acknowledging the complete terms
+ * and conditions of licensing of this project as specified in AAF
+ * Inspectable Licence 1.0 available at this URL:
+ *
  * https://github.com/aldrinsartfactory/InspectableLicence/
- * 
- * This project may contain parts that are not licensed under the 
- * same licence. If so, the licences of those parts should be 
- * appropriately mentioned in those parts of the project. The 
- * Author MAY provide a notice about the parts of the project that 
- * are not licensed under the same licence in a publicly visible 
+ *
+ * This project may contain parts that are not licensed under the
+ * same licence. If so, the licences of those parts should be
+ * appropriately mentioned in those parts of the project. The
+ * Author MAY provide a notice about the parts of the project that
+ * are not licensed under the same licence in a publicly visible
  * manner.
- * 
- * You are NOT ALLOWED to sell, or distribute THIS project, its 
- * contents, the source or the product or the build result of the 
- * source under commercial or non-commercial purposes. You are NOT 
- * ALLOWED to revamp, rebrand, refactor, modify, the source, product 
+ *
+ * You are NOT ALLOWED to sell, or distribute THIS project, its
+ * contents, the source or the product or the build result of the
+ * source under commercial or non-commercial purposes. You are NOT
+ * ALLOWED to revamp, rebrand, refactor, modify, the source, product
  * or the contents of this project.
- * 
- * You are NOT ALLOWED to use the name, branding and identity of this 
- * project to identify or brand any other project. You ARE however 
- * allowed to use the name and branding to pinpoint/show the source 
- * of the contents/code/logic of any other project. You are not 
- * allowed to use the identification of the Authors of this project 
- * to associate them to other projects, in a way that is deceiving 
- * or misleading or gives out false information. 
+ *
+ * You are NOT ALLOWED to use the name, branding and identity of this
+ * project to identify or brand any other project. You ARE however
+ * allowed to use the name and branding to pinpoint/show the source
+ * of the contents/code/logic of any other project. You are not
+ * allowed to use the identification of the Authors of this project
+ * to associate them to other projects, in a way that is deceiving
+ * or misleading or gives out false information.
  */
 
 #include "lexer.hpp"
@@ -75,8 +75,9 @@ void qat::lexer::Lexer::readNext(std::string lexerContext) {
 }
 
 qat::utilities::FilePosition qat::lexer::Lexer::getPosition() {
-  return qat::utilities::FilePosition(fsexp::path(filePath), lineNumber,
-                                      characterNumber);
+  return qat::utilities::FilePosition(
+      fsexp::path(filePath), lineNumber,
+      ((characterNumber > 0) ? (characterNumber - 1) : 0));
 }
 
 std::vector<qat::lexer::Token> *qat::lexer::Lexer::analyse() {
@@ -430,8 +431,9 @@ qat::lexer::Token qat::lexer::Lexer::tokeniser() {
           isFloat = true;
           numberValue += '.';
         } else {
+          /// This is in the reverse order since the last element is returned first
           buffer.push_back(Token::normal(TokenType::stop, this->getPosition()));
-          buffer.push_back(Token::valued(TokenType::FloatLiteral, numberValue,
+          buffer.push_back(Token::valued(TokenType::IntegerLiteral, numberValue,
                                          this->getPosition()));
           return tokeniser();
         }

feebfe3c8dbd3c2fd5128e1fdfdd3712b560c845
Aldrin Mathew <admin@aldrinmathew.com>
2022-02-10 00:54:39 +0530

    Added `qat::AST::Float` datatype
    - Added `qat::AST::Float` to support floating point numbers in the
      language.
    
    - The language now supports different kinds of floats.

---
+++ src/AST/types/float.cpp
@@ -0,0 +1,69 @@
+/**
+ * Qat Programming Language : Copyright 2022 : Aldrin Mathew
+ *
+ * AAF INSPECTABLE LICENCE - 1.0
+ *
+ * This project is licensed under the AAF Inspectable Licence 1.0.
+ * You are allowed to inspect the source of this project(s) free of
+ * cost, and also to verify the authenticity of the product.
+ *
+ * Unless required by applicable law, this project is provided
+ * "AS IS", WITHOUT ANY WARRANTIES OR PROMISES OF ANY KIND, either
+ * expressed or implied. The author(s) of this project is not
+ * liable for any harms, errors or troubles caused by using the
+ * source or the product, unless implied by law. By using this
+ * project, or part of it, you are acknowledging the complete terms
+ * and conditions of licensing of this project as specified in AAF
+ * Inspectable Licence 1.0 available at this URL:
+ *
+ * https://github.com/aldrinsartfactory/InspectableLicence/
+ *
+ * This project may contain parts that are not licensed under the
+ * same licence. If so, the licences of those parts should be
+ * appropriately mentioned in those parts of the project. The
+ * Author MAY provide a notice about the parts of the project that
+ * are not licensed under the same licence in a publicly visible
+ * manner.
+ *
+ * You are NOT ALLOWED to sell, or distribute THIS project, its
+ * contents, the source or the product or the build result of the
+ * source under commercial or non-commercial purposes. You are NOT
+ * ALLOWED to revamp, rebrand, refactor, modify, the source, product
+ * or the contents of this project.
+ *
+ * You are NOT ALLOWED to use the name, branding and identity of this
+ * project to identify or brand any other project. You ARE however
+ * allowed to use the name and branding to pinpoint/show the source
+ * of the contents/code/logic of any other project. You are not
+ * allowed to use the identification of the Authors of this project
+ * to associate them to other projects, in a way that is deceiving
+ * or misleading or gives out false information.
+ */
+
+#include "./float.hpp"
+
+llvm::Type *qat::AST::FloatType::generate(qat::IR::Generator *generator) {
+  switch (bitWidth) {
+  case FloatTypeKind::_brain: {
+    return generator->builder.getBFloatTy();
+  }
+  case FloatTypeKind::_half: {
+    return generator->builder.getHalfTy();
+  }
+  case FloatTypeKind::_32: {
+    return generator->builder.getFloatTy();
+  }
+  case FloatTypeKind::_64: {
+    return generator->builder.getDoubleTy();
+  }
+  case FloatTypeKind::_80: {
+    return llvm::Type::getX86_FP80Ty(generator->llvmContext);
+  }
+  case FloatTypeKind::_128PPC: {
+    return llvm::Type::getPPC_FP128Ty(generator->llvmContext);
+  }
+  case FloatTypeKind::_128: {
+    return llvm::Type::getFP128Ty(generator->llvmContext);
+  }
+  }
+}

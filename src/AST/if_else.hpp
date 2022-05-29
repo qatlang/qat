8258c977f2a4d285a49b03c6893dbdd393dfbb13
Aldrin Mathew <admin@aldrinmathew.com>
2022-05-30 00:03:09 +0530

    Added `qat::AST::IfElse`
    - IfElse is used to represent two kinds of conditional sentences - `if`
      & `if-else`. The Else block is optional and if omitted, IfElse becomes
      a plain `if` sentence
    
    - This also can be used for nesting conditionals, from my understanding.
      Although testing might prove me wrong

---
+++ src/AST/if_else.hpp
@@ -0,0 +1,126 @@
+/**
+ * Qat Programming Language : Copyright 2022 : Aldrin Mathew
+ *
+ * AAF INSPECTABLE LICENCE - 1.0
+ *
+ * This project is licensed under the AAF Inspectable Licence 1.0.
+ * You are allowed to inspect the source of this project(s) free of
+ * cost, and also to verify the authenticity of the product.
+ *
+ * Unless required by applicable law, this project is provided
+ * "AS IS", WITHOUT ANY WARRANTIES OR PROMISES OF ANY KIND, either
+ * expressed or implied. The author(s) of this project is not
+ * liable for any harms, errors or troubles caused by using the
+ * source or the product, unless implied by law. By using this
+ * project, or part of it, you are acknowledging the complete terms
+ * and conditions of licensing of this project as specified in AAF
+ * Inspectable Licence 1.0 available at this URL:
+ *
+ * https://github.com/aldrinsartfactory/InspectableLicence/
+ *
+ * This project may contain parts that are not licensed under the
+ * same licence. If so, the licences of those parts should be
+ * appropriately mentioned in those parts of the project. The
+ * Author MAY provide a notice about the parts of the project that
+ * are not licensed under the same licence in a publicly visible
+ * manner.
+ *
+ * You are NOT ALLOWED to sell, or distribute THIS project, its
+ * contents, the source or the product or the build result of the
+ * source under commercial or non-commercial purposes. You are NOT
+ * ALLOWED to revamp, rebrand, refactor, modify, the source, product
+ * or the contents of this project.
+ *
+ * You are NOT ALLOWED to use the name, branding and identity of this
+ * project to identify or brand any other project. You ARE however
+ * allowed to use the name and branding to pinpoint/show the source
+ * of the contents/code/logic of any other project. You are not
+ * allowed to use the identification of the Authors of this project
+ * to associate them to other projects, in a way that is deceiving
+ * or misleading or gives out false information.
+ */
+
+#ifndef QAT_AST_IF_ELSE_HPP
+#define QAT_AST_IF_ELSE_HPP
+
+#include "../utils/llvm_type_to_name.hpp"
+#include "./block.hpp"
+#include "./expression.hpp"
+#include "./node_type.hpp"
+#include "./sentence.hpp"
+#include <optional>
+#include <vector>
+
+namespace qat {
+namespace AST {
+
+/**
+ * @brief IfElse is used to represent two kinds of conditional sentences : If
+ * and If-Else. The Else block is optional and if omitted, IfElse becomes a
+ * plain if sentence
+ *
+ */
+class IfElse : public Sentence {
+private:
+  /**
+   * @brief Condition for this if sentence
+   *
+   */
+  Expression condition;
+
+  /**
+   * @brief The block belonging to the if section
+   *
+   */
+  Block if_block;
+
+  /**
+   * @brief The optional block belonging to the else section
+   *
+   */
+  std::optional<Block> else_block;
+
+  /**
+   * @brief The
+   *
+   */
+  Block merge_block;
+
+public:
+  /**
+   * @brief IfElse is used to represent two kinds of conditional statements : If
+   * and If-Else. The Else block is optional and if omitted, this becomes a
+   * plain if sentence
+   *
+   * @param _condition Condition in the if sentence
+   * @param _if_block The Block of the if sentence
+   * @param _else_block The optional Block of the else sentence
+   * @param _merge_block The optional Block representing the remaining sentences
+   * not part of the conditional blocks
+   * @param _filePlacement
+   *
+   */
+  IfElse(Expression _condition, Block _if_block,
+         std::optional<Block> _else_block, std::optional<Block> _merge_block,
+         utils::FilePlacement _filePlacement);
+
+  /**
+   * @brief This is the code generator function that handles the generation of
+   * LLVM IR
+   *
+   * @param generator The IR::Generator instance that handles LLVM IR Generation
+   * @return llvm::Value*
+   */
+  llvm::Value *generate(IR::Generator *generator);
+
+  /**
+   * @brief Type of the node represented by this AST member
+   *
+   * @return NodeType
+   */
+  NodeType nodeType() { return NodeType::ifElse; }
+};
+} // namespace AST
+} // namespace qat
+
+#endif

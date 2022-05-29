9935108d48154ecba3c76a061749da3a4a5e250c
Aldrin Mathew <admin@aldrinmathew.com>
2022-05-29 11:09:35 +0530

    Added `qat::CLI::Config`
    - Config handles all CLI arguments passed to the compiler and also sets
      the options and values according to those args
    
    - Some arguments trigger message display and those are achieved by using
      functions in qat::CLI::display
    
    - Currently some options related to compiler behaviour are set by Config
      when it encounters some arguments, in the corresponding parts of the
      compiler. This will later be changed so that those appropriate parts
      will read the values from Config instead

---
+++ src/CLI/config.cpp
@@ -0,0 +1,114 @@
+#include "./config.hpp"
+
+namespace qat {
+namespace CLI {
+
+Config *Config::instance = nullptr;
+
+Config Config::init(u64 count, const char **args) {
+  if (instance == nullptr) {
+    return *(new Config(count, args));
+  } else {
+    return get();
+  }
+}
+
+void Config::destroy() {
+  if (Config::instance != nullptr) {
+    delete Config::instance;
+    Config::instance = nullptr;
+  }
+}
+
+bool Config::has_instance() { return Config::instance != nullptr; }
+
+Config Config::get() {
+  if (has_instance()) {
+    return *instance;
+  }
+}
+
+Config::Config(u64 count, const char **args) {
+  if (!has_instance()) {
+    Config::instance = this;
+    invoke_path = args[0];
+    verbose = false;
+    exit_after = false;
+    if (count <= 1) {
+      std::cout << "No commands or arguments provided" << std::endl;
+      exit(0);
+    }
+    build_commit = BUILD_COMMIT_QUOTED;
+    if (build_commit.find('"') != std::string::npos) {
+      std::string res;
+      for (auto ch : build_commit) {
+        if (ch != '"') {
+          res += ch;
+        }
+      }
+      build_commit = res;
+    }
+
+    std::string command = args[1];
+    int proceed = 2;
+    if (command == "run") {
+      // TODO - Implement this after implementing Compile and Run
+    } else if (command == "build") {
+      if (count == 2) {
+        paths.push_back(fsexp::current_path());
+        return;
+      }
+    } else if (command == "help") {
+      display::help();
+      exit_after = true;
+    } else if (command == "about") {
+      display::about(build_commit);
+      exit_after = true;
+    } else if (command == "version") {
+      display::version();
+      exit_after = true;
+    } else if (command == "show") {
+      if (count == 2) {
+        std::cout << "Nothing to show here" << std::endl;
+      } else {
+        std::string candidate = args[2];
+        if (candidate == "build-info") {
+          display::build_info(build_commit);
+        } else if (candidate == "web") {
+          display::websites();
+        } else if (candidate == "errors") {
+          display::errors();
+        }
+        proceed = 3;
+      }
+      exit_after = true;
+    }
+    for (std::size_t i = proceed; i < count; i++) {
+      std::string arg = args[i];
+      if (arg == "-v" || arg == "--verbose") {
+        verbose = true;
+      } else if (arg == "--report") {
+        lexer::Lexer::show_report = true;
+        // TODO - Set parser report option here once that is implemented
+      } else if (arg == "--emit-tokens") {
+        lexer::Lexer::emit_tokens = true;
+      } else {
+        if (fsexp::exists(arg)) {
+          paths.push_back(fsexp::path(arg));
+        } else {
+          std::cout
+              << "Provided path " << fsexp::path(arg).string()
+              << " does not exist! Please provide path to a file or directory"
+              << std::endl;
+          exit_after = true;
+        }
+      }
+    }
+    if (exit_after && (proceed == 2) && (count > 2)) {
+      std::cout << "\nIgnoring additional arguments provided...\n";
+    }
+  }
+}
+
+} // namespace CLI
+} // namespace qat
---
+++ src/CLI/config.hpp
@@ -0,0 +1,140 @@
+#ifndef QAT_CLI_CONFIG_HPP
+#define QAT_CLI_CONFIG_HPP
+
+#include "../lexer/lexer.hpp"
+#include "../utils/types.hpp"
+#include "./display.hpp"
+#include <experimental/filesystem>
+#include <iostream>
+#include <string>
+#include <vector>
+namespace qat {
+namespace CLI {
+namespace fsexp = std::experimental::filesystem;
+/**
+ * @brief CLI Configuration -
+ *
+ * This is a singleton and the instance is dynamically allocated. It is freed
+ * automatically when the program exits, but can also be freed by calling
+ * Config::destroy()
+ *
+ */
+class Config {
+  // TODO - Add support for output paths
+private:
+  /**
+   * @brief Construct a new Config object
+   * This expects the number of arguments passed to the compiler and the
+   * arguments, and it initialises the members and sets corresponding values
+   *
+   * @param count
+   * @param args
+   */
+  Config(u64 count, const char **args);
+
+  /**
+   * @brief The first CLI argument. Indicates the path specified to invoke the
+   * compiler
+   *
+   */
+  std::string invoke_path;
+
+  /**
+   * @brief All paths provided to the compiler for compilation of qat source
+   * code.
+   *
+   */
+  std::vector<fsexp::path> paths;
+
+  /**
+   * @brief The latest commit at the time of the build of the compiler
+   *
+   */
+  std::string build_commit;
+
+  /**
+   * @brief Whether the compiler should exit after initialisation of Config is
+   * complete and arguments have been handled
+   *
+   */
+  bool exit_after;
+
+  /**
+   * @brief Whether the user has chosen to be in Verbose mode
+   *
+   */
+  bool verbose;
+
+public:
+  /**
+   * @brief The pointer to the only instance of Config. If this is nullptr, the
+   * either the Config hasn't been initialised or it has been freed from memory
+   *
+   */
+  static Config *instance;
+
+  /**
+   * @brief The initialisation function for Config. Multiple calls to this
+   * function returns the same instance
+   *
+   * @param count The number of arguments provided to the compiler
+   * @param args The arguments provided to the compiler
+   * @return Config
+   */
+  static Config init(u64 count, const char **args);
+
+  /**
+   * @brief This is technically the proper function to get the existing instance
+   * of Config. Or may be not, since init already returns an instance
+   *
+   * @return Config
+   */
+  static Config get();
+
+  /**
+   * @brief Whether there is an instance of Config that has been initialised
+   *
+   * @return true
+   * @return false
+   */
+  static bool has_instance();
+
+  /**
+   * @brief Function that destroys/deletes the single instance of Config
+   *
+   */
+  static void destroy();
+
+  /** Behaviour specific functions */
+
+  /**
+   * @brief Whether there are paths provided in the CLI and the compilation step
+   * should proceed
+   *
+   * @return true
+   * @return false
+   */
+  bool is_compile() { return !paths.empty(); }
+
+  /**
+   * @brief Whether compiler should exit after arguments are handled by Config
+   *
+   * This is usually true for simple actions like version display, about... and
+   * if there were errors during Config initialisation
+   *
+   * @return true
+   * @return false
+   */
+  bool should_exit() { return exit_after; }
+
+  /**
+   * @brief Get paths provided to the compiler for the compilation stage
+   *
+   * @return std::vector<fsexp::path>
+   */
+  std::vector<fsexp::path> get_paths() { return paths; }
+};
+} // namespace CLI
+} // namespace qat
+
+#endif

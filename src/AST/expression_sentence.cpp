70070b81e86a5980bcb14c344d92123237ffce73
Aldrin Mathew <admin@aldrinmathew.com>
2022-06-04 15:56:41 +0530

    Added `qat::AST::ExpressionSentence`
    - Added AST representation for a sentence that is just an expression.
    
    - This is used to represent a function call that is not assigned to any
      entity, a standalone binary or unary expression, or series of function
      calls

---
+++ src/AST/expression_sentence.cpp
@@ -0,0 +1,15 @@
+#include "./expression_sentence.hpp"
+
+namespace qat {
+namespace AST {
+
+ExpressionSentence::ExpressionSentence(Expression _exp,
+                                       utils::FilePlacement _filePlacement)
+    : expr(_exp), qat::AST::Sentence(_filePlacement) {}
+
+llvm::Value *ExpressionSentence::generate(IR::Generator *generator) {
+  return expr.generate(generator);
+}
+
+} // namespace AST
+} // namespace qat
---
+++ src/AST/expression_sentence.hpp
@@ -0,0 +1,52 @@
+#ifndef QAT_EXPRESSION_SENTENCE_HPP
+#define QAT_EXPRESSION_SENTENCE_HPP
+
+#include "./expression.hpp"
+#include "./sentence.hpp"
+
+namespace qat {
+namespace AST {
+
+/**
+ * @brief ExpressionSentence is used to represent a sentence that is a
+ * standalone expression. Like a function call with no assignment,  lonely
+ * binary or unary expression or series of function calls...
+ *
+ */
+class ExpressionSentence : public Sentence {
+  /**
+   * @brief Expression in this sentence
+   *
+   */
+  Expression expr;
+
+public:
+  /**
+   * @brief Construct a new Sentence that is just an expression
+   *
+   * @param _exp Expression in the sentence
+   * @param _filePlacement
+   */
+  ExpressionSentence(Expression _expr, utils::FilePlacement _filePlacement);
+
+  /**
+   * @brief This is the code generator function that handles the generation of
+   * LLVM IR
+   *
+   * @param generator The IR::Generator instance that handles LLVM IR Generation
+   * @return llvm::Value*
+   */
+  llvm::Value *generate(IR::Generator *generator);
+
+  /**
+   * @brief Type of the node represented by this AST member
+   *
+   * @return NodeType
+   */
+  NodeType nodeType() { return NodeType::expressionSentence; }
+};
+
+} // namespace AST
+} // namespace qat
+
+#endif

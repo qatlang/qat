b26d562a1e4f91d254385218fea0ab6c3d04abe9
Aldrin Mathew <admin@aldrinmathew.com>
2022-06-13 00:41:33 +0530

    Added `qat::AST::NullPointer`
    - Added AST representation for null pointers. Users can use the `null`
      keyword for null pointers
    
    - This will be automatically cast to appropriate type during assignments
      and declaration

---
+++ src/AST/expressions/null_pointer.cpp
@@ -0,0 +1,21 @@
+#include "./null_pointer.hpp"
+
+namespace qat {
+namespace AST {
+
+NullPointer::NullPointer(utils::FilePlacement _filePlacement)
+    : type(nullptr), Expression(_filePlacement) {}
+
+NullPointer::NullPointer(llvm::Type *_type, utils::FilePlacement _filePlacement)
+    : type(_type), Expression(_filePlacement) {}
+
+void NullPointer::set_type(llvm::Type *type_val) { type = type_val; }
+
+llvm::Value *NullPointer::generate(IR::Generator *generator) {
+  return llvm::Constant::getNullValue(
+      type ? type->getPointerTo()
+           : llvm::Type::getVoidTy(generator->llvmContext)->getPointerTo());
+}
+
+} // namespace AST
+} // namespace qat
---
+++ src/AST/expressions/null_pointer.hpp
@@ -0,0 +1,65 @@
+#ifndef QAT_AST_EXPRESSIONS_NULL_POINTER_HPP
+#define QAT_AST_EXPRESSIONS_NULL_POINTER_HPP
+
+#include "../expression.hpp"
+#include "llvm/IR/Type.h"
+
+namespace qat {
+namespace AST {
+
+/**
+ * @brief A null pointer
+ *
+ */
+class NullPointer : public Expression {
+private:
+  /**
+   * @brief Type of the pointer
+   *
+   */
+  llvm::Type *type;
+
+public:
+  /**
+   * @brief A null pointer
+   *
+   * @param _filePlacement
+   */
+  NullPointer(utils::FilePlacement _filePlacement);
+
+  /**
+   * @brief A null pointer of the specified type
+   *
+   * @param _type LLVM Type of the pointer
+   * @param _filePlacement
+   */
+  NullPointer(llvm::Type *_type, utils::FilePlacement _filePlacement);
+
+  /**
+   * @brief Set the type for this null pointer
+   *
+   * @param type Type of the pointer
+   */
+  void set_type(llvm::Type *type);
+
+  /**
+   * @brief This is the code generator function that handles the generation of
+   * LLVM IR
+   *
+   * @param generator The IR::Generator instance that handles LLVM IR Generation
+   * @return llvm::Value*
+   */
+  llvm::Value *generate(IR::Generator *generator);
+
+  /**
+   * @brief Type of the node represented by this AST member
+   *
+   * @return NodeType
+   */
+  NodeType nodeType() { return NodeType::nullPointer; }
+};
+
+} // namespace AST
+} // namespace qat
+
+#endif

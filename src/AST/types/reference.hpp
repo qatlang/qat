3b0d6b2abd3047f1dacc47d5b7f06fd8a9d48aed
Aldrin Mathew <admin@aldrinmathew.com>
2022-06-12 23:28:05 +0530

    Added `qat::AST::ReferenceType`
    - Added type representation in the AST for references. This is not that
      different from PointerType, except for the typeKind function
    
    - The typeKind function makes this representation useful

---
+++ src/AST/types/reference.hpp
@@ -0,0 +1,92 @@
+/**
+ * Qat Programming Language : Copyright 2022 : Aldrin Mathew
+ *
+ * AAF INSPECTABLE LICENCE - 1.0
+ *
+ * This project is licensed under the AAF Inspectable Licence 1.0.
+ * You are allowed to inspect the source of this project(s) free of
+ * cost, and also to verify the authenticity of the product.
+ *
+ * Unless required by applicable law, this project is provided
+ * "AS IS", WITHOUT ANY WARRANTIES OR PROMISES OF ANY KIND, either
+ * expressed or implied. The author(s) of this project is not
+ * liable for any harms, errors or troubles caused by using the
+ * source or the product, unless implied by law. By using this
+ * project, or part of it, you are acknowledging the complete terms
+ * and conditions of licensing of this project as specified in AAF
+ * Inspectable Licence 1.0 available at this URL:
+ *
+ * https://github.com/aldrinsartfactory/InspectableLicence/
+ *
+ * This project may contain parts that are not licensed under the
+ * same licence. If so, the licences of those parts should be
+ * appropriately mentioned in those parts of the project. The
+ * Author MAY provide a notice about the parts of the project that
+ * are not licensed under the same licence in a publicly visible
+ * manner.
+ *
+ * You are NOT ALLOWED to sell, or distribute THIS project, its
+ * contents, the source or the product or the build result of the
+ * source under commercial or non-commercial purposes. You are NOT
+ * ALLOWED to revamp, rebrand, refactor, modify, the source, product
+ * or the contents of this project.
+ *
+ * You are NOT ALLOWED to use the name, branding and identity of this
+ * project to identify or brand any other project. You ARE however
+ * allowed to use the name and branding to pinpoint/show the source
+ * of the contents/code/logic of any other project. You are not
+ * allowed to use the identification of the Authors of this project
+ * to associate them to other projects, in a way that is deceiving
+ * or misleading or gives out false information.
+ */
+
+#ifndef QAT_TYPES_POINTER_HPP
+#define QAT_TYPES_POINTER_HPP
+
+#include "./qat_type.hpp"
+#include "./void.hpp"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/Support/raw_ostream.h"
+#include <string>
+
+namespace qat {
+namespace AST {
+
+/**
+ * @brief A reference type in the language
+ *
+ */
+class ReferenceType : public QatType {
+private:
+  QatType type;
+
+public:
+  /**
+   * @brief Create a reference to the provided datatype
+   *
+   * @param _type Datatype to which the pointer is pointing to
+   * @param _filePlacement
+   */
+  ReferenceType(QatType _type, utils::FilePlacement _filePlacement);
+
+  /**
+   * @brief This is the code generator function that handles the generation of
+   * LLVM IR
+   *
+   * @param generator The IR::Generator instance that handles LLVM IR Generation
+   * @return llvm::Type*
+   */
+  llvm::Type *generate(IR::Generator *generator);
+
+  /**
+   * @brief TypeKind is used to detect variants of the QatType
+   *
+   * @return TypeKind
+   */
+  TypeKind typeKind();
+};
+
+} // namespace AST
+} // namespace qat
+
+#endif

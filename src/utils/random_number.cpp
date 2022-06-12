b3cf9ba194aadb5faa880c00fa1c6800128a4fb2
Aldrin Mathew <admin@aldrinmathew.com>
2022-06-12 20:23:08 +0530

    Added `qat::utils::random_number`
    - Added a simple random number generator with 64 bits of randomness

---
+++ src/utils/random_number.cpp
@@ -0,0 +1,15 @@
+#include "./random_number.hpp"
+
+namespace qat {
+namespace utils {
+
+uint64_t random_number() {
+  std::random_device dev;
+  std::mt19937_64 rng(dev());
+  std::uniform_int_distribution<std::mt19937_64::result_type> dist(
+      1, UINT_FAST64_MAX);
+  return dist(rng);
+}
+
+} // namespace utils
+} // namespace qat
---
+++ src/utils/random_number.hpp
@@ -0,0 +1,22 @@
+#ifndef QAT_UTILS_RANDOM_NUMBER_HPP
+#define QAT_UTILS_RANDOM_NUMBER_HPP
+
+#include <cstdint>
+#include <random>
+#include <sstream>
+#include <string>
+
+namespace qat {
+namespace utils {
+
+/**
+ * @brief Generate a 64-bit random number with 64-bits of randomness
+ *
+ * @return uint64_t
+ */
+uint64_t random_number();
+
+} // namespace utils
+} // namespace qat
+
+#endif

3a63d16d74f010c3935d046e31b50cdca9fa4f80
Aldrin Mathew <admin@aldrinmathew.com>
2022-06-18 11:51:00 +0530

    Updated `qat::IR::Generator`
    - This class is used by all AST members for LLVM IR Generation
    
    - The new IR representation is not integrated with the Generator yet
    
    - This file contains months of work, and also was completely lost 4
      times due to git messing up my project

--- src/IR/generator.hpp
+++ src/IR/generator.hpp
@@ -1,70 +1,137 @@
 #ifndef QAT_IR_GENERATOR_HPP
 #define QAT_IR_GENERATOR_HPP
 
-#include <vector>
+#include "../AST/box.hpp"
+#include "../IO/qat_file.hpp"
+#include "../utils/visibility.hpp"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/GlobalValue.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Type.h"
+#include <string>
+#include <vector>
 
-namespace qat
-{
-    namespace IR
-    {
-        /**
-         * @brief The IR Generator for QAT
-         */
-        class Generator
-        {
-        private:
-            /**
-             * @brief Module is a Top level container of all LLVM IR objects
-             * Multiple modules can represent multiple libraries
-             */
-            std::vector<std::unique_ptr<llvm::Module>> modules;
-            std::size_t activeModuleIndex = 0;
+namespace qat {
+namespace IR {
+/**
+ * @brief The IR Generator for QAT
+ */
+class Generator {
+private:
+  IO::QatFile *unit;
 
-        public:
-            /**
-             * @brief The LLVMContext determines the context and scope of all
-             * instructions, functions, blocks and variables
-             */
-            llvm::LLVMContext llvmContext = llvm::LLVMContext();
+  /**
+   * @brief The name to be prefixed to all new global variables, functions,
+   * boxes, libs and types
+   *
+   */
+  std::string name_prefix;
 
-            /**
-             * @brief Create a Function object pointer. This is not accompanying Function AST as
-             * the module is a private variable
-             * 
-             * @param name Name of the function
-             * @param parameterTypes LLVM Type of the parameters of the function
-             * @param returnType LLVM Type of the return value of the function
-             * @param isVariableArguments Whether the function has variable number of arguments
-             * @param linkageType How to link the function symbol
-             * @return llvm::Function* 
-             */
-            llvm::Function *createFunction(
-                std::string name,
-                std::vector<llvm::Type *> parameterTypes,
-                llvm::Type *returnType,
-                bool isVariableArguments,
-                llvm::GlobalValue::LinkageTypes linkageType);
+public:
+  /**
+   * @brief The LLVMContext determines the context and scope of all
+   * instructions, functions, blocks and variables
+   */
+  llvm::LLVMContext llvmContext;
 
-            /**
-             * @brief Provided of Basic API to create instructions and then append
-             * it to the end of a BasicBlock or to a specified location  
-             */
-            std::unique_ptr<llvm::IRBuilder<>> builder = std::make_unique<llvm::IRBuilder<>>(
-                llvm::IRBuilder(
-                    llvmContext,
-                    llvm::ConstantFolder(),
-                    llvm::IRBuilderDefaultInserter(),
-                    nullptr,
-                    llvm::None) //
-            );
-        };
+  /**
+   * @brief Provides Basic API to create instructions and then append
+   * it to the end of a BasicBlock or to a specified location
+   */
+  llvm::IRBuilder<llvm::ConstantFolder, llvm::IRBuilderDefaultInserter>
+      builder = llvm::IRBuilder(llvmContext, llvm::ConstantFolder(),
+                                llvm::IRBuilderDefaultInserter(), nullptr,
+                                llvm::None);
+
+  /**
+   * @brief All the boxes exposed in the current scope. This will automatically
+   * be populated and de-populated when the expose scope starts and ends
+   *
+   */
+  std::vector<qat::AST::Box> exposed;
+
+  /**
+   * @brief All visibilities of the symbols generated till a particular time
+   * period. This is used to compound visibility. This is very helpful to manage
+   * the public visibility.
+   *
+   */
+  std::vector<utils::VisibilityKind> visibilities;
+
+  /**
+   * @brief Create a Function object pointer. This is not accompanying Function
+   * AST as the module is a private variable
+   *
+   * @param name Name of the function
+   * @param parameterTypes LLVM Type of the parameters of the function
+   * @param returnType LLVM Type of the return value of the function
+   * @param isVariableArguments Whether the function has variable number of
+   * arguments
+   * @param linkageType How to link the function symbol
+   * @return llvm::Function*
+   */
+  llvm::Function *
+  create_function(const std::string name,
+                  const std::vector<llvm::Type *> parameterTypes,
+                  llvm::Type *returnType, const bool isVariableArguments,
+                  const llvm::GlobalValue::LinkageTypes linkageType);
+
+  llvm::GlobalVariable *
+  create_global_variable(const std::string name, llvm::Type *type,
+                         llvm::Value *value, const bool is_constant,
+                         const utils::VisibilityInfo access_info);
+
+  llvm::Function *get_function(const std::string name);
+
+  llvm::GlobalVariable *get_global_variable(const std::string name);
+
+  bool does_function_exist(const std::string name);
+
+  class AddResult {
+  private:
+    AddResult(bool _result, std::optional<std::string> _kind)
+        : result(_result), kind(_kind) {}
+
+  public:
+    bool result;
+
+    std::optional<std::string> kind;
+
+    static AddResult True() { return AddResult(true, std::nullopt); }
+
+    static AddResult False(std::string _kind) {
+      return AddResult(false, _kind);
     }
-}
+
+    std::string getKind() { return kind.value_or(""); }
+  };
+
+  std::string get_name(std::string item);
+
+  bool has_type(const std::string name, const bool full_name);
+
+  AddResult add_type(const std::string self_name);
+
+  void close_type();
+
+  bool has_box(const std::string name, const bool full_name);
+
+  AddResult add_box(const std::string self_name);
+
+  void close_box();
+
+  bool has_lib(const std::string name, const bool full_name);
+
+  AddResult add_lib(const std::string self_name);
+
+  void close_lib();
+
+  void throw_error(const std::string message,
+                   const utils::FilePlacement placement);
+};
+} // namespace IR
+} // namespace qat
 
 #endif

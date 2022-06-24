c829dba0359200ce0512c203596852d2883abb12
Aldrin Mathew <admin@aldrinmathew.com>
2022-06-25 00:20:15 +0530

    Changed `CoreType` to `NamedType`
    - CoreType is not required in the type retrieval part of the AST and
      also doesn't make sense, since other types can also be referred to by
      names
    
    - NamedType will now look up the name of the type in the new IR system
      and retrieve the appropriate type. It can be a core type, sum type,
      type definition or anything else that's a valid type

---
+++ src/AST/types/named.cpp
@@ -0,0 +1,25 @@
+#include "./named.hpp"
+
+namespace qat {
+namespace AST {
+
+NamedType::NamedType(const std::string _name, const bool _variable,
+                     const utils::FilePlacement _filePlacement)
+    : name(_name), QatType(_variable, _filePlacement) {}
+
+llvm::Type *qat::AST::NamedType::generate(qat::IR::Generator *generator) {
+  // FIXME - Support sum types, other kinds of named types and type definitions
+  auto structType = llvm::StructType::getTypeByName(generator->llvmContext,
+                                                    llvm::StringRef(name));
+  if (structType == nullptr) {
+    generator->throw_error("Type " + name + " cannot be found", filePlacement);
+  }
+  return structType;
+}
+
+std::string NamedType::get_name() const { return name; }
+
+TypeKind NamedType::typeKind() { return TypeKind::Float; }
+
+} // namespace AST
+} // namespace qat
---
+++ src/AST/types/named.hpp
@@ -0,0 +1,67 @@
+#ifndef QAT_AST_TYPES_NAMED_HPP
+#define QAT_AST_TYPES_NAMED_HPP
+
+#include "../../IR/generator.hpp"
+#include "../box.hpp"
+#include "../function_definition.hpp"
+#include "./qat_type.hpp"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Type.h"
+#include <string>
+#include <vector>
+
+namespace qat {
+namespace AST {
+
+/**
+ * @brief NamedType is a type, usually a core type, that can be identified by a
+ * name
+ *
+ */
+class NamedType : public QatType {
+private:
+  /**
+   * @brief Name of the type
+   *
+   */
+  std::string name;
+
+public:
+  /**
+   * @brief NamedType is a type, usually a core type, that can be identified by
+   * a name
+   *
+   * @param _name Name of the type
+   * @param _filePlacement
+   */
+  NamedType(const std::string _name, const bool _variable,
+            const utils::FilePlacement _filePlacement);
+
+  /**
+   * @brief This is the code generator function that handles the generation of
+   * LLVM IR
+   *
+   * @param generator The IR::Generator instance that handles LLVM IR Generation
+   * @return llvm::Type*
+   */
+  llvm::Type *generate(IR::Generator *generator);
+
+  /**
+   * @brief Get the name of the type
+   *
+   * @return std::string
+   */
+  std::string get_name() const;
+
+  /**
+   * @brief TypeKind is used to detect variants of the QatType
+   *
+   * @return TypeKind
+   */
+  TypeKind typeKind();
+};
+
+} // namespace AST
+} // namespace qat
+
+#endif

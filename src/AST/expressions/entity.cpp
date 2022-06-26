1e8b4215f15cea682ad4d9f7b55e1114b0eaf47f
Aldrin Mathew <admin@aldrinmathew.com>
2022-06-27 00:07:19 +0530

    Entity updated to support references & kind
    - Entity has been updated to support references and expression kinds
    
    - If the entity is a reference/pointer, if the expression is supposed to
      be assignable, then the reference/pointer is returned.
    
    - If the expression is not supposed to be assignable, and if entity is a
      reference, then the value of the pointee of the reference is loaded

--- src/AST/expressions/entity.cpp
+++ src/AST/expressions/entity.cpp
@@ -1,53 +1,7 @@
-/**
- * Qat Programming Language : Copyright 2022 : Aldrin Mathew
- *
- * AAF INSPECTABLE LICENCE - 1.0
- *
- * This project is licensed under the AAF Inspectable Licence 1.0.
- * You are allowed to inspect the source of this project(s) free of
- * cost, and also to verify the authenticity of the product.
- *
- * Unless required by applicable law, this project is provided
- * "AS IS", WITHOUT ANY WARRANTIES OR PROMISES OF ANY KIND, either
- * expressed or implied. The author(s) of this project is not
- * liable for any harms, errors or troubles caused by using the
- * source or the product, unless implied by law. By using this
- * project, or part of it, you are acknowledging the complete terms
- * and conditions of licensing of this project as specified in AAF
- * Inspectable Licence 1.0 available at this URL:
- *
- * https://github.com/aldrinsartfactory/InspectableLicence/
- *
- * This project may contain parts that are not licensed under the
- * same licence. If so, the licences of those parts should be
- * appropriately mentioned in those parts of the project. The
- * Author MAY provide a notice about the parts of the project that
- * are not licensed under the same licence in a publicly visible
- * manner.
- *
- * You are NOT ALLOWED to sell, or distribute THIS project, its
- * contents, the source or the product or the build result of the
- * source under commercial or non-commercial purposes. You are NOT
- * ALLOWED to revamp, rebrand, refactor, modify, the source, product
- * or the contents of this project.
- *
- * You are NOT ALLOWED to use the name, branding and identity of this
- * project to identify or brand any other project. You ARE however
- * allowed to use the name and branding to pinpoint/show the source
- * of the contents/code/logic of any other project. You are not
- * allowed to use the identification of the Authors of this project
- * to associate them to other projects, in a way that is deceiving
- * or misleading or gives out false information.
- */
-
 #include "./entity.hpp"
 
 namespace qat {
 namespace AST {
-
-ReferenceEntity Entity::to_reference() {
-  return ReferenceEntity(name, file_placement);
-}
 
 llvm::Value *Entity::generate(IR::Generator *generator) {
   auto func = generator->builder.GetInsertBlock()->getParent();
@@ -71,14 +25,31 @@ llvm::Value *Entity::generate(IR::Generator *generator) {
     }
   }
   if (var_alloca) {
-    var_load = generator->builder.CreateLoad(var_alloca->getType(), var_alloca,
-                                             false, name);
-    if (utils::PointerKind::is_reference(var_alloca)) {
-      var_load = generator->builder.CreateLoad(var_load->getType(), var_load,
-                                               false, name);
+    if (isExpectedKind(ExpressionKind::assignable)) {
+      if (utils::PointerKind::is_reference(var_alloca)) {
+        auto result = generator->builder.CreateLoad(var_alloca, name);
+        utils::Variability::propagate(generator->llvmContext, var_alloca,
+                                      result);
+        return result;
+      } else {
+        if (!(var_alloca->getAllocatedType()->isPointerTy())) {
+          generator->throw_error(
+              "This entity is not a pointer and hence is not assignable",
+              file_placement);
+        }
+        return var_alloca;
+      }
+    } else {
+      var_load = generator->builder.CreateLoad(var_alloca->getType(),
+                                               var_alloca, false, name);
+      if (utils::PointerKind::is_reference(var_alloca)) {
+        var_load = generator->builder.CreateLoad(var_load->getType(), var_load,
+                                                 false, name);
+      }
+      utils::Variability::propagate(generator->llvmContext, var_alloca,
+                                    var_load);
+      return var_load;
     }
-    utils::Variability::propagate(generator->llvmContext, var_alloca, var_load);
-    return var_load;
   } else {
     for (std::size_t i = 0; i < func->arg_size(); i++) {
       auto candidate = func->getArg(i);
@@ -88,37 +59,86 @@ llvm::Value *Entity::generate(IR::Generator *generator) {
       }
     }
     if (arg_val) {
-      var_load = generator->builder.CreateLoad(arg_val->getType(), arg_val,
-                                               false, name);
-      if (utils::PointerKind::is_reference(arg_val)) {
-        var_load = generator->builder.CreateLoad(var_load->getType(), var_load,
-                                                 false, name);
-      }
-      utils::Variability::propagate(generator->llvmContext, arg_val, var_load);
-      return var_load;
-    } else {
-      auto globalVariable = generator->get_global_variable(name);
-      if (globalVariable) {
-        var_load = generator->builder.CreateLoad(globalVariable->getType(),
-                                                 globalVariable, false, name);
-        if (utils::PointerKind::is_reference(globalVariable)) {
+      if (isExpectedKind(ExpressionKind::assignable)) {
+        if (utils::PointerKind::is_reference(arg_val)) {
+          auto result = generator->builder.CreateLoad(arg_val, name);
+          utils::Variability::propagate(generator->llvmContext, arg_val,
+                                        result);
+          return result;
+        } else {
+          if (!(arg_val->getType()->isPointerTy())) {
+            generator->throw_error(
+                "This entity is not a pointer and hence is not assignable",
+                file_placement);
+          }
+          return arg_val;
+        }
+      } else {
+        var_load = generator->builder.CreateLoad(arg_val, name);
+        if (utils::PointerKind::is_reference(arg_val)) {
           var_load = generator->builder.CreateLoad(var_load->getType(),
                                                    var_load, false, name);
         }
-        utils::Variability::set(generator->llvmContext, var_load,
-                                globalVariable->isConstant());
+        utils::Variability::propagate(generator->llvmContext, arg_val,
+                                      var_load);
         return var_load;
+      }
+    } else {
+      auto globalVariable = generator->get_global_variable(name);
+      if (globalVariable) {
+        if (isExpectedKind(ExpressionKind::assignable)) {
+          if (utils::PointerKind::is_reference(globalVariable)) {
+            auto result = generator->builder.CreateLoad(globalVariable, name);
+            utils::Variability::propagate(generator->llvmContext,
+                                          globalVariable, result);
+            return result;
+          } else {
+            if (!(globalVariable->getValueType()->isPointerTy())) {
+              generator->throw_error(
+                  "This entity is not a pointer and hence is not assignable",
+                  file_placement);
+            }
+            return globalVariable;
+          }
+        } else {
+          var_load = generator->builder.CreateLoad(globalVariable->getType(),
+                                                   globalVariable, false, name);
+          if (utils::PointerKind::is_reference(globalVariable)) {
+            var_load = generator->builder.CreateLoad(var_load->getType(),
+                                                     var_load, false, name);
+          }
+          utils::Variability::set(generator->llvmContext, var_load,
+                                  globalVariable->isConstant());
+          return var_load;
+        }
       } else {
         for (std::size_t i = 0; i < generator->exposed.size(); i++) {
           auto space_name = generator->exposed.at(i).generate() + name;
           globalVariable = generator->get_global_variable(space_name);
           if (globalVariable) {
-            var_load = generator->builder.CreateLoad(
-                globalVariable->getValueType(), globalVariable, false,
-                llvm::Twine(space_name));
-            utils::Variability::set(generator->llvmContext, var_load,
-                                    globalVariable->isConstant());
-            return var_load;
+            if (isExpectedKind(ExpressionKind::assignable)) {
+              if (utils::PointerKind::is_reference(globalVariable)) {
+                auto result =
+                    generator->builder.CreateLoad(globalVariable, space_name);
+                utils::Variability::propagate(generator->llvmContext,
+                                              globalVariable, result);
+                return result;
+              } else {
+                if (!(globalVariable->getValueType()->isPointerTy())) {
+                  generator->throw_error("This entity is not a pointer and "
+                                         "hence is not assignable",
+                                         file_placement);
+                }
+                return globalVariable;
+              }
+            } else {
+              var_load = generator->builder.CreateLoad(
+                  globalVariable->getValueType(), globalVariable, false,
+                  llvm::Twine(space_name));
+              utils::Variability::set(generator->llvmContext, var_load,
+                                      globalVariable->isConstant());
+              return var_load;
+            }
           }
         }
       }
@@ -126,7 +146,7 @@ llvm::Value *Entity::generate(IR::Generator *generator) {
       generator->throw_error("Entity `" + name +
                                  "` cannot be found in function `" +
                                  e_block.getParent()->getName().str() +
-                                 "` and is not a Global Entity",
+                                 "` and is not a global or static value",
                              file_placement);
     }
   }
--- src/AST/expressions/entity.hpp
+++ src/AST/expressions/entity.hpp
@@ -1,45 +1,3 @@
-/**
- * Qat Programming Language : Copyright 2022 : Aldrin Mathew
- *
- * AAF INSPECTABLE LICENCE - 1.0
- *
- * This project is licensed under the AAF Inspectable Licence 1.0.
- * You are allowed to inspect the source of this project(s) free of
- * cost, and also to verify the authenticity of the product.
- *
- * Unless required by applicable law, this project is provided
- * "AS IS", WITHOUT ANY WARRANTIES OR PROMISES OF ANY KIND, either
- * expressed or implied. The author(s) of this project is not
- * liable for any harms, errors or troubles caused by using the
- * source or the product, unless implied by law. By using this
- * project, or part of it, you are acknowledging the complete terms
- * and conditions of licensing of this project as specified in AAF
- * Inspectable Licence 1.0 available at this URL:
- *
- * https://github.com/aldrinsartfactory/InspectableLicence/
- *
- * This project may contain parts that are not licensed under the
- * same licence. If so, the licences of those parts should be
- * appropriately mentioned in those parts of the project. The
- * Author MAY provide a notice about the parts of the project that
- * are not licensed under the same licence in a publicly visible
- * manner.
- *
- * You are NOT ALLOWED to sell, or distribute THIS project, its
- * contents, the source or the product or the build result of the
- * source under commercial or non-commercial purposes. You are NOT
- * ALLOWED to revamp, rebrand, refactor, modify, the source, product
- * or the contents of this project.
- *
- * You are NOT ALLOWED to use the name, branding and identity of this
- * project to identify or brand any other project. You ARE however
- * allowed to use the name and branding to pinpoint/show the source
- * of the contents/code/logic of any other project. You are not
- * allowed to use the identification of the Authors of this project
- * to associate them to other projects, in a way that is deceiving
- * or misleading or gives out false information.
- */
-
 #ifndef QAT_AST_EXPRESSIONS_ENTITY_HPP
 #define QAT_AST_EXPRESSIONS_ENTITY_HPP
 
@@ -48,7 +6,6 @@
 #include "../../utils/variability.hpp"
 #include "../expression.hpp"
 #include "../node_type.hpp"
-#include "./reference_entity.hpp"
 #include "llvm/IR/Value.h"
 #include <string>
 
@@ -56,7 +13,7 @@ namespace qat {
 namespace AST {
 
 /**
- * @brief Entity represents either a variable or constant. The name of the
+ *  Entity represents either a variable or constant. The name of the
  * entity is mostly just an identifier, but it can be other values if the
  * entity is present in the global constant
  *
@@ -65,14 +22,14 @@ class Entity : public Expression {
 
 private:
   /**
-   * @brief Name of the entity
+   *  Name of the entity
    *
    */
   std::string name;
 
 public:
   /**
-   * @brief Entity represents either a variable or constant. The name of the
+   *  Entity represents either a variable or constant. The name of the
    * entity is mostly just an identifier, but it can be other values if the
    * entity is present in the global constant
    *
@@ -84,14 +41,7 @@ public:
       : name(_name), Expression(_filePlacement) {}
 
   /**
-   * @brief Convert this entity to a ReferenceEntity
-   *
-   * @return ReferenceEntity
-   */
-  ReferenceEntity to_reference();
-
-  /**
-   * @brief This is the code generator function that handles the generation of
+   *  This is the code generator function that handles the generation of
    * LLVM IR
    *
    * @param generator The IR::Generator instance that handles LLVM IR Generation
@@ -100,7 +50,7 @@ public:
   llvm::Value *generate(IR::Generator *generator);
 
   /**
-   * @brief Type of the node represented by this AST member
+   *  Type of the node represented by this AST member
    *
    * @return NodeType
    */

b542052cd8f3f18096147b66d2c557c29f19c6cd
Aldrin Mathew <admin@aldrinmathew.com>
2022-06-26 23:16:17 +0530

    Added `utils::Visibility` and helper types
    - Added utils::Visibility to support visibility for different entities
      in the IR
    
    - The default visibility is `private to the parent entity`

---
+++ src/utils/visibility.cpp
@@ -0,0 +1,207 @@
+#include "./visibility.hpp"
+
+namespace qat {
+namespace utils {
+
+#define VISIB_KIND "visibility_kind"
+#define VISIB_VALUE "visibility_value"
+
+bool RequesterInfo::has_access_to_box(const std::string other) const {
+  return box.has_value() ? ((other == box.value()) ||
+                            (other.find(box.value()) != std::string::npos))
+                         : false;
+}
+
+bool RequesterInfo::has_access_to_lib(const std::string other) const {
+  return lib.has_value() ? ((other == lib.value()) ||
+                            (other.find(lib.value()) != std::string::npos))
+                         : false;
+}
+
+bool RequesterInfo::is_same_file(const std::string other) const {
+  return file == other;
+}
+
+bool RequesterInfo::is_same_type(const std::string other) const {
+  return type.has_value() ? (type.value() == other) : false;
+}
+
+const std::map<VisibilityKind, std::string> Visibility::kind_value_map = {
+    {VisibilityKind::pub, "public"},
+    {VisibilityKind::type, "type"},
+    {VisibilityKind::lib, "library"},
+    {VisibilityKind::file, "file"},
+    {VisibilityKind::box, "box"}};
+
+const std::map<std::string, VisibilityKind> Visibility::value_kind_map = {
+    {"public", VisibilityKind::pub},
+    {"type", VisibilityKind::type},
+    {"library", VisibilityKind::lib},
+    {"file", VisibilityKind::file},
+    {"box", VisibilityKind::box}};
+
+llvm::GlobalValue::LinkageTypes
+Visibility::get_linkage(const VisibilityKind kind) {
+  switch (kind) {
+  case VisibilityKind::pub:
+  case VisibilityKind::file:
+  case VisibilityKind::box: {
+    return llvm::GlobalValue::LinkageTypes::ExternalLinkage;
+  }
+  case VisibilityKind::lib:
+  case VisibilityKind::type: {
+    return llvm::GlobalValue::LinkageTypes::PrivateLinkage;
+  }
+  }
+}
+
+llvm::GlobalValue::VisibilityTypes
+Visibility::get_llvm_visibility(const VisibilityKind kind) {
+  switch (kind) {
+  case VisibilityKind::pub:
+  case VisibilityKind::file:
+  case VisibilityKind::box: {
+    return llvm::GlobalValue::VisibilityTypes::DefaultVisibility;
+  }
+  case VisibilityKind::lib:
+  case VisibilityKind::type: {
+    return llvm::GlobalValue::VisibilityTypes::HiddenVisibility;
+  }
+  }
+}
+
+void Visibility::set(llvm::GlobalVariable *global_var,
+                     const VisibilityInfo access_info,
+                     llvm::LLVMContext &context) {
+  global_var->setLinkage(get_linkage(access_info.kind));
+  global_var->setMetadata(
+      VISIB_KIND,
+      llvm::MDNode::get(
+          context,
+          llvm::MDString::get(context, kind_value_map.at(access_info.kind))));
+  global_var->setMetadata(
+      VISIB_VALUE, llvm::MDNode::get(context, llvm::MDString::get(
+                                                  context, access_info.value)));
+}
+
+void Visibility::set(llvm::Function *function, const VisibilityInfo access_info,
+                     llvm::LLVMContext &context) {
+  function->setLinkage(get_linkage(access_info.kind));
+  function->setMetadata(
+      VISIB_KIND,
+      llvm::MDNode::get(
+          context,
+          llvm::MDString::get(context, kind_value_map.at(access_info.kind))));
+  function->setMetadata(
+      VISIB_VALUE, llvm::MDNode::get(context, llvm::MDString::get(
+                                                  context, access_info.value)));
+}
+
+void Visibility::set(llvm::Module *mod, const VisibilityInfo access_info,
+                     llvm::LLVMContext &context) {
+
+  auto kind_mdnode = llvm::MDNode::get(
+      context,
+      llvm::MDString::get(context, kind_value_map.at(access_info.kind)));
+  auto kind_named_md = mod->getOrInsertNamedMetadata(VISIB_KIND);
+  kind_named_md->addOperand(kind_mdnode);
+  auto value_mdnode = llvm::MDNode::get(
+      context, llvm::MDString::get(context, access_info.value));
+  auto value_named_md = mod->getOrInsertNamedMetadata(VISIB_VALUE);
+  value_named_md->addOperand(value_mdnode);
+}
+
+void Visibility::set(std::string name, std::string type,
+                     const VisibilityInfo visibility_info,
+                     llvm::LLVMContext &context) {
+  auto name_mdnode =
+      llvm::MDNode::get(context, llvm::MDString::get(context, name));
+  // FIXME
+}
+
+VisibilityKind Visibility::get_kind(llvm::GlobalVariable *var) {
+  auto kind = llvm::dyn_cast<llvm::MDString>(
+                  var->getMetadata(VISIB_KIND)->getOperand(0))
+                  ->getString()
+                  .str();
+  return value_kind_map.at(kind);
+}
+
+VisibilityKind Visibility::get_kind(llvm::Function *func) {
+  auto kind = llvm::dyn_cast<llvm::MDString>(
+                  func->getMetadata(VISIB_KIND)->getOperand(0))
+                  ->getString()
+                  .str();
+  return value_kind_map.at(kind);
+}
+
+VisibilityKind Visibility::get_kind(llvm::Module *mod) {
+  auto kind = llvm::dyn_cast<llvm::MDString>(
+                  mod->getNamedMetadata(VISIB_KIND)->getOperand(0))
+                  ->getString()
+                  .str();
+  return value_kind_map.at(kind);
+}
+
+VisibilityKind Visibility::get_kind(std::string name) {
+  // FIXME
+}
+
+std::string Visibility::get_value(llvm::GlobalVariable *var) {
+  return llvm::dyn_cast<llvm::MDString>(
+             var->getMetadata(VISIB_VALUE)->getOperand(0))
+      ->getString()
+      .str();
+}
+
+std::string Visibility::get_value(llvm::Function *func) {
+  return llvm::dyn_cast<llvm::MDString>(
+             func->getMetadata(VISIB_VALUE)->getOperand(0))
+      ->getString()
+      .str();
+}
+
+std::string Visibility::get_value(llvm::Module *mod) {
+  return llvm::dyn_cast<llvm::MDString>(
+             mod->getNamedMetadata(VISIB_VALUE)->getOperand(0))
+      ->getString()
+      .str();
+}
+
+bool Visibility::isAccessible(const VisibilityInfo visibility,
+                              const RequesterInfo req_info) {
+  switch (visibility.kind) {
+  case VisibilityKind::box: {
+    if (req_info.box.has_value()) {
+      return (req_info.box.value().find(visibility.value) == 0);
+    } else {
+      return false;
+    }
+  }
+  case VisibilityKind::file: {
+    return (visibility.value == req_info.file);
+  }
+  case VisibilityKind::folder: {
+    return (req_info.file.find(visibility.value) == 0);
+  }
+  case VisibilityKind::lib: {
+    if (req_info.lib.has_value()) {
+      return (req_info.lib.value().find(visibility.value) == 0);
+    } else {
+      return false;
+    }
+  }
+  case VisibilityKind::pub: {
+    return true;
+  }
+  case VisibilityKind::type: {
+    if (req_info.type.has_value()) {
+      return (req_info.type.value().find(visibility.value) == 0);
+    } else {
+      return false;
+    }
+  }
+  }
+}
+} // namespace utils
+} // namespace qat
---
+++ src/utils/visibility.hpp
@@ -0,0 +1,364 @@
+#ifndef QAT_UTILS_VISIBILITY_HPP
+#define QAT_UTILS_VISIBILITY_HPP
+
+#include "llvm/IR/Function.h"
+#include "llvm/IR/GlobalValue.h"
+#include "llvm/IR/GlobalVariable.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Metadata.h"
+#include "llvm/IR/Module.h"
+#include <map>
+#include <optional>
+
+namespace qat {
+namespace utils {
+
+/**
+ *  Visibility
+ *
+ * Libraries, Boxes, Types, Global Variables, Global & Static Functions
+ * will always have a visibility associated with them.
+ *
+ * Visibility for Libraries, Global Variables and Functions are stored directly
+ * in LLVM IR using metadata. Since boxes and types are constructs of the
+ * language, those have to be handled separately.
+ *
+ *
+ *
+ */
+
+/**
+ *  VisibilityKind tells where a type, global variable,
+ * function, library or box can be accessed
+ *
+ */
+enum class VisibilityKind {
+  // TODO - Think about extension functions and how this should behave for those
+  /**
+   *  Visibility is public only inside the parent type
+   *
+   */
+  type,
+  /**
+   *  Visibility is public, but will attain the most strict visibility of
+   * any of its parent entities
+   *
+   */
+  pub,
+  /**
+   *  Visibility is public only inside the parent library
+   *
+   */
+  lib,
+  /**
+   *  Visibility is public only inside the parent file
+   *
+   */
+  file,
+  /**
+   *  Visibility is public only inside the parent directory/folder
+   *
+   */
+  folder,
+  /**
+   *  Visibility is public only inside the parent box
+   *
+   */
+  box
+};
+
+/**
+ *  VisibilityInfo is used to store details about the visibility kind of
+ * an entity. Some kinds require a value to be complete, others don't.
+ *
+ */
+class VisibilityInfo {
+private:
+  VisibilityInfo(VisibilityKind _kind, std::string _value)
+      : kind(_kind), value(_value) {}
+
+public:
+  /**
+   *  VisibilityInfo with Private kind
+   *
+   * @return VisibilityInfo
+   */
+  static VisibilityInfo type() {
+    return VisibilityInfo(VisibilityKind::type, "");
+  }
+
+  /**
+   *  VisibilityInfo with Public kind
+   *
+   * @return VisibilityInfo
+   */
+  static VisibilityInfo pub() {
+    return VisibilityInfo(VisibilityKind::pub, "");
+  }
+
+  /**
+   *  VisibilityInfo with Library kind
+   *
+   * @param name Name of the library
+   * @return VisibilityInfo
+   */
+  static VisibilityInfo lib(std::string name) {
+    return VisibilityInfo(VisibilityKind::lib, name);
+  }
+
+  /**
+   *  VisibilityInfo with File kind
+   *
+   * @param path Absolute path to the file
+   * @return VisibilityInfo
+   */
+  static VisibilityInfo file(std::string path) {
+    return VisibilityInfo(VisibilityKind::file, path);
+  }
+
+  /**
+   *  VisibilityInfo
+   *
+   * @param path
+   * @return VisibilityInfo
+   */
+  static VisibilityInfo folder(std::string path) {
+    return VisibilityInfo(VisibilityKind::folder, path);
+  }
+
+  /**
+   *  VisibilityInfo with Box kind
+   *
+   * @param name Name of the box
+   * @return VisibilityInfo
+   */
+  static VisibilityInfo box(std::string name) {
+    return VisibilityInfo(VisibilityKind::box, name);
+  }
+
+  /**
+   *  Kind of the Visibility of the entity
+   *
+   */
+  VisibilityKind kind;
+
+  /**
+   *  Value related to the VisibilityKind. If not empty, this is either a
+   * library name, box name or file path.
+   *
+   */
+  std::string value;
+};
+
+/**
+ *  Information about the entity requesting access
+ *
+ */
+class RequesterInfo {
+public:
+  /**
+   *  Parent library of the entity requesting for access.
+   *
+   */
+  std::optional<std::string> lib;
+
+  /**
+   *  Parent box of the entity requesting for access.
+   *
+   */
+  std::optional<std::string> box;
+
+  /**
+   *  Parent file of the entity requesting for access. This will never be
+   * a null-value. This is also used for the parent folder
+   *
+   */
+  std::string file;
+
+  /**
+   *  Parent type of the entity requesting for access.
+   *
+   */
+  std::optional<std::string> type;
+
+  /**
+   *  Whether the requester has access to the lib represented by the
+   * provided name
+   *
+   * @param other The lib to check for
+   * @return true
+   * @return false
+   */
+  bool has_access_to_lib(const std::string other) const;
+
+  /**
+   *  Whether the requester has access to the box represented by the
+   * provided name
+   *
+   * @param other The box to check for
+   * @return true
+   * @return false
+   */
+  bool has_access_to_box(const std::string other) const;
+
+  /**
+   *  Whether the requester's parent file is the same as that of the
+   * entity to be accessed
+   *
+   * @param other File path of the entity to check for
+   * @return true
+   * @return false
+   */
+  bool is_same_file(const std::string other) const;
+
+  /**
+   *  Whether the requester's parent type is the same as that of the
+   * entity to be accessed
+   *
+   * @param other Name of the type of the entity to check for
+   * @return true
+   * @return false
+   */
+  bool is_same_type(const std::string other) const;
+};
+
+/**
+ *  Visibility provides static functions to manage, set and retrieve
+ * access kinds and info related to that
+ *
+ */
+class Visibility {
+private:
+  /**
+   *  VisibilityKind, VisibilityValue
+   *
+   */
+  static const std::map<VisibilityKind, std::string> kind_value_map;
+
+  /**
+   *  VisibilityValue, VisibilityKind
+   *
+   */
+  static const std::map<std::string, VisibilityKind> value_kind_map;
+
+  /**
+   *  Get the appropriate linkage for an entity from its VisibilityKind
+   *
+   * @param kind VisibilityKind value
+   * @return llvm::GlobalValue::LinkageTypes
+   */
+  static llvm::GlobalValue::LinkageTypes get_linkage(const VisibilityKind kind);
+
+  /**
+   *  Get the LLVM VisibilityTypes value of an entity from its
+   * VisibilityKind
+   *
+   * @param kind VisibilityKind value
+   * @return llvm::GlobalValue::VisibilityTypes
+   */
+  static llvm::GlobalValue::VisibilityTypes
+  get_llvm_visibility(const VisibilityKind kind);
+
+  /**
+   *  Get the VisibilityKind of the provided GlobalVariable
+   *
+   * @param var The GlobalVariable to find the value of
+   * @return VisibilityKind
+   */
+  static VisibilityKind get_kind(llvm::GlobalVariable *var);
+
+  /**
+   *  Get the VisibilityKind of the provided Function
+   *
+   * @param func The Function to find the value of
+   * @return VisibilityKind
+   */
+  static VisibilityKind get_kind(llvm::Function *func);
+
+  /**
+   *  Get the VisibilityKind value of the provided Module
+   *
+   * @param mod The GlobalVariable to find the value of
+   * @return VisibilityKind
+   */
+  static VisibilityKind get_kind(llvm::Module *mod);
+
+  /**
+   *  Get the VisibilityKind value of an entity with the provided name. It
+   * can be a lib, box or type
+   *
+   * @param name The Name of the entity to find the value of
+   * @return VisibilityKind
+   */
+  static VisibilityKind get_kind(std::string name);
+
+  /**
+   *  Get the visibility value of the provided GlobalVariable
+   *
+   * @param var
+   * @return std::string
+   */
+  static std::string get_value(llvm::GlobalVariable *var);
+
+  /**
+   *  Get the visibility value of the provided Function
+   *
+   * @param func
+   * @return std::string
+   */
+  static std::string get_value(llvm::Function *func);
+
+  /**
+   *  Get the visibility value of the provided Module
+   *
+   * @param mod
+   * @return std::string
+   */
+  static std::string get_value(llvm::Module *mod);
+
+  /**
+   *  Get the visibility value of an entity with the provided name. It
+   * can be a lib, box or type
+   *
+   * @param name The Name of the entity to find the value of
+   * @return std::string
+   */
+  static std::string get_value(std::string name);
+
+public:
+  /**
+   *  Set the visibility of the provided GlobalVariable
+   *
+   * @param var
+   * @param info
+   * @param context
+   */
+  static void set(llvm::GlobalVariable *var, const VisibilityInfo info,
+                  llvm::LLVMContext &context);
+
+  static void set(llvm::Function *func, const VisibilityInfo info,
+                  llvm::LLVMContext &context);
+
+  static void set(llvm::Module *mod, const VisibilityInfo info,
+                  llvm::LLVMContext &context);
+
+  static void set(std::string name, std::string type, const VisibilityInfo info,
+                  llvm::LLVMContext &context);
+
+  /**
+   *  Compare the provided visibility and requester-info and see if the
+   * entity whose visibility is provided, can be accessed by the requester
+   *
+   * @param vinfo VisibilityInfo of the entity that has to be accessed
+   * @param reqinfo RequesterInfo of the part that is trying to access the value
+   * @return true If it is accessible
+   * @return false If it is not accessible
+   */
+  static bool isAccessible(const VisibilityInfo vinfo,
+                           const RequesterInfo reqinfo);
+};
+
+} // namespace utils
+} // namespace qat
+
+#endif

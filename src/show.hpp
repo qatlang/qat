0453e2bdf45dbc1922db7e2c3401bbdccdd3e087
Aldrin Mathew <admin@aldrinmathew.com>
2022-06-27 18:05:16 +0530

    Added a proper debug print system
    - Added SHOW macro that displays messages only if the build is in debug
      mode
    
    - If the build is in Release mode, the macro is replaced with a blank
      line
    
    - Debugging using LLDB has already been setup, however, sometimes
      nothing can beat a print statement

---
+++ src/show.hpp
@@ -0,0 +1,12 @@
+#ifndef QAT_SHOW_HPP
+#define QAT_SHOW_HPP
+
+#include "./CLI/color.hpp"
+
+#if NDEBUG
+#define SHOW(val)
+#else
+#define SHOW(val) std::cout << colors::blue << val << colors::reset << "\n";
+#endif
+
+#endif

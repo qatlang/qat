2153faf4f0b87c59d15b797891817f65fe5c2bb0
Aldrin Mathew <admin@aldrinmathew.com>
2022-06-27 16:41:37 +0530

    Added `toJSON` function in `qat::AST::Node`
    - This is in preparation of the JSON backend of the language

--- src/AST/node.hpp
+++ src/AST/node.hpp
@@ -1,6 +1,7 @@
 #ifndef QAT_AST_NODE_HPP
 #define QAT_AST_NODE_HPP
 
+#include "../Backend/JSON/json.hpp"
 #include "../IR/generator.hpp"
 #include "../utils/file_placement.hpp"
 #include "./errors.hpp"
@@ -11,7 +12,7 @@ namespace qat {
 namespace AST {
 
 /**
- * @brief Node is the base class for all AST members of the language and it
+ *  Node is the base class for all AST members of the language and it
  * requires a FilePlacement instance that indicates its position in the
  * corresponding file
  *
@@ -19,7 +20,7 @@ namespace AST {
 class Node {
 public:
   /**
-   * @brief Node is the base class for all AST members of the language and it
+   *  Node is the base class for all AST members of the language and it
    * requires a FilePlacement instance that indicates its position in the
    * corresponding file
    *
@@ -31,7 +32,7 @@ public:
   virtual ~Node(){};
 
   /**
-   * @brief This is the code generator function that handles the generation of
+   *  This is the code generator function that handles the generation of
    * LLVM IR
    *
    * @param generator The IR::Generator instance that handles LLVM IR Generation
@@ -40,14 +41,20 @@ public:
   virtual llvm::Value *generate(IR::Generator *generator){};
 
   /**
-   * @brief Type of the node represented by this AST member
+   * This is the code generator function that handles the generation of JSON
+   *
+   */
+  virtual backend::JSON toJSON(){};
+
+  /**
+   *  Type of the node represented by this AST member
    *
    * @return NodeType
    */
   virtual NodeType nodeType(){};
 
   /**
-   * @brief A range present in a file that represents the placement spanned by
+   *  A range present in a file that represents the placement spanned by
    * the tokens that made up this AST member
    *
    */

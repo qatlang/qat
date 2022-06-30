c1929bf3e1f3e1c2d005bf9467b3bc201d91c7b8
Aldrin Mathew <admin@aldrinmathew.com>
2022-06-30 20:35:35 +0530

    Updated `Assignment` to support expressions
    - Assignments previously worked only for symbols, and hence didn't allow
      reference and pointers as the left hand side
    
    - This has been fixed
    
    - Added JSON converter for Assignment

--- src/AST/assignment.cpp
+++ src/AST/assignment.cpp
@@ -1,122 +1,64 @@
-/**
- * Qat Programming Language : Copyright 2022 : Aldrin Mathew
- *
- * AAF INSPECTABLE LICENCE - 1.0
- *
- * This project is licensed under the AAF Inspectable Licence 1.0.
- * You are allowed to inspect the source of this project(s) free of
- * cost, and also to verify the authenticity of the product.
- *
- * Unless required by applicable law, this project is provided
- * "AS IS", WITHOUT ANY WARRANTIES OR PROMISES OF ANY KIND, either
- * expressed or implied. The author(s) of this project is not
- * liable for any harms, errors or troubles caused by using the
- * source or the product, unless implied by law. By using this
- * project, or part of it, you are acknowledging the complete terms
- * and conditions of licensing of this project as specified in AAF
- * Inspectable Licence 1.0 available at this URL:
- *
- * https://github.com/aldrinsartfactory/InspectableLicence/
- *
- * This project may contain parts that are not licensed under the
- * same licence. If so, the licences of those parts should be
- * appropriately mentioned in those parts of the project. The
- * Author MAY provide a notice about the parts of the project that
- * are not licensed under the same licence in a publicly visible
- * manner.
- *
- * You are NOT ALLOWED to sell, or distribute THIS project, its
- * contents, the source or the product or the build result of the
- * source under commercial or non-commercial purposes. You are NOT
- * ALLOWED to revamp, rebrand, refactor, modify, the source, product
- * or the contents of this project.
- *
- * You are NOT ALLOWED to use the name, branding and identity of this
- * project to identify or brand any other project. You ARE however
- * allowed to use the name and branding to pinpoint/show the source
- * of the contents/code/logic of any other project. You are not
- * allowed to use the identification of the Authors of this project
- * to associate them to other projects, in a way that is deceiving
- * or misleading or gives out false information.
- */
-
 #include "./assignment.hpp"
 
 namespace qat {
 namespace AST {
 
-Assignment::Assignment(std::string _name, Expression _value,
+Assignment::Assignment(Expression *_lhs, Expression *_value,
                        utils::FilePlacement _filePlacement)
-    : name(_name), value(_value), Sentence(_filePlacement) {}
+    : lhs(_lhs), value(_value), Sentence(_filePlacement) {}
 
 llvm::Value *Assignment::generate(qat::IR::Generator *generator) {
-  auto gen_val = value.generate(generator);
-  auto &e_block =
-      generator->builder.GetInsertBlock()->getParent()->getEntryBlock();
-  auto insert_p = e_block.begin();
-  llvm::AllocaInst *var_alloca = nullptr;
-  for (auto &instr : e_block) {
-    if (llvm::isa<llvm::AllocaInst>(instr)) {
-      auto tmp_alloc = llvm::dyn_cast<llvm::AllocaInst>(&instr);
-      if (tmp_alloc->hasName()) {
-        auto var_name = tmp_alloc->getName();
-        if (var_name.str() == name) {
-          var_alloca = tmp_alloc;
-          break;
-        }
+  lhs->setExpectedKind(ExpressionKind::assignable);
+  auto lhs_val = lhs->generate(generator);
+  bool is_reference = false;
+  llvm::Type *lhs_type = nullptr;
+  if (llvm::isa<llvm::Instruction>(lhs_val)) {
+    bool isAlloca = llvm::isa<llvm::AllocaInst>(lhs_val);
+    auto inst = isAlloca ? llvm::dyn_cast<llvm::AllocaInst>(lhs_val)
+                         : llvm::dyn_cast<llvm::Instruction>(lhs_val);
+    if (utils::Variability::get(inst)) {
+      if (isAlloca) {
+        lhs_type =
+            llvm::dyn_cast<llvm::AllocaInst>(lhs_val)->getAllocatedType();
       }
+      is_reference = utils::PointerKind::is_reference(inst);
     } else {
-      break;
+      Errors::AST1(lhs_val->getName().str(), lhs->file_placement);
     }
-  }
-  llvm::StoreInst *var_store = nullptr;
-  if (var_alloca) {
-    if (utils::Variability::get(var_alloca)) {
-      gen_val = utils::cast_if_null_pointer(var_alloca, gen_val);
-      if (var_alloca->getType() != gen_val->getType()) {
-        generator->throw_error(
-            "The variable `" + name + "` is of the type `" +
-                utils::llvmTypeToName(var_alloca->getType()) +
-                "`, but the provided expression is of the type `" +
-                utils::llvmTypeToName(gen_val->getType()) + "`",
-            value.file_placement);
-      }
-      var_store = generator->builder.CreateStore(gen_val, var_alloca, false);
+  } else if (llvm::isa<llvm::GlobalVariable>(lhs_val)) {
+    auto gvar = llvm::dyn_cast<llvm::GlobalVariable>(lhs_val);
+    if (utils::Variability::get(gvar)) {
+      lhs_type = gvar->getValueType();
+      is_reference = utils::PointerKind::is_reference(gvar);
     } else {
-      generator->throw_error(
-          name + " is not a variable value and cannot be reassigned",
-          file_placement);
-    }
-  } else {
-    auto global_var = generator->get_global_variable(name);
-    if (global_var) {
-      if (!global_var->isConstant()) {
-        gen_val = utils::cast_if_null_pointer(global_var, gen_val);
-        if (global_var->getValueType() != gen_val->getType()) {
-          generator->throw_error(
-              "The global variable " + name + " is of the type `" +
-                  utils::llvmTypeToName(global_var->getValueType()) +
-                  "`, but the provided expression is of the type `" +
-                  utils::llvmTypeToName(gen_val->getType()) + "`",
-              value.file_placement);
-        }
-        var_store = generator->builder.CreateStore(gen_val, global_var, false);
-      } else {
-        generator->throw_error(
-            "Global entity " + name +
-                " is not a variable and cannot be reassigned.",
-            file_placement);
-      }
+      Errors::AST2(true, lhs_val->getName().str(), file_placement);
     }
   }
-  if (var_store) {
-    return var_store;
-  } else {
-    generator->throw_error("The name `" + name + "` not found in function " +
-                               e_block.getParent()->getName().str() +
-                               " and is not a Global Variable",
-                           file_placement);
+  if (!lhs_type) {
+    lhs_type = lhs_val->getType();
   }
+  auto gen_val = value->generate(generator);
+  if (is_reference
+          ? (llvm::dyn_cast<llvm::PointerType>(lhs_type)->getElementType() !=
+             gen_val->getType())
+          : lhs_type != gen_val->getType()) {
+    Errors::AST0(
+        utils::llvmTypeToName(
+            is_reference
+                ? llvm::dyn_cast<llvm::PointerType>(lhs_type)->getElementType()
+                : lhs_type),
+        utils::llvmTypeToName(gen_val->getType()), file_placement);
+  }
+  // TODO - Support copy and move semantics
+  return generator->builder.CreateStore(gen_val, lhs_val, false);
+}
+
+backend::JSON Assignment::toJSON() const {
+  return backend::JSON()
+      ._("nodeType", "assignment")
+      ._("lhs", lhs->toJSON())
+      ._("rhs", value->toJSON())
+      ._("filePlacement", file_placement);
 }
 
 } // namespace AST
--- src/AST/assignment.hpp
+++ src/AST/assignment.hpp
@@ -4,11 +4,13 @@
 #include "../IR/generator.hpp"
 #include "../utils/cast_if_null_pointer.hpp"
 #include "../utils/llvm_type_to_name.hpp"
+#include "../utils/pointer_kind.hpp"
 #include "../utils/variability.hpp"
 #include "./expression.hpp"
 #include "./node_type.hpp"
 #include "./sentence.hpp"
 #include "llvm/IR/Instructions.h"
+#include "llvm/IR/Type.h"
 #include "llvm/IR/Value.h"
 #include <string>
 
@@ -16,54 +18,44 @@ namespace qat {
 namespace AST {
 
 /**
- * @brief Assignment represents storage of a value to a variable in the
+ *  Assignment represents storage of a value to a variable in the
  * language. This will throw errors if the entity to be reassigned is not a
  * variable.
  */
 class Assignment : public Sentence {
 private:
   /**
-   * @brief Name of the variable to which an expression has to be assigned
+   *  The left hand side of the expression
    *
    */
-  std::string name;
+  Expression *lhs;
 
   /**
-   * @brief Expression instance that represents the expression that has to be
+   *  Expression instance that represents the expression that has to be
    * assigned to the variable
    *
    */
-  Expression value;
+  Expression *value;
 
 public:
   /**
-   * @brief Assignment represents storage of a value to a variable in the
+   *  Assignment represents storage of a value to a variable in the
    * language. This will throw errors if the entity to be reassigned is not a
    * variable
    *
-   * @param _name Name of the variable that has to be reassigned
+   * @param _lhs Expression to be reassigned to
    * @param _value The Expression instance that represents the expression that
    * should be assigned to the variable
    * @param _filePlacement FilePLacement instance that represents the range
    * spanned by the tokens making up this AST member
    */
-  Assignment(std::string _name, Expression _value,
+  Assignment(Expression *_lhs, Expression *_value,
              utils::FilePlacement _filePlacement);
 
-  /**
-   * @brief This is the code generator function that handles the generation of
-   * LLVM IR
-   *
-   * @param generator The IR::Generator instance that handles LLVM IR Generation
-   * @return llvm::Value*
-   */
   llvm::Value *generate(IR::Generator *generator);
 
-  /**
-   * @brief Type of the node represented by this AST member
-   *
-   * @return NodeType
-   */
+  backend::JSON toJSON() const;
+
   NodeType nodeType() { return qat::AST::NodeType::reassignment; }
 };
 

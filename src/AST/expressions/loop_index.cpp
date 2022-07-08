891dd9ba20571295a4dfac80c2544f11d2e48fbe
Aldrin Mathew <admin@aldrinmathew.com>
2022-07-08 11:21:23 +0530

    Added `qat::AST::LoopIndex`
    - LoopIndex is an expression used to get the counter of a loop in the
      language with fixed number of runs
    
    - It is not a variable

---
+++ src/AST/expressions/loop_index.cpp
@@ -0,0 +1,20 @@
+#include "./loop_index.hpp"
+
+namespace qat {
+namespace AST {
+
+LoopIndex::LoopIndex(utils::FilePlacement _filePlacement)
+    : Expression(_filePlacement) {}
+
+llvm::Value *LoopIndex::emit(IR::Context *ctx) {
+  // TODO - Implement this
+}
+
+void LoopIndex::emitCPP(backend::cpp::File &file, bool isHeader) const {
+  if (!isHeader) {
+    file += (file.getLoopIndex() + " ");
+  }
+}
+
+} // namespace AST
+} // namespace qat
---
+++ src/AST/expressions/loop_index.hpp
@@ -0,0 +1,23 @@
+#ifndef QAT_AST_EXPRESSIONS_LOOP_INDEX_HPP
+#define QAT_AST_EXPRESSIONS_LOOP_INDEX_HPP
+
+#include "../expression.hpp"
+
+namespace qat {
+namespace AST {
+
+class LoopIndex : public Expression {
+public:
+  LoopIndex(utils::FilePlacement _filePlacement);
+
+  llvm::Value *emit(IR::Context *ctx);
+
+  void emitCPP(backend::cpp::File &file, bool isHeader) const;
+
+  NodeType nodeType() const { return NodeType::loopIndex; }
+};
+
+} // namespace AST
+} // namespace qat
+
+#endif

1bfa89b1c01c766fadfaa4fee64c4bf552dfcfb1
Aldrin Mathew <admin@aldrinmathew.com>
2022-07-08 19:33:04 +0530

    Added `qat::backend::cpp`
    - Added classes to support the C++ backend
    
    - This contains abstractions to represent C++ header and source files
      and also namespaces. Namespaces are not currently used by the current
      C++ emitter function
    
    - Updated build configuration for the backend section

---
+++ src/backend/cpp.cpp
@@ -0,0 +1,142 @@
+#include "./cpp.hpp"
+#include <filesystem>
+#include <fstream>
+#include <ios>
+
+namespace qat {
+namespace backend {
+namespace cpp {
+
+Content::Content() : value() {}
+
+void Content::operator+=(std::string content) { value += content; }
+
+void Content::operator+=(const char *content) { value += content; }
+
+std::string Content::getContent() const { return value; }
+
+void Content::setContent(std::string val) { value = val; }
+
+Namespace::Namespace(std::string _name, Namespace *_parent, bool _isFile)
+    : name(_name), parent(_parent), isFile(_isFile), children(), Content() {}
+
+std::string Namespace::getName() const { return name; }
+
+std::string Namespace::getFullName() const {
+  if (parent) {
+    return parent->getFullName() + "::" + name;
+  } else {
+    if (isFile) {
+      return "";
+    } else {
+      return name;
+    }
+  }
+}
+
+bool Namespace::hasParent() const { return parent != nullptr; }
+
+Namespace *Namespace::getActive() { return active ? active : this; }
+
+Namespace *Namespace::addChild(std::string name) {
+  children.push_back(new Namespace(name, this, false));
+  active = children.back();
+  return active;
+}
+
+void Namespace::addEnclosedComment(const std::string comment) {
+  *this += (" /** " + comment + " */ ");
+}
+
+void Namespace::addSingleLineComment(const std::string comment) {
+  *this += ("// " + comment + "\n");
+}
+
+void Namespace::closeChild() { active = nullptr; }
+
+File *Namespace::getFile() {
+  if (isFile) {
+    return (File *)this;
+  } else if (hasParent()) {
+    return parent->getFile();
+  }
+}
+
+File::File(std::string _path, bool _isHeader)
+    : path(_path), isHeader(_isHeader), Namespace("", nullptr, true) {}
+
+File File::Header(std::string path) { return File(path, false); }
+
+File File::Source(std::string path) { return File(path, true); }
+
+void File::addInclude(const std::string unit) {
+  bool exists = false;
+  for (auto val : includes) {
+    if (val == unit) {
+      exists = true;
+      break;
+    }
+  }
+  for (auto val : headerIncludes) {
+    if (val == unit) {
+      exists = true;
+      break;
+    }
+  }
+  if (!exists) {
+    includes.push_back(unit);
+  }
+}
+
+std::vector<std::string> File::getIncludes() const { return includes; }
+
+void File::moveIncludes(File &other) {
+  for (auto inc : includes) {
+    other.addInclude(inc);
+  }
+  includes.clear();
+}
+
+void File::updateHeaderIncludes(std::vector<std::string> values) {
+  for (auto val : values) {
+    headerIncludes.push_back(val);
+  }
+}
+
+void File::addLoopID(std::string ID) { loopIndices.push_back(ID); }
+
+std::string File::getLoopIndex() const { return loopIndices.back(); }
+
+void File::popLastLoopIndex() { loopIndices.pop_back(); }
+
+void File::setOpenBlock(bool val) { openBlock = val; }
+
+bool File::getOpenBlock() const { return openBlock; }
+
+bool File::isHeaderFile() const { return isHeader; }
+
+bool File::isSourceFile() const { return !isHeader; }
+
+void File::write() {
+  std::string includePart;
+  for (auto inc : includes) {
+    if (inc.find("<") == 0 && inc.find_last_of('>')) {
+      includePart += ("#include " + inc + "\n");
+    } else {
+      includePart += ("#include \"" + inc + "\"\n");
+    }
+  }
+  setContent(includePart + "\n" + getContent());
+
+  std::fstream outputStream;
+  auto filepath = std::filesystem::path(path);
+  outputStream.open(filepath, std::ios_base::out);
+  if (outputStream.is_open()) {
+    outputStream << getContent();
+    outputStream.close();
+  }
+}
+
+} // namespace cpp
+} // namespace backend
+} // namespace qat
---
+++ src/backend/cpp.hpp
@@ -0,0 +1,119 @@
+#ifndef QAT_BACKEND_CPP_HPP
+#define QAT_BACKEND_CPP_HPP
+
+#include <filesystem>
+#include <string>
+#include <vector>
+
+namespace qat {
+namespace backend {
+
+namespace cpp {
+
+class File;
+
+class Content {
+private:
+  std::string value;
+
+protected:
+  friend class File;
+  void setContent(std::string val);
+
+public:
+  Content();
+
+  void operator+=(std::string content);
+
+  void operator+=(const char *content);
+
+  std::string getContent() const;
+};
+
+class File;
+
+class Namespace : public Content {
+private:
+  std::string name;
+
+  Namespace *parent;
+
+  Namespace *active;
+
+  std::vector<Namespace *> children;
+
+  bool isFile;
+
+public:
+  Namespace(std::string _name, Namespace *parent, bool isFile);
+
+  std::string getName() const;
+
+  std::string getFullName() const;
+
+  bool hasParent() const;
+
+  Namespace *getActive();
+
+  Namespace *addChild(const std::string name);
+
+  void addEnclosedComment(const std::string name);
+
+  void addSingleLineComment(const std::string name);
+
+  void closeChild();
+
+  File *getFile();
+};
+
+class File : public Namespace {
+private:
+  bool isHeader;
+
+  std::string path;
+
+  std::vector<std::string> includes;
+
+  std::vector<std::string> headerIncludes;
+
+  std::vector<std::string> loopIndices;
+
+  File(std::string _path, bool _isHeader);
+
+  bool openBlock;
+
+public:
+  static File Header(std::string path);
+
+  static File Source(std::string path);
+
+  void addInclude(const std::string unit);
+
+  std::vector<std::string> getIncludes() const;
+
+  void updateHeaderIncludes(std::vector<std::string> values);
+
+  void moveIncludes(File &other);
+
+  bool isHeaderFile() const;
+
+  bool isSourceFile() const;
+
+  void addLoopID(std::string ID);
+
+  std::string getLoopIndex() const;
+
+  void popLastLoopIndex();
+
+  void setOpenBlock(bool val);
+
+  bool getOpenBlock() const;
+
+  void write();
+};
+
+} // namespace cpp
+} // namespace backend
+} // namespace qat
+
+#endif

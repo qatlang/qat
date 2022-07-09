98f3d22c3a9f9290ff2af93088ca80c3a8ee02f3
Aldrin Mathew <admin@aldrinmathew.com>
2022-07-09 17:46:39 +0530

    Added `qat::IR::MemberFunction`
    - Added IR representation for member functions of core types
    
    - Both static and non-static methods are represented by the same

---
+++ src/IR/member_function.cpp
@@ -0,0 +1,98 @@
+#include "./member_function.hpp"
+#include "function.hpp"
+#include "types/core_type.hpp"
+#include "types/pointer.hpp"
+#include "types/qat_type.hpp"
+#include "types/void.hpp"
+
+namespace qat {
+namespace IR {
+
+MemberFunction::MemberFunction(llvm::Module *module, CoreType *_parent,
+                               std::string _name, QatType *returnType,
+                               bool _is_async, std::vector<Argument> _args,
+                               bool is_variable_arguments, bool _is_static,
+                               bool _returns_reference,
+                               const utils::FilePlacement _placement,
+                               utils::VisibilityInfo _visibility_info)
+    : parent(_parent),
+      Function(module, parent->getName(), _name, returnType, _is_async, _args,
+               is_variable_arguments, _returns_reference, _placement,
+               _visibility_info) {
+  std::vector<llvm::Type *> arg_types;
+  for (auto arg : arguments) {
+    arg_types.push_back(arg.getType()->getLLVMType());
+  }
+  function = llvm::Function::Create(
+      llvm::FunctionType::get(returnType->getLLVMType(), arg_types,
+                              is_variable_arguments),
+      llvm::GlobalValue::LinkageTypes::WeakAnyLinkage, 0,
+      (parent->getName() + (isStatic ? ":" : "'") + _name), module);
+  utils::Visibility::set(function, visibility_info, module->getContext());
+  utils::PointerKind::set(module->getContext(), function, _returns_reference);
+  for (std::size_t i = 0; i < arguments.size(); i++) {
+    utils::Variability::set(function->getArg(i),
+                            arguments.at(i).get_variability());
+  }
+}
+
+MemberFunction *MemberFunction::Create(
+    llvm::Module *mod, CoreType *parent, const bool is_variation,
+    const std::string name, QatType *returnTy, const bool _is_async,
+    const std::vector<Argument> args, const bool has_variadic_args,
+    const bool returns_reference, const utils::FilePlacement placement,
+    const utils::VisibilityInfo visib_info) {
+  std::vector<Argument> args_info;
+  args_info.push_back(is_variation ? Argument::CreateVariable("self", parent, 0)
+                                   : Argument::Create("self", parent, 0));
+  return new MemberFunction(mod, parent, name, returnTy, _is_async, args_info,
+                            has_variadic_args, false, returns_reference,
+                            placement, visib_info);
+}
+
+MemberFunction *MemberFunction::CreateStatic(
+    llvm::Module *mod, CoreType *parent, const std::string name,
+    QatType *returnTy, const bool is_async, const std::vector<Argument> args,
+    const bool has_variadic_args, const bool returns_reference,
+    const utils::FilePlacement placement,
+    const utils::VisibilityInfo visib_info //
+) {
+  return new MemberFunction(mod, parent, name, returnTy, is_async, args,
+                            has_variadic_args, true, returns_reference,
+                            placement, visib_info);
+}
+
+MemberFunction *
+MemberFunction::CreateDestructor(llvm::Module *mod, CoreType *parent,
+                                 const utils::FilePlacement placement) {
+  return new MemberFunction(
+      mod, parent, "end", new VoidType(mod->getContext()), false,
+      std::vector<Argument>(
+          {Argument::CreateVariable("self", new PointerType(parent), 0)}),
+      false, false, false, placement, utils::VisibilityInfo::pub());
+}
+
+bool MemberFunction::isVariationFunction() const {
+  return isStatic
+             ? false
+             : function->getArg(0)->hasAttribute(llvm::Attribute::ReadOnly);
+}
+
+std::string MemberFunction::getFullName() const {
+  return parent->getFullName() + ":" + name;
+}
+
+bool MemberFunction::isStaticFunction() const { return isStatic; }
+
+llvm::CallInst *
+MemberFunction::create_call(llvm::IRBuilder<> &builder,
+                            llvm::ArrayRef<llvm::Value *> args //
+) const {
+  return builder.CreateCall(function->getFunctionType(), function, args,
+                            getFullName(), nullptr);
+}
+
+bool MemberFunction::isMemberFunction() const { return true; }
+
+} // namespace IR
+} // namespace qat
---
+++ src/IR/member_function.hpp
@@ -0,0 +1,137 @@
+#ifndef QAT_IR_MEMBER_FUNCTION_HPP
+#define QAT_IR_MEMBER_FUNCTION_HPP
+
+#include "../utils/pointer_kind.hpp"
+#include "../utils/variability.hpp"
+#include "../utils/visibility.hpp"
+#include "./argument.hpp"
+#include "./function.hpp"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/GlobalValue.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/Module.h"
+#include <string>
+#include <vector>
+
+namespace qat {
+namespace IR {
+
+class CoreType;
+
+/**
+ *  MemberFunction represents a member function for a core type in
+ * the language. It can be static or non-static
+ *
+ */
+class MemberFunction : public Function {
+private:
+  /**
+   *  Parent type
+   *
+   */
+  CoreType *parent;
+
+  /**
+   *  Whether this is a static member function or not
+   *
+   */
+  bool isStatic;
+
+  /**
+   *  Private constructor for MemberFunction
+   *
+   * @param mod LLVM Module to insert the generated function to
+   * @param _name Name of the function
+   * @param returnType LLVM Type of the return value
+   * @param _arg_names Names of arguments
+   * @param _arg_types LLVM Types of the arguments
+   * @param _args_variability Variability values of the arguments
+   * @param has_variadic_arguments Whether the function has variable number of
+   * arguments
+   * @param _is_static Whether the function is a static member function
+   * @param _visibility_info VisibilityInfo of the function
+   */
+  MemberFunction(llvm::Module *mod, CoreType *_parent, std::string _name,
+                 QatType *returnType, bool _is_async,
+                 std::vector<Argument> _args, bool has_variadic_arguments,
+                 bool _is_static, bool _returns_reference,
+                 utils::FilePlacement _placement,
+                 utils::VisibilityInfo _visibility_info);
+
+public:
+  /**
+   *  Create a member function for the provided parent type
+   *
+   * @param mod LLVM Module to insert the generated LLVM Function to
+   * @param parent LLVM Type of the parent core type
+   * @param is_variation Whether this is a variation or not
+   * @param name Name of the function (short)
+   * @param return_type LLVM Type of the return type
+   * @param args Arguments of the function
+   * @param has_variadic_args Whether the function has variadic arguments
+   * @param visib_info VisibilityInfo of the function
+   * @return MemberFunction
+   */
+  static MemberFunction *
+  Create(llvm::Module *mod, CoreType *parent, const bool is_variation,
+         const std::string name, QatType *return_type, const bool is_async,
+         const std::vector<Argument> args, const bool has_variadic_args,
+         const bool returns_reference, const utils::FilePlacement placement,
+         const utils::VisibilityInfo visib_info);
+
+  static MemberFunction *CreateDestructor(llvm::Module *mod, CoreType *parent,
+                                          const utils::FilePlacement placement);
+
+  /**
+   *  Create a static member function for the provided parent type
+   *
+   * @param mod LLVM Module to insert the generated LLVM function to
+   * @param parent LLVM Type of the parent core type
+   * @param name Name of the function (short)
+   * @param return_type LLVM Type of the return type
+   * @param args Arguments of the function
+   * @param has_variadic_args Whether the function has variadic arguments
+   * @param visib_info VisibilityInfo of the function
+   * @return MemberFunction
+   */
+  static MemberFunction *CreateStatic(llvm::Module *mod, CoreType *parent,
+                                      const std::string name,
+                                      QatType *return_type, const bool is_async,
+                                      const std::vector<Argument> args,
+                                      const bool has_variadic_args,
+                                      const bool returns_reference,
+                                      const utils::FilePlacement placement,
+                                      const utils::VisibilityInfo visib_info);
+
+  /**
+   *  Whether this function is a variation or not. This only applies to
+   * non-static member functions
+   *
+   * @return true
+   * @return false
+   */
+  bool isVariationFunction() const;
+
+  bool isStaticFunction() const;
+
+  std::string getFullName() const;
+
+  /**
+   *  Create a call to this function
+   *
+   * @return llvm::CallInst*
+   */
+  llvm::CallInst *create_call(
+      llvm::IRBuilder<llvm::ConstantFolder, llvm::IRBuilderDefaultInserter>
+          &builder,
+      llvm::ArrayRef<llvm::Value *> args) const;
+
+  bool isMemberFunction() const;
+};
+
+} // namespace IR
+} // namespace qat
+
+#endif

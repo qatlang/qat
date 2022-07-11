71a8c104c535b49df13b9185b7ad7a4fa4492492
Aldrin Mathew <admin@aldrinmathew.com>
2022-07-12 00:00:16 +0530

    Added emitter virtual functions to QatType
    - Virtual functions for emitters are added to qat::IR::QatType
    
    - Added llvmHelper to contain the LLVM context and IR builder

---
+++ src/IR/llvm_helper.hpp
@@ -0,0 +1,20 @@
+#ifndef QAT_IR_LLVM_HELPER_HPP
+#define QAT_IR_LLVM_HELPER_HPP
+
+#include "llvm/IR/ConstantFolder.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+
+namespace qat::IR {
+
+class llvmHelper {
+public:
+  llvmHelper() : llctx(), builder(llctx) {}
+
+  llvm::LLVMContext llctx;
+  llvm::IRBuilder<llvm::ConstantFolder, llvm::IRBuilderDefaultInserter> builder;
+};
+
+} // namespace qat::IR
+
+#endif
--- src/IR/types/qat_type.hpp
+++ src/IR/types/qat_type.hpp
@@ -1,7 +1,11 @@
 #ifndef QAT_IR_TYPES_QAT_TYPE_HPP
 #define QAT_IR_TYPES_QAT_TYPE_HPP
 
+#include "../../backend/cpp.hpp"
+#include "../../backend/json.hpp"
+#include "../llvm_helper.hpp"
 #include "./type_kind.hpp"
+#include "llvm/IR/Type.h"
 #include <string>
 #include <vector>
 
@@ -18,13 +22,23 @@ public:
   virtual ~QatType(){};
 
   // TypeKind is used to detect variants of the QatType
-  virtual TypeKind typeKind() const {};
+  virtual TypeKind typeKind() const {}
 
   static bool checkTypeExists(const std::string name);
 
   bool isSame(QatType *other) const;
 
+  // Emitters
+
+  virtual llvm::Type *emitLLVM(llvmHelper helper) const {}
+
+  virtual void emitCPP(backend::cpp::File &file) const {}
+
+  // Converters
+
   virtual std::string toString() const {}
+
+  virtual backend::JSON toJSON() const {}
 };
 
 } // namespace qat::IR

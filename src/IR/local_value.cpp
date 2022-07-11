a537e2f46cc5d6cd8e67e3c194624a82bb9d1ed2
Aldrin Mathew <admin@aldrinmathew.com>
2022-07-11 17:05:26 +0530

    Added block field to LocalValue
    - This enables the LocalValue to interact with the parent block freely
    
    - This is also useful in the code generation phase

--- src/IR/local_value.cpp
+++ src/IR/local_value.cpp
@@ -6,20 +6,20 @@ namespace IR {
 
 LocalValue::LocalValue(std::string _name, QatType *_type, bool is_variable,
                        Value *_initial, Block *block)
-    : name(_name), initial(_initial), parent(block), loads(0), stores(0), refers(0),
-      Value(_type, is_variable, Kind::assignable) {}
+    : name(_name), initial(_initial), parent(block), loads(0), stores(0),
+      refers(0), Value(_type, is_variable, Kind::assignable) {}
 
 bool LocalValue::isRemovable() const {
-    if (getType()->typeKind() != IR::TypeKind::reference) {
-        return (getType()->typeKind() != IR::TypeKind::pointer && (loads == 0) && (refers == 0) ||
-                ((loads == 0) && (stores == 0) && (refers == 0)));
-    } else {
-        return (loads == 0) && (stores == 0) && (refers == 0);
-    }
-
-  Block* LocalValue::getParent() { return parent; }
-
+  if (getType()->typeKind() != IR::TypeKind::reference) {
+    return (getType()->typeKind() != IR::TypeKind::pointer && (loads == 0) &&
+                (refers == 0) ||
+            ((loads == 0) && (stores == 0) && (refers == 0)));
+  } else {
+    return (loads == 0) && (stores == 0) && (refers == 0);
+  }
 }
+
+Block *LocalValue::getParent() { return parent; }
 
 } // namespace IR
 } // namespace qat

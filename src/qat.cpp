5742b9055bcea684e59fee796ed4adfb959d0256
Aldrin Mathew <admin@aldrinmathew.com>
2022-07-20 14:43:55 +0530

    Added function to set colors in windows
    - Added function that communicates with native windows API so as to set
      console colors to on
    
    - The new default behaviour is off, which causes the ascii color
      characters to be displayed as actual characters instead of colors

--- src/qat.cpp
+++ src/qat.cpp
@@ -1,7 +1,27 @@
 #include "./qat_sitter.hpp"
 
+#if PLATFORM_IS_WINDOWS
+#include <iostream>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+#endif
+
+void setTerminalColors() {
+#if PLATFORM_IS_WINDOWS
+  HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+  DWORD consoleMode;
+  GetConsoleMode(handleOut, &consoleMode);
+  consoleMode |= 0x0004;
+  consoleMode |= 0x0008;
+  SetConsoleMode(handleOut, consoleMode);
+#endif
+}
+
 int main(int count, const char **args) {
   using qat::cli::Config;
+
+  setTerminalColors();
 
   auto cli = Config::init(count, args);
   if (cli->shouldExit()) {
--- src/show.hpp
+++ src/show.hpp
@@ -3,10 +3,14 @@
 
 #include "./cli/color.hpp"
 
+#define HLIGHT(col, val) colors::col << val << colors::reset
 #if NDEBUG
 #define SHOW(val)
+#define HIGHLIGHT(val)
 #else
 #define SHOW(val) std::cout << colors::blue << val << colors::reset << "\n";
+#define HIGHLIGHT(col, val)                                                    \
+  std::cout << colors::col << val << colors::reset << "\n";
 #endif
 
 #endif

f2426d50a067128417fd7a413d437b2b5e0b196b
Aldrin Mathew <admin@aldrinmathew.com>
2022-08-07 14:43:36 +0530

    Added emitter function for null pointer
    - Implemented LLVM emitter function for null pointers
    
    - Right now, it doesn't change the underlying type for other pointer
      types. It should automatically change the type and this will be
      implemented later

--- src/ast/expressions/null_pointer.cpp
+++ src/ast/expressions/null_pointer.cpp
@@ -1,18 +1,23 @@
 #include "./null_pointer.hpp"
+#include "llvm/IR/Constants.h"
 
 namespace qat::ast {
 
 NullPointer::NullPointer(utils::FileRange _fileRange)
-    : type(nullptr), Expression(std::move(_fileRange)) {}
+    : Expression(std::move(_fileRange)), type(nullptr) {}
 
 NullPointer::NullPointer(llvm::Type *_type, utils::FileRange _fileRange)
-    : type(_type), Expression(std::move(_fileRange)) {}
+    : Expression(std::move(_fileRange)), type(_type) {}
 
 IR::Value *NullPointer::emit(IR::Context *ctx) {
   if (getExpectedKind() == ExpressionKind::assignable) {
     ctx->Error("Null pointer is not assignable", fileRange);
   }
-  // TODO - Implement this
+  return new IR::Value(
+      llvm::ConstantPointerNull::get(
+          llvm::Type::getInt8Ty(ctx->llctx)->getPointerTo()),
+      IR::PointerType::get(IR::VoidType::get(ctx->llctx), ctx->llctx), false,
+      IR::Nature::pure);
 }
 
 nuo::Json NullPointer::toJson() const {

715c4e5810485f1e05ef5dfc27c0ad91a7db6520
Aldrin Mathew <admin@aldrinmathew.com>
2022-08-07 14:41:54 +0530

    Added emitter for string literals
    - Implement LLVM emitter function for string literals. This creates a
      global string pointer in the IR, like a c-style string

--- src/ast/expressions/string_literal.cpp
+++ src/ast/expressions/string_literal.cpp
@@ -1,14 +1,18 @@
 #include "./string_literal.hpp"
+#include "../../IR/types/string_slice.hpp"
 
 namespace qat::ast {
 
 StringLiteral::StringLiteral(String _value, utils::FileRange _fileRange)
-    : value(std::move(_value)), Expression(std::move(_fileRange)) {}
+    : Expression(std::move(_fileRange)), value(std::move(_value)) {}
 
 String StringLiteral::get_value() const { return value; }
 
 IR::Value *StringLiteral::emit(IR::Context *ctx) {
-  // TODO - Implement this
+  return new IR::Value(ctx->builder.CreateGlobalStringPtr(
+                           value, "str", 0U, ctx->getMod()->getLLVMModule()),
+                       IR::StringSliceType::get(ctx->llctx), false,
+                       IR::Nature::pure);
 }
 
 nuo::Json StringLiteral::toJson() const {

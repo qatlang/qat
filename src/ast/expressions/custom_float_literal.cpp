ee11f4454bf498d7b8a9b84932ff4f7e5e61e5a2
Aldrin Mathew <admin@aldrinmathew.com>
2022-08-13 13:19:56 +0530

    Updated emission for custom float literals
    - Currently there is an error in the LLVM emission side as all floating
      point constants emitted as of FP128 type, which is causing issues in
      declarations

--- src/ast/expressions/custom_float_literal.cpp
+++ src/ast/expressions/custom_float_literal.cpp
@@ -1,17 +1,62 @@
 #include "./custom_float_literal.hpp"
+#include "llvm/ADT/APFloat.h"
 
 namespace qat::ast {
 
 CustomFloatLiteral::CustomFloatLiteral(String _value, String _kind,
                                        utils::FileRange _fileRange)
-    : value(std::move(_value)), kind(std::move(_kind)),
-      Expression(std::move(_fileRange)) {}
+    : Expression(std::move(_fileRange)), value(std::move(_value)),
+      kind(std::move(_kind)) {}
 
 IR::Value *CustomFloatLiteral::emit(IR::Context *ctx) {
   if (isExpectedKind(ExpressionKind::assignable)) {
     ctx->Error("Float literals are not assignable", fileRange);
   }
-  // TODO - Implement this
+  if (kind == "fhalf") {
+    return new IR::Value(
+        llvm::ConstantFP::get(llvm::Type::getHalfTy(ctx->llctx),
+                              std::stof(value)),
+        IR::FloatType::get(IR::FloatTypeKind::_half, ctx->llctx), false,
+        IR::Nature::pure);
+  } else if (kind == "fbrain") {
+    return new IR::Value(
+        llvm::ConstantFP::get(llvm::Type::getBFloatTy(ctx->llctx),
+                              std::stof(value)),
+        IR::FloatType::get(IR::FloatTypeKind::_brain, ctx->llctx), false,
+        IR::Nature::pure);
+  } else if (kind == "f32") {
+    return new IR::Value(
+        llvm::ConstantFP::get(llvm::Type::getFloatTy(ctx->llctx),
+                              std::stof(value)),
+        IR::FloatType::get(IR::FloatTypeKind::_32, ctx->llctx), false,
+        IR::Nature::pure);
+  } else if (kind == "f64") {
+    return new IR::Value(
+        llvm::ConstantFP::get(llvm::Type::getDoubleTy(ctx->llctx),
+                              std::stod(value)),
+        IR::FloatType::get(IR::FloatTypeKind::_64, ctx->llctx), false,
+        IR::Nature::pure);
+  } else if (kind == "f80") {
+    return new IR::Value(
+        llvm::ConstantFP::get(llvm::Type::getX86_FP80Ty(ctx->llctx),
+                              std::stod(value)),
+        IR::FloatType::get(IR::FloatTypeKind::_80, ctx->llctx), false,
+        IR::Nature::pure);
+  } else if (kind == "f128") {
+    return new IR::Value(
+        llvm::ConstantFP::get(llvm::Type::getFP128Ty(ctx->llctx),
+                              std::stod(value)),
+        IR::FloatType::get(IR::FloatTypeKind::_128, ctx->llctx), false,
+        IR::Nature::pure);
+  } else if (kind == "f128ppc") {
+    return new IR::Value(
+        llvm::ConstantFP::get(llvm::Type::getPPC_FP128Ty(ctx->llctx),
+                              std::stod(value)),
+        IR::FloatType::get(IR::FloatTypeKind::_128PPC, ctx->llctx), false,
+        IR::Nature::pure);
+  } else {
+    ctx->Error("Invalid specification for custom float literal", fileRange);
+  }
 }
 
 nuo::Json CustomFloatLiteral::toJson() const {

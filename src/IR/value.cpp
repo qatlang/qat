89026433dec760383a9b4fce8274ca2216a4e03c
Aldrin Mathew <admin@aldrinmathew.com>
2022-08-14 00:41:39 +0530

    Updated `IR::Value`
    - Added function is IR::Value that automatically loads the llvm value if
      the value is an implicit pointer (AllocaInst or GlobalVariable)
    
    - Added function that automatically creates an AllocaInst for the
      current llvm value. This is useful when the pointer to the value is
      required for a specific instruction (Ex. GEP, StructGEP...).

--- src/IR/value.cpp
+++ src/IR/value.cpp
@@ -1,15 +1,37 @@
 #include "./value.hpp"
 #include "./types/qat_type.hpp"
+#include "llvm/IR/Instructions.h"
 
 namespace qat::IR {
 
 Value::Value(llvm::Value *_llvmValue, IR::QatType *_type, bool _isVariable,
              Nature _kind)
-    : ll(_llvmValue), type(_type), variable(_isVariable), nature(_kind) {}
+    : type(_type), nature(_kind), variable(_isVariable), ll(_llvmValue) {
+  allValues.push_back(this);
+}
+
+Vec<Value *> Value::allValues = {};
 
 QatType *Value::getType() const { return type; }
 
 llvm::Value *Value::getLLVM() { return ll; }
+
+IR::Value *Value::createAlloca(llvm::IRBuilder<> &builder) {
+  auto *alloc = builder.CreateAlloca(getType()->getLLVMType());
+  builder.CreateStore(ll, alloc);
+  return new IR::Value(alloc, getType(), variable, nature);
+}
+
+bool Value::isImplicitPointer() const {
+  return ll && (llvm::isa<llvm::AllocaInst>(ll) ||
+                llvm::isa<llvm::GlobalVariable>(ll));
+}
+
+void Value::loadImplicitPointer(llvm::IRBuilder<> &builder) {
+  if (isImplicitPointer()) {
+    ll = builder.CreateLoad(getType()->getLLVMType(), ll);
+  }
+}
 
 bool Value::isPointer() const {
   return (type->typeKind() == IR::TypeKind::pointer);
@@ -22,5 +44,12 @@ bool Value::isReference() const {
 bool Value::isVariable() const { return variable; }
 
 Nature Value::getNature() const { return nature; }
+
+void Value::clearAll() {
+  for (auto *val : allValues) {
+    delete val;
+  }
+  allValues.clear();
+}
 
 } // namespace qat::IR
--- src/IR/value.hpp
+++ src/IR/value.hpp
@@ -5,6 +5,7 @@
 #include "llvm_helper.hpp"
 #include "llvm/IR/ConstantFolder.h"
 #include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/IR/Value.h"
 #include <nuo/json.hpp>
 
@@ -15,6 +16,9 @@ class QatType;
 enum class Nature { assignable, temporary, pure, expired };
 
 class Value {
+private:
+  static Vec<IR::Value *> allValues;
+
 protected:
   IR::QatType *type;     // Type representation of the value
   Nature       nature;   // The nature of the value
@@ -25,12 +29,19 @@ public:
   Value(llvm::Value *_llValue, IR::QatType *_type, bool _isVariable,
         Nature kind);
 
+  virtual ~Value() = default;
+
   useit QatType *getType() const; // Type of the value
   useit llvm::Value *getLLVM();
   useit bool         isReference() const;
   useit bool         isPointer() const;
   useit bool         isVariable() const;
   useit Nature       getNature() const;
+  useit IR::Value *createAlloca(llvm::IRBuilder<> &builder);
+  useit bool       isImplicitPointer() const;
+  void             loadImplicitPointer(llvm::IRBuilder<> &builder);
+
+  static void clearAll();
 };
 
 } // namespace qat::IR

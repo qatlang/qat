e2a1e51d46d7a4786e5a997a5ad16590ce9d736d
Aldrin Mathew <admin@aldrinmathew.com>
2022-08-21 12:53:18 +0530

    Added emission for `ast::LoopWhile`
    - Now `loop while` syntax is completely supported
    
    - Proper LLVM emission has been added

--- src/ast/sentences/loop_while.cpp
+++ src/ast/sentences/loop_while.cpp
@@ -2,21 +2,58 @@
 
 namespace qat::ast {
 
-LoopWhile::LoopWhile(Expression *_condition, Block *_block, Block *_after,
+LoopWhile::LoopWhile(Expression *_condition, Vec<Sentence *> _sentences,
                      utils::FileRange _fileRange)
-    : Sentence(_fileRange), block(_block), after(_after),
-      condition(_condition) {}
+    : Sentence(std::move(_fileRange)), condition(_condition),
+      sentences(std::move(_sentences)) {}
 
 IR::Value *LoopWhile::emit(IR::Context *ctx) {
-  // TODO - Implement this
+  auto *cond = condition->emit(ctx);
+  cond->loadImplicitPointer(ctx->builder);
+  if (cond->getType()->isUnsignedInteger() ||
+      (cond->getType()->isReference() &&
+       cond->getType()->asReference()->getSubType()->isUnsignedInteger())) {
+    auto *fun       = ctx->fn;
+    auto *trueBlock = new IR::Block(fun, fun->getBlock());
+    auto *restBlock = new IR::Block(fun, fun->getBlock());
+    auto *llCond    = cond->getLLVM();
+    if (cond->getType()->isReference()) {
+      llCond = ctx->builder.CreateLoad(
+          cond->getType()->asReference()->getSubType()->getLLVMType(), llCond);
+    }
+    ctx->builder.CreateCondBr(llCond, trueBlock->getBB(), restBlock->getBB());
+    trueBlock->setActive(ctx->builder);
+    for (auto *snt : sentences) {
+      (void)snt->emit(ctx);
+    }
+    cond = condition->emit(ctx);
+    cond->loadImplicitPointer(ctx->builder);
+    if (cond->getType()->isReference()) {
+      llCond = ctx->builder.CreateLoad(
+          cond->getType()->asReference()->getSubType()->getLLVMType(),
+          cond->getLLVM());
+    } else {
+      llCond = cond->getLLVM();
+    }
+    ctx->builder.CreateCondBr(llCond, trueBlock->getBB(), restBlock->getBB());
+    restBlock->setActive(ctx->builder);
+  } else {
+    ctx->Error("The type of expression for the condition is not of unsigned "
+               "integer type",
+               condition->fileRange);
+  }
+  return nullptr;
 }
 
 nuo::Json LoopWhile::toJson() const {
+  Vec<nuo::JsonValue> snts;
+  for (auto *snt : sentences) {
+    snts.push_back(snt->toJson());
+  }
   return nuo::Json()
       ._("nodeType", "loopWhile")
       ._("condition", condition->toJson())
-      ._("block", block->toJson())
-      ._("after", after->toJson())
+      ._("sentences", snts)
       ._("fileRange", fileRange);
 }
 
--- src/ast/sentences/loop_while.hpp
+++ src/ast/sentences/loop_while.hpp
@@ -13,43 +13,16 @@ namespace qat::ast {
  *
  */
 class LoopWhile : public Sentence {
-  /**
-   *  The block that will be looped over
-   *
-   */
-  Block *block;
-
-  /**
-   *  The block that happens after the loop block
-   *
-   */
-  Block *after;
-
-  /**
-   *  Expression representing the condition for the loop
-   *
-   */
-  Expression *condition;
+  Expression     *condition;
+  Vec<Sentence *> sentences;
 
 public:
-  /**
-   *  LoopWhile is used to loop through a block of code while a condition
-   * is true
-   *
-   * @param _condition Condition that controls the number of times the loop
-   * executes
-   * @param _block The main block of the loop
-   * @param _after The block after the loop, which is not part of it
-   * @param _fileRange
-   */
-  LoopWhile(Expression *_condition, Block *_block, Block *_after,
+  LoopWhile(Expression *_condition, Vec<Sentence *> _sentences,
             utils::FileRange _fileRange);
 
-  IR::Value *emit(IR::Context *ctx);
-
-  nuo::Json toJson() const;
-
-  NodeType nodeType() const { return NodeType::loopWhile; }
+  useit IR::Value *emit(IR::Context *ctx) final;
+  useit nuo::Json toJson() const final;
+  useit NodeType  nodeType() const final { return NodeType::loopWhile; }
 };
 
 } // namespace qat::ast

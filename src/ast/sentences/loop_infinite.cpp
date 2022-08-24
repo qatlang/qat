f4698d92454cda5feb2338ae0a12ac8b0ab1a1ec
Aldrin Mathew <admin@aldrinmathew.com>
2022-08-25 00:38:38 +0530

    Added `ast::LoopInfinite`
    - LoopInfinite represents a loop that does not have any condition or a
      count
    
    - Users can use `break` and `continue` sentences to manage control flow
    
    - Loop tags/labels are supported for this kind of loop

---
+++ src/ast/sentences/loop_infinite.cpp
@@ -0,0 +1,63 @@
+#include "./loop_infinite.hpp"
+#include "../../IR/control_flow.hpp"
+
+namespace qat::ast {
+
+LoopInfinite::LoopInfinite(Vec<Sentence *> _sentences, Maybe<String> _tag,
+                           utils::FileRange _fileRange)
+    : Sentence(std::move(_fileRange)), sentences(std::move(_sentences)),
+      tag(std::move(_tag)) {}
+
+IR::Value *LoopInfinite::emit(IR::Context *ctx) {
+  String uniq;
+  if (tag.has_value()) {
+    uniq = tag.value();
+    for (const auto &info : ctx->loopsInfo) {
+      if (info->name == uniq) {
+        ctx->Error(
+            "The tag provided for this loop is already used by another loop",
+            fileRange);
+      }
+    }
+    for (const auto &brek : ctx->breakables) {
+      if (brek->tag.has_value() && (brek->tag.value() == tag.value())) {
+        ctx->Error("The tag provided for the loop is already used by another "
+                   "loop or switch",
+                   fileRange);
+      }
+    }
+  } else {
+    uniq = utils::unique_id();
+  }
+  auto *trueBlock = new IR::Block(ctx->fn, ctx->fn->getBlock());
+  SHOW("Infinite loop true block " << trueBlock->getName())
+  auto *restBlock = new IR::Block(ctx->fn, ctx->fn->getBlock());
+  SHOW("Infinite loop rest block " << restBlock->getName())
+  (void)IR::addBranch(ctx->builder, trueBlock->getBB());
+  trueBlock->setActive(ctx->builder);
+  ctx->loopsInfo.push_back(new IR::LoopInfo(uniq, trueBlock, nullptr, restBlock,
+                                            nullptr, IR::LoopType::infinite));
+  ctx->breakables.push_back(new IR::Breakable(
+      tag.has_value() ? Maybe<String>(uniq) : None, restBlock));
+  emitSentences(sentences, ctx);
+  ctx->loopsInfo.pop_back();
+  ctx->breakables.pop_back();
+  (void)IR::addBranch(ctx->builder, trueBlock->getBB());
+  restBlock->setActive(ctx->builder);
+  return nullptr;
+}
+
+nuo::Json LoopInfinite::toJson() const {
+  Vec<nuo::JsonValue> snts;
+  for (auto *snt : sentences) {
+    snts.push_back(snt->toJson());
+  }
+  return nuo::Json()
+      ._("nodeType", "loopNormal")
+      ._("sentences", snts)
+      ._("hasTag", tag.has_value())
+      ._("tag", tag.value_or(""))
+      ._("fileRange", fileRange);
+}
+
+} // namespace qat::ast
---
+++ src/ast/sentences/loop_infinite.hpp
@@ -0,0 +1,26 @@
+#ifndef QAT_AST_SENTENCES_LOOP_INFINITE_HPP
+#define QAT_AST_SENTENCES_LOOP_INFINITE_HPP
+
+#include "../expression.hpp"
+#include "../node_type.hpp"
+#include "../sentence.hpp"
+#include "./block.hpp"
+
+namespace qat::ast {
+
+class LoopInfinite : public Sentence {
+  Vec<Sentence *> sentences;
+  Maybe<String>   tag;
+
+public:
+  LoopInfinite(Vec<Sentence *> _sentences, Maybe<String> _tag,
+               utils::FileRange _fileRange);
+
+  useit IR::Value *emit(IR::Context *ctx) final;
+  useit nuo::Json toJson() const final;
+  useit NodeType  nodeType() const final { return NodeType::loopNormal; }
+};
+
+} // namespace qat::ast
+
+#endif

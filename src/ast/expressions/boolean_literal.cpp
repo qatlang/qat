77490b58b7a22b745771b30158f29a7f36cfdf02
Aldrin Mathew <admin@aldrinmathew.com>
2022-09-03 21:39:32 +0530

    Added `ast::BooleanLiteral`
    - I forgot to add boolean literals to the language till now. Now they
      are supported

---
+++ src/ast/expressions/boolean_literal.cpp
@@ -0,0 +1,21 @@
+#include "./boolean_literal.hpp"
+
+namespace qat::ast {
+
+BooleanLiteral::BooleanLiteral(bool _value, utils::FileRange _fileRange)
+    : Expression(std::move(_fileRange)), value(_value) {}
+
+IR::Value *BooleanLiteral::emit(IR::Context *ctx) {
+  return new IR::Value(llvm::ConstantInt::getBool(ctx->llctx, value),
+                       IR::UnsignedType::get(1u, ctx->llctx), false,
+                       IR::Nature::pure);
+}
+
+nuo::Json BooleanLiteral::toJson() const {
+  return nuo::Json()
+      ._("nodeType", "booleanLiteral")
+      ._("value", value)
+      ._("fileRange", fileRange);
+}
+
+} // namespace qat::ast
---
+++ src/ast/expressions/boolean_literal.hpp
@@ -0,0 +1,23 @@
+#ifndef QAT_AST_EXPRESSIONS_BOOLEAN_LITERAL_HPP
+#define QAT_AST_EXPRESSIONS_BOOLEAN_LITERAL_HPP
+
+#include "../expression.hpp"
+#include "llvm/IR/Instructions.h"
+
+namespace qat::ast {
+
+class BooleanLiteral : public Expression {
+private:
+  bool value;
+
+public:
+  BooleanLiteral(bool _value, utils::FileRange _fileRange);
+
+  useit IR::Value *emit(IR::Context *ctx) final;
+  useit nuo::Json toJson() const final;
+  useit NodeType  nodeType() const final { return NodeType::booleanLiteral; }
+};
+
+} // namespace qat::ast
+
+#endif

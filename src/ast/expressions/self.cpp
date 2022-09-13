70728dbc777817817332d7961455af94fdbb54e7
Aldrin Mathew <admin@aldrinmathew.com>
2022-09-13 12:34:37 +0530

    Added emission for `ast::Self`
    - Self is used to get the reference to the parent instance in a member
      function
    
    - This adds significant capabilities to member functions, constructors
      and operators

--- src/ast/expressions/self.cpp
+++ src/ast/expressions/self.cpp
@@ -8,7 +8,25 @@ IR::Value *Self::emit(IR::Context *ctx) {
   if (getExpectedKind() == ExpressionKind::assignable) {
     ctx->Error("Self is not assignable", fileRange);
   }
-  // TODO - Implement this
+  if (ctx->fn) {
+    if (ctx->fn->isMemberFunction()) {
+      auto *mFn = (IR::MemberFunction *)ctx->fn;
+      if (ctx->selfVal) {
+        return new IR::Value(ctx->selfVal,
+                             IR::ReferenceType::get(mFn->isVariationFunction(),
+                                                    mFn->getParentType(),
+                                                    ctx->llctx),
+                             false, IR::Nature::temporary);
+      } else {
+        ctx->Error("Invalid value for self in the current scope", fileRange);
+      }
+    } else {
+      // FIXME - Support extension functions that will have non-core type parent
+      ctx->Error("The current function is not a member function", fileRange);
+    }
+  } else {
+    ctx->Error("No active function in the current scope", fileRange);
+  }
 }
 
 nuo::Json Self::toJson() const {

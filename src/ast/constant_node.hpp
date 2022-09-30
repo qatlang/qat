630d09248673bbff4e19f051531d6099fe38d0d6
Aldrin Mathew <admin@aldrinmathew.com>
2022-09-30 16:26:20 +0530

    Added `ast::ConstantNode`
    - Added a variant of Node to represent constant expressions

---
+++ src/ast/constant_node.hpp
@@ -0,0 +1,23 @@
+#ifndef QAT_AST_CONSTANT_HPP
+#define QAT_AST_CONSTANT_HPP
+
+#include "./node.hpp"
+
+namespace qat::ast {
+
+class ConstantNode : public Node {
+public:
+  ConstantNode(utils::FileRange _fileRange) : Node(std::move(_fileRange)) {}
+
+  void  createModule(IR::Context *ctx) const final {}
+  void  defineType(IR::Context *ctx) final {}
+  void  define(IR::Context *ctx) final {}
+  useit IR::ConstantValue *emit(IR::Context *ctx) override = 0;
+  useit Json               toJson() const override         = 0;
+  useit NodeType           nodeType() const override       = 0;
+  static void              clearAll();
+};
+
+} // namespace qat::ast
+
+#endif

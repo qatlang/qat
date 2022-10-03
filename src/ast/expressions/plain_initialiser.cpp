3ee308a1296e2dca0ef857404a0bd0d6dd8d3b29
Aldrin Mathew <admin@aldrinmathew.com>
2022-10-03 15:34:21 +0530

    Plain initialiser can now use default values
    - If there are no values provided to the plain initialiser, it is
      recognised as the default value of the type
    
    - If the core type does not have default constructor, this will throw an
      error

--- src/ast/expressions/plain_initialiser.cpp
+++ src/ast/expressions/plain_initialiser.cpp
@@ -1,51 +1,72 @@
 #include "./plain_initialiser.hpp"
+#include "../../IR/logic.hpp"
+#include "llvm/IR/Instructions.h"
 
 namespace qat::ast {
 
-PlainInitialiser::PlainInitialiser(QatType                            *_type,
-                                   Vec<Pair<String, utils::FileRange>> _fields,
-                                   Vec<Expression *> _fieldValues,
-                                   utils::FileRange  _fileRange)
-    : Expression(std::move(_fileRange)), type(_type),
-      fields(std::move(_fields)), fieldValues(std::move(_fieldValues)) {}
+PlainInitialiser::PlainInitialiser(QatType* _type, Vec<Pair<String, utils::FileRange>> _fields,
+                                   Vec<Expression*> _fieldValues, utils::FileRange _fileRange)
+    : Expression(std::move(_fileRange)), type(_type), fields(std::move(_fields)), fieldValues(std::move(_fieldValues)) {
+}
 
-IR::Value *PlainInitialiser::emit(IR::Context *ctx) {
+IR::Value* PlainInitialiser::emit(IR::Context* ctx) {
   if (type) {
     auto  reqInfo  = ctx->getReqInfo();
-    auto *typeEmit = type->emit(ctx);
+    auto* typeEmit = type->emit(ctx);
     if (typeEmit->isCoreType()) {
-      auto *cTy = typeEmit->asCore();
-      if (ctx->fn->isMemberFunction()
-              ? (!((IR::MemberFunction *)ctx->fn)->getParentType()->isSame(cTy))
-              : true) {
+      auto* cTy = typeEmit->asCore();
+      if (fieldValues.empty()) {
+        if (cTy->hasDefaultConstructor()) {
+          auto* dFn = cTy->getDefaultConstructor();
+          if (dFn->isAccessible(ctx->getReqInfo())) {
+            llvm::AllocaInst* alloca = nullptr;
+            if (local) {
+              alloca = local->getAlloca();
+            } else if (!irName.empty()) {
+              local  = ctx->fn->getBlock()->newValue(irName, cTy, isVar);
+              alloca = local->getAlloca();
+            } else {
+              alloca = IR::Logic::newAlloca(ctx->fn, utils::unique_id(), cTy->getLLVMType());
+            }
+            (void)dFn->call(ctx, {alloca}, ctx->getMod());
+            if (local) {
+              auto* val = new IR::Value(local->getAlloca(), local->getType(), local->isVariable(), local->getNature());
+              val->setLocalID(local->getLocalID());
+              return val;
+            } else {
+              return new IR::Value(alloca, cTy, true, IR::Nature::pure);
+            }
+          } else {
+            ctx->Error("The default constructor of core type " + ctx->highlightError(cTy->getFullName()) +
+                           " is not accessible here",
+                       fileRange);
+          }
+        }
+      }
+      if (ctx->fn->isMemberFunction() ? (!((IR::MemberFunction*)ctx->fn)->getParentType()->isSame(cTy)) : true) {
         if (cTy->hasAnyConstructor()) {
           ctx->Error("Core type " + ctx->highlightError(cTy->getFullName()) +
                          " have constructors and hence the plain initialiser "
                          "cannot be used for this type",
                      fileRange);
         } else if (cTy->hasAnyFromConvertor()) {
-          ctx->Error(
-              "Core type " + ctx->highlightError(cTy->getFullName()) +
-                  " has convertor constructors and hence the plain initialiser "
-                  "syntax cannot be used for this type",
-              fileRange);
+          ctx->Error("Core type " + ctx->highlightError(cTy->getFullName()) +
+                         " has convertor constructors and hence the plain initialiser "
+                         "syntax cannot be used for this type",
+                     fileRange);
         }
       }
       if (cTy->getMemberCount() != fieldValues.size()) {
-        ctx->Error(
-            "Core type " + ctx->highlightError(cTy->toString()) + " has " +
-                ctx->highlightError(std::to_string(cTy->getMemberCount())) +
-                " members. But there are only " +
-                ctx->highlightError(std::to_string(fieldValues.size())) +
-                " expressions provided",
-            fileRange);
+        ctx->Error("Core type " + ctx->highlightError(cTy->toString()) + " has " +
+                       ctx->highlightError(std::to_string(cTy->getMemberCount())) + " members. But there are only " +
+                       ctx->highlightError(std::to_string(fieldValues.size())) + " expressions provided",
+                   fileRange);
       }
       if (fields.empty()) {
         for (usize i = 0; i < fieldValues.size(); i++) {
-          auto *mem = cTy->getMemberAt(i);
+          auto* mem = cTy->getMemberAt(i);
           if (!mem->visibility.isAccessible(reqInfo)) {
-            ctx->Error("Member " + ctx->highlightError(mem->name) +
-                           " of core type " +
+            ctx->Error("Member " + ctx->highlightError(mem->name) + " of core type " +
                            ctx->highlightError(cTy->getFullName()) +
                            " is not accessible here and hence plain "
                            "initialiser cannot be used for this type",
@@ -58,69 +79,53 @@ IR::Value *PlainInitialiser::emit(IR::Context *ctx) {
           auto fName  = fields.at(i).first;
           auto fRange = fields.at(i).second;
           if (cTy->hasMember(fName)) {
-            auto *mem = cTy->getMemberAt(cTy->getIndexOf(fName).value());
+            auto* mem = cTy->getMemberAt(cTy->getIndexOf(fName).value());
             if (!mem->visibility.isAccessible(reqInfo)) {
-              ctx->Error("The member " + ctx->highlightError(fName) +
-                             " of core type " +
-                             ctx->highlightError(cTy->getFullName()) +
-                             " is not accessible here",
+              ctx->Error("The member " + ctx->highlightError(fName) + " of core type " +
+                             ctx->highlightError(cTy->getFullName()) + " is not accessible here",
                          fRange);
             }
             for (usize j = 0; j != i; j++) {
               if (fields.at(i).first == fields.at(j).first) {
-                ctx->Error("The value for member " +
-                               ctx->highlightError(fields.at(i).first) +
-                               " of core type " +
-                               ctx->highlightError(cTy->toString()) +
-                               " is already provided. Please check logic",
+                ctx->Error("The value for member " + ctx->highlightError(fields.at(i).first) + " of core type " +
+                               ctx->highlightError(cTy->toString()) + " is already provided. Please check logic",
                            fields.at(i).second);
               }
             }
             indices.push_back(cTy->getIndexOf(fields.at(i).first).value());
           } else {
-            ctx->Error("The core type " + ctx->highlightError(cTy->toString()) +
-                           " does not have a member named " +
+            ctx->Error("The core type " + ctx->highlightError(cTy->toString()) + " does not have a member named " +
                            ctx->highlightError(fields.at(i).first),
                        fields.at(i).second);
           }
         }
       }
       // FIXME - Support default values
-      Vec<IR::Value *> irVals;
+      Vec<IR::Value*> irVals;
       for (usize i = 0; i < fieldValues.size(); i++) {
-        auto *fVal  = fieldValues.at(i)->emit(ctx);
-        auto *memTy = cTy->getMemberAt(i)->type;
+        auto* fVal  = fieldValues.at(i)->emit(ctx);
+        auto* memTy = cTy->getMemberAt(i)->type;
         if (fVal->getType()->isSame(memTy)) {
           fVal->loadImplicitPointer(ctx->builder);
           irVals.push_back(fVal);
-        } else if (memTy->isReference() &&
-                   memTy->asReference()->getSubType()->isSame(
-                       fVal->getType()) &&
-                   fVal->isImplicitPointer() &&
-                   (memTy->asReference()->isSubtypeVariable() ==
-                    fVal->isVariable())) {
+        } else if (memTy->isReference() && memTy->asReference()->getSubType()->isSame(fVal->getType()) &&
+                   fVal->isImplicitPointer() && (memTy->asReference()->isSubtypeVariable() == fVal->isVariable())) {
           irVals.push_back(fVal);
-        } else if (fVal->getType()->isReference() &&
-                   fVal->getType()->asReference()->getSubType()->isSame(
-                       memTy)) {
-          fVal = new IR::Value(
-              ctx->builder.CreateLoad(memTy->getLLVMType(), fVal->getLLVM()),
-              memTy, false, IR::Nature::temporary);
+        } else if (fVal->getType()->isReference() && fVal->getType()->asReference()->getSubType()->isSame(memTy)) {
+          fVal = new IR::Value(ctx->builder.CreateLoad(memTy->getLLVMType(), fVal->getLLVM()), memTy, false,
+                               IR::Nature::temporary);
           irVals.push_back(fVal);
         } else {
           ctx->Error("This expression does not match the type of the "
                      "corresponding member " +
-                         ctx->highlightError(cTy->getMemberNameAt(i)) +
-                         " of core type " +
-                         ctx->highlightError(cTy->toString()) +
-                         ". The member is of type " +
-                         ctx->highlightError(memTy->toString()) +
-                         ", but the expression is of type " +
+                         ctx->highlightError(cTy->getMemberNameAt(i)) + " of core type " +
+                         ctx->highlightError(cTy->toString()) + ". The member is of type " +
+                         ctx->highlightError(memTy->toString()) + ", but the expression is of type " +
                          ctx->highlightError(fVal->getType()->toString()),
                      fieldValues.at(i)->fileRange);
         }
       }
-      llvm::AllocaInst *alloca;
+      llvm::AllocaInst* alloca;
       if (local) {
         alloca = local->getAlloca();
       } else if (!irName.empty()) {
@@ -130,21 +135,18 @@ IR::Value *PlainInitialiser::emit(IR::Context *ctx) {
         alloca = ctx->builder.CreateAlloca(cTy->getLLVMType(), 0u);
       }
       for (usize i = 0; i < irVals.size(); i++) {
-        auto *memPtr = ctx->builder.CreateStructGEP(cTy->getLLVMType(), alloca,
-                                                    indices.at(i));
+        auto* memPtr = ctx->builder.CreateStructGEP(cTy->getLLVMType(), alloca, indices.at(i));
         ctx->builder.CreateStore(irVals.at(i)->getLLVM(), memPtr);
       }
       if (local) {
-        return new IR::Value(local->getAlloca(), local->getType(),
-                             local->isVariable(), local->getNature());
+        return new IR::Value(local->getAlloca(), local->getType(), local->isVariable(), local->getNature());
       } else {
         return new IR::Value(alloca, cTy, true, IR::Nature::pure);
       }
     } else {
-      ctx->Error(
-          "This type is not a core type and hence cannot be used in a plain "
-          "initialiser",
-          type->fileRange);
+      ctx->Error("This type is not a core type and hence cannot be used in a plain "
+                 "initialiser",
+                 type->fileRange);
     }
   } else {
     ctx->Error("No type provided for plain initialisation", fileRange);
@@ -153,10 +155,7 @@ IR::Value *PlainInitialiser::emit(IR::Context *ctx) {
 }
 
 Json PlainInitialiser::toJson() const {
-  return Json()
-      ._("nodeType", "plainInitialiser")
-      ._("type", type->toJson())
-      ._("fileRange", fileRange);
+  return Json()._("nodeType", "plainInitialiser")._("type", type->toJson())._("fileRange", fileRange);
 }
 
 } // namespace qat::ast

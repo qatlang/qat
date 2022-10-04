5b17bb70d19a5b37e1b5c89ac56602a38ca7f250
Aldrin Mathew <admin@aldrinmathew.com>
2022-10-05 00:27:49 +0530

    Added type inference to function call
    - Updated ast::FunctionCall with type inference. The following are the
      additions
    
    - Added support for responsive signed and unsigned integer literals
    
    - Added support for null pointers
    
    - Added support for default values

--- src/ast/expressions/function_call.cpp
+++ src/ast/expressions/function_call.cpp
@@ -1,57 +1,73 @@
 #include "./function_call.hpp"
+#include "../constants/integer_literal.hpp"
+#include "../constants/null_pointer.hpp"
+#include "../constants/unsigned_literal.hpp"
+#include "default.hpp"
 
 namespace qat::ast {
 
-FunctionCall::FunctionCall(Expression *_fnExpr, Vec<Expression *> _arguments,
-                           utils::FileRange _fileRange)
-    : Expression(std::move(_fileRange)), fnExpr(_fnExpr),
-      values(std::move(_arguments)) {}
+FunctionCall::FunctionCall(Expression* _fnExpr, Vec<Expression*> _arguments, utils::FileRange _fileRange)
+    : Expression(std::move(_fileRange)), fnExpr(_fnExpr), values(std::move(_arguments)) {}
 
-IR::Value *FunctionCall::emit(IR::Context *ctx) {
-  auto *mod   = ctx->getMod();
-  auto *fnVal = fnExpr->emit(ctx);
+IR::Value* FunctionCall::emit(IR::Context* ctx) {
+  auto* mod   = ctx->getMod();
+  auto* fnVal = fnExpr->emit(ctx);
   if (fnVal && fnVal->getType()->isFunction()) {
-    auto *fun = (IR::Function *)fnVal;
-    if (fun->getType()->asFunction()->getArgumentTypes().size() !=
-        values.size()) {
+    auto* fun = (IR::Function*)fnVal;
+    if (fun->getType()->asFunction()->getArgumentTypes().size() != values.size()) {
       ctx->Error("Number of arguments provided for the function call does not "
                  "match the function signature",
                  fileRange);
     }
-    Vec<IR::Value *> argsEmit;
-    for (auto *val : values) {
-      argsEmit.push_back(val->emit(ctx));
+    Vec<IR::Value*> argsEmit;
+    auto*           fnTy = fun->getType()->asFunction();
+    for (usize i = 0; i < values.size(); i++) {
+      if (fnTy->getArgumentCount() > (u64)i) {
+        auto* argTy = fnTy->getArgumentTypeAt(i)->getType();
+        if (values.at(i)->nodeType() == NodeType::integerLiteral) {
+          if (argTy->isInteger() || argTy->isUnsignedInteger()) {
+            ((IntegerLiteral*)values.at(i))->setType(argTy);
+          }
+        } else if (values.at(i)->nodeType() == NodeType::unsignedLiteral) {
+          if (argTy->isInteger() || argTy->isUnsignedInteger()) {
+            ((UnsignedLiteral*)values.at(i))->setType(argTy);
+          }
+        } else if (values.at(i)->nodeType() == NodeType::nullPointer) {
+          if (argTy->isPointer()) {
+            ((NullPointer*)values.at(i))
+                ->setType(argTy->asPointer()->isSubtypeVariable(), argTy->asPointer()->getSubType());
+          } else {
+            ctx->Error("The expression provided does not match the type of the argument", values.at(i)->fileRange);
+          }
+        } else if (values.at(i)->nodeType() == NodeType::Default) {
+          ((Default*)values.at(i))->setType(argTy);
+        }
+      }
+      argsEmit.push_back(values.at(i)->emit(ctx));
     }
     SHOW("Argument values generated")
     auto fnArgsTy = fnVal->getType()->asFunction()->getArgumentTypes();
     for (usize i = 0; i < fnArgsTy.size(); i++) {
       if (!fnArgsTy.at(i)->getType()->isSame(argsEmit.at(i)->getType()) ||
           (argsEmit.at(i)->getType()->isReference() &&
-           !fnArgsTy.at(i)->getType()->isSame(
-               argsEmit.at(i)->getType()->asReference()->getSubType()))) {
+           !fnArgsTy.at(i)->getType()->isSame(argsEmit.at(i)->getType()->asReference()->getSubType()))) {
         ctx->Error("Type of this expression does not match the type of the "
                    "corresponding argument at " +
-                       ctx->highlightError(std::to_string(i)) +
-                       " of the function " +
+                       ctx->highlightError(std::to_string(i)) + " of the function " +
                        ctx->highlightError(fun->getName()),
                    values.at(i)->fileRange);
       }
     }
-    Vec<llvm::Value *> argValues;
+    Vec<llvm::Value*> argValues;
     for (usize i = 0; i < fnArgsTy.size(); i++) {
-      if (fnArgsTy.at(i)->getType()->isReference() &&
-          !argsEmit.at(i)->isReference()) {
+      if (fnArgsTy.at(i)->getType()->isReference() && !argsEmit.at(i)->isReference()) {
         if (!argsEmit.at(i)->isImplicitPointer()) {
-          ctx->Error(
-              "Cannot pass a value for the argument that expects a reference",
-              values.at(i)->fileRange);
+          ctx->Error("Cannot pass a value for the argument that expects a reference", values.at(i)->fileRange);
         }
       } else if (argsEmit.at(i)->isReference()) {
-        argsEmit.at(i) = new IR::Value(
-            ctx->builder.CreateLoad(argsEmit.at(i)->getType()->getLLVMType(),
-                                    argsEmit.at(i)->getLLVM()),
-            argsEmit.at(i)->getType(), argsEmit.at(i)->isVariable(),
-            argsEmit.at(i)->getNature());
+        argsEmit.at(i) =
+            new IR::Value(ctx->builder.CreateLoad(argsEmit.at(i)->getType()->getLLVMType(), argsEmit.at(i)->getLLVM()),
+                          argsEmit.at(i)->getType(), argsEmit.at(i)->isVariable(), argsEmit.at(i)->getNature());
       } else {
         argsEmit.at(i)->loadImplicitPointer(ctx->builder);
       }
@@ -59,16 +75,14 @@ IR::Value *FunctionCall::emit(IR::Context *ctx) {
     }
     return fun->call(ctx, argValues, mod);
   } else {
-    ctx->Error("The expression is not callable. It has type " +
-                   fnVal->getType()->toString(),
-               fnExpr->fileRange);
+    ctx->Error("The expression is not callable. It has type " + fnVal->getType()->toString(), fnExpr->fileRange);
   }
   return nullptr;
 }
 
 Json FunctionCall::toJson() const {
   Vec<JsonValue> args;
-  for (auto *arg : values) {
+  for (auto* arg : values) {
     args.emplace_back(arg->toJson());
   }
   return Json()

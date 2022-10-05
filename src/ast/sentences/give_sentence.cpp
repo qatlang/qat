86681347374de552107e030b9932d186601f8909
Aldrin Mathew <admin@aldrinmathew.com>
2022-10-05 16:09:59 +0530

    Updated `ast::GiveSentence` with type inference
    - Added support for responsive signed or unsigned integer literals
    
    - Added support for null pointer
    
    - Added support for default values

--- src/ast/sentences/give_sentence.cpp
+++ src/ast/sentences/give_sentence.cpp
@@ -1,61 +1,71 @@
 #include "./give_sentence.hpp"
+#include "../constants/integer_literal.hpp"
+#include "../constants/null_pointer.hpp"
+#include "../constants/unsigned_literal.hpp"
+#include "../expressions/default.hpp"
+
 namespace qat::ast {
 
-GiveSentence::GiveSentence(Maybe<Expression *> _given_expr,
-                           utils::FileRange    _fileRange)
+GiveSentence::GiveSentence(Maybe<Expression*> _given_expr, utils::FileRange _fileRange)
     : Sentence(std::move(_fileRange)), give_expr(_given_expr) {}
 
-IR::Value *GiveSentence::emit(IR::Context *ctx) {
-  auto *fun = ctx->fn;
-  if (fun->getType()->asFunction()->getReturnType()->typeKind() ==
-      IR::TypeKind::Void) {
+IR::Value* GiveSentence::emit(IR::Context* ctx) {
+  auto* fun = ctx->fn;
+  if (fun->getType()->asFunction()->getReturnType()->typeKind() == IR::TypeKind::Void) {
     if (give_expr.has_value()) {
       ctx->Error("Given value type of the function is void. Please remove this "
                  "unnecessary value",
                  give_expr.value()->fileRange);
     } else {
-      if (fun->isMemberFunction() &&
-          ((IR::MemberFunction *)fun)->getMemberFnType() ==
-              IR::MemberFnType::destructor) {
-        auto *mFn = (IR::MemberFunction *)fun;
-        auto *cTy = mFn->getParentType();
+      if (fun->isMemberFunction() && ((IR::MemberFunction*)fun)->getMemberFnType() == IR::MemberFnType::destructor) {
+        auto* mFn = (IR::MemberFunction*)fun;
+        auto* cTy = mFn->getParentType();
         for (usize i = 0; i < cTy->getMemberCount(); i++) {
           if (cTy->getMemberAt(i)->type->isCoreType()) {
-            auto *memPtr = ctx->builder.CreateStructGEP(
-                mFn->getParentType()->getLLVMType(), ctx->selfVal, i);
-            auto *destructor = cTy->getDestructor();
+            auto* memPtr     = ctx->builder.CreateStructGEP(mFn->getParentType()->getLLVMType(), ctx->selfVal, i);
+            auto* destructor = cTy->getDestructor();
             (void)destructor->call(ctx, {memPtr}, ctx->getMod());
           }
         }
       }
-      Vec<IR::LocalValue *> locals;
+      Vec<IR::LocalValue*> locals;
       fun->getBlock()->collectLocalValues(locals);
-      for (auto *loc : locals) {
+      for (auto* loc : locals) {
         if (loc->getType()->isCoreType()) {
-          auto *cTy        = loc->getType()->asCore();
-          auto *destructor = cTy->getDestructor();
+          auto* cTy        = loc->getType()->asCore();
+          auto* destructor = cTy->getDestructor();
           (void)destructor->call(ctx, {loc->getAlloca()}, ctx->getMod());
         }
       }
       locals.clear();
-      return new IR::Value(ctx->builder.CreateRetVoid(),
-                           IR::VoidType::get(ctx->llctx), false,
-                           IR::Nature::pure);
+      return new IR::Value(ctx->builder.CreateRetVoid(), IR::VoidType::get(ctx->llctx), false, IR::Nature::pure);
     }
   } else {
     if (give_expr.has_value()) {
-      auto *retVal  = give_expr.value()->emit(ctx);
-      auto *retType = fun->getType()->asFunction()->getReturnType();
+      auto* retType = fun->getType()->asFunction()->getReturnType();
+      if (give_expr.value()->nodeType() == NodeType::Default) {
+        ((Default*)give_expr.value())->setType(retType);
+      }
+      if (retType->isInteger() || retType->isUnsignedInteger()) {
+        if (give_expr.value()->nodeType() == NodeType::integerLiteral) {
+          ((IntegerLiteral*)give_expr.value())->setType(retType);
+        } else if (give_expr.value()->nodeType() == NodeType::unsignedLiteral) {
+          ((UnsignedLiteral*)give_expr.value())->setType(retType);
+        }
+      } else if (retType->isPointer()) {
+        if (give_expr.value()->nodeType() == NodeType::nullPointer) {
+          ((NullPointer*)give_expr.value())
+              ->setType(retType->asPointer()->isSubtypeVariable(), retType->asPointer()->getSubType());
+        }
+      }
+      auto* retVal = give_expr.value()->emit(ctx);
       if (retType->isSame(retVal->getType()) ||
-          (retType->isReference() &&
-           retType->asReference()->getSubType()->isSame(retVal->getType()) &&
+          (retType->isReference() && retType->asReference()->getSubType()->isSame(retVal->getType()) &&
            retVal->isImplicitPointer()) ||
-          (retVal->getType()->isReference() &&
-           retVal->getType()->asReference()->getSubType()->isSame(retType))) {
+          (retVal->getType()->isReference() && retVal->getType()->asReference()->getSubType()->isSame(retType))) {
         if (retVal->getType()->isReference() && !(retType->isReference())) {
-          retVal = new IR::Value(ctx->builder.CreateLoad(retType->getLLVMType(),
-                                                         retVal->getLLVM()),
-                                 retType, false, IR::Nature::temporary);
+          retVal = new IR::Value(ctx->builder.CreateLoad(retType->getLLVMType(), retVal->getLLVM()), retType, false,
+                                 IR::Nature::temporary);
         } else if (retVal->isImplicitPointer() && !retType->isReference()) {
           retVal->loadImplicitPointer(ctx->builder);
         }
@@ -65,25 +75,21 @@ IR::Value *GiveSentence::emit(IR::Context *ctx) {
                        "the function call is complete",
                        fileRange);
         }
-        Vec<IR::LocalValue *> locals;
+        Vec<IR::LocalValue*> locals;
         fun->getBlock()->collectLocalValues(locals);
-        for (auto *loc : locals) {
+        for (auto* loc : locals) {
           if (loc->getType()->isCoreType()) {
-            auto *cTy        = loc->getType()->asCore();
-            auto *destructor = cTy->getDestructor();
+            auto* cTy        = loc->getType()->asCore();
+            auto* destructor = cTy->getDestructor();
             (void)destructor->call(ctx, {loc->getAlloca()}, ctx->getMod());
           }
         }
         locals.clear();
-        return new IR::Value(ctx->builder.CreateRet(retVal->getLLVM()),
-                             retVal->getType(), false, IR::Nature::pure);
+        return new IR::Value(ctx->builder.CreateRet(retVal->getLLVM()), retVal->getType(), false, IR::Nature::pure);
       } else {
-        ctx->Error(
-            "Given value type of the function is " +
-                fun->getType()->asFunction()->getReturnType()->toString() +
-                ", but the provided value in the give sentence is " +
-                retVal->getType()->toString(),
-            give_expr.value()->fileRange);
+        ctx->Error("Given value type of the function is " + fun->getType()->asFunction()->getReturnType()->toString() +
+                       ", but the provided value in the give sentence is " + retVal->getType()->toString(),
+                   give_expr.value()->fileRange);
       }
     } else {
       ctx->Error("No value is provided for the give sentence. Please provide a "

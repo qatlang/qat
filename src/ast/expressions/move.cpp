246caa126f44179f6d5e10f3c16a66c85f308e35
Aldrin Mathew <admin@aldrinmathew.com>
2022-10-30 11:16:17 +0530

    Updated `ast::Move` for `maybe` types
    - Manual move has been updated to support maybe types. There is an
      optimisation to avoid multiple allocations in local declarations
      with a maybe type

--- src/ast/expressions/move.cpp
+++ src/ast/expressions/move.cpp
@@ -1,5 +1,6 @@
 #include "./move.hpp"
 #include "../../IR/logic.hpp"
+#include "../../IR/types/maybe.hpp"
 
 namespace qat::ast {
 
@@ -7,18 +8,20 @@ Move::Move(Expression* _exp, utils::FileRange _fileRange) : Expression(std::move
 
 IR::Value* Move::emit(IR::Context* ctx) {
   auto* expEmit = exp->emit(ctx);
-  if ((expEmit->getType()->isCoreType() && expEmit->isImplicitPointer() && expEmit->isVariable()) ||
-      (expEmit->getType()->isReference() && expEmit->getType()->asReference()->getSubType()->isCoreType() &&
-       expEmit->getType()->asReference()->isSubtypeVariable())) {
+  if ((expEmit->getType()->isCoreType() && expEmit->isImplicitPointer()) ||
+      (expEmit->getType()->isReference() && expEmit->getType()->asReference()->getSubType()->isCoreType())) {
     auto* cTy = expEmit->isReference() ? expEmit->getType()->asReference()->getSubType()->asCore()
                                        : expEmit->getType()->asCore();
     if (cTy->hasMoveConstructor()) {
-      llvm::AllocaInst* alloca = nullptr;
+      llvm::Value* alloca = nullptr;
       if (local) {
-        if (!cTy->isSame(local->getType())) {
+        if (!cTy->isSame(local->getType()) &&
+            (local->getType()->isMaybe() && !cTy->isSame(local->getType()->asMaybe()->getSubType()))) {
           ctx->Error("The type provided in the local declaration does not match the type to be moved", fileRange);
         }
-        alloca = local->getAlloca();
+        alloca = local->getType()->isMaybe()
+                     ? ctx->builder.CreateStructGEP(local->getType()->getLLVMType(), local->getAlloca(), 1u)
+                     : local->getAlloca();
       } else if (irName.has_value()) {
         SHOW("Created local for move from name")
         local  = ctx->fn->getBlock()->newValue(irName.value(), cTy, isVar);
@@ -28,6 +31,11 @@ IR::Value* Move::emit(IR::Context* ctx) {
       }
       (void)(cTy->getMoveConstructor()->call(ctx, {alloca, expEmit->getLLVM()}, ctx->getMod()));
       if (local) {
+        if (local->getType()->isMaybe()) {
+          ctx->builder.CreateStore(
+              llvm::ConstantInt::get(llvm::Type::getInt1Ty(ctx->llctx), 1u),
+              ctx->builder.CreateStructGEP(local->getType()->getLLVMType(), local->getAlloca(), 0u));
+        }
         auto* val = new IR::Value(local->getAlloca(), local->getType(), local->isVariable(), local->getNature());
         val->setLocalID(local->getLocalID());
         return val;

5bba93a7a5b45b596ad8b1c21c0c7fd5b9da5aec
Aldrin Mathew <admin@aldrinmathew.com>
2022-11-08 11:32:29 +0530

    Added `maybe` support in assignments
    - Assignments now supports maybe types, with proper copy & move semantics
      support
    
    - This took a significant amount of time and effort to achieve
    
    - maybe of core types has proper support for copy & move construction, and
      copy & move assignment

--- src/ast/sentences/assignment.cpp
+++ src/ast/sentences/assignment.cpp
@@ -1,11 +1,16 @@
 #include "./assignment.hpp"
+#include "../../IR/control_flow.hpp"
+#include "../../IR/function.hpp"
 #include "../../IR/logic.hpp"
+#include "../../IR/types/maybe.hpp"
 #include "../constants/integer_literal.hpp"
 #include "../constants/null_pointer.hpp"
 #include "../constants/unsigned_literal.hpp"
 #include "../expressions/copy.hpp"
 #include "../expressions/default.hpp"
 #include "../expressions/move.hpp"
+#include "../expressions/none.hpp"
+#include "llvm/IR/Constants.h"
 #include <cerrno>
 
 namespace qat::ast {
@@ -25,6 +30,14 @@ IR::Value* Assignment::emit(IR::Context* ctx) {
     } else if (lhsVal->getType()->isReference() && lhsVal->getType()->asReference()->getSubType()->isPointer()) {
       nullVal->setType(lhsVal->getType()->asReference()->getSubType()->asPointer()->isSubtypeVariable(),
                        lhsVal->getType()->asReference()->getSubType()->asPointer()->getSubType());
+    } else if (lhsVal->getType()->isMaybe() && lhsVal->getType()->asMaybe()->getSubType()->isPointer()) {
+      nullVal->setType(lhsVal->getType()->asMaybe()->getSubType()->asPointer()->isSubtypeVariable(),
+                       lhsVal->getType()->asMaybe()->getSubType());
+    } else if (lhsVal->getType()->isReference() && lhsVal->getType()->asReference()->getSubType()->isMaybe() &&
+               lhsVal->getType()->asReference()->getSubType()->asMaybe()->getSubType()->isPointer()) {
+      nullVal->setType(
+          lhsVal->getType()->asReference()->getSubType()->asMaybe()->getSubType()->asPointer()->isSubtypeVariable(),
+          lhsVal->getType()->asReference()->getSubType()->asMaybe()->getSubType());
     } else {
       ctx->Error("Type of the LHS is not compatible with the RHS, which is a "
                  "null pointer",
@@ -48,6 +61,13 @@ IR::Value* Assignment::emit(IR::Context* ctx) {
       uLit->setType(lhsVal->getType());
     } else if (lhsVal->getType()->isUnsignedInteger()) {
       uLit->setType(lhsVal->getType());
+    } else if (lhsVal->getType()->isMaybe() && (lhsVal->getType()->asMaybe()->getSubType()->isInteger() ||
+                                                lhsVal->getType()->asMaybe()->getSubType()->isUnsignedInteger())) {
+      uLit->setType(lhsVal->getType()->asMaybe()->getSubType());
+    } else if (lhsVal->getType()->isReference() && lhsVal->getType()->asReference()->getSubType()->isMaybe() &&
+               (lhsVal->getType()->asReference()->getSubType()->asMaybe()->getSubType()->isInteger() ||
+                lhsVal->getType()->asReference()->getSubType()->asMaybe()->getSubType()->isUnsignedInteger())) {
+      uLit->setType(lhsVal->getType()->asReference()->getSubType()->asMaybe()->getSubType());
     } else {
       ctx->Error("Type of LHS is not compatible with the RHS", fileRange);
     }
@@ -62,13 +82,234 @@ IR::Value* Assignment::emit(IR::Context* ctx) {
       iLit->setType(lhsVal->getType());
     } else if (lhsVal->getType()->isUnsignedInteger()) {
       iLit->setType(lhsVal->getType());
+    } else if (lhsVal->getType()->isMaybe() && (lhsVal->getType()->asMaybe()->getSubType()->isInteger() ||
+                                                lhsVal->getType()->asMaybe()->getSubType()->isUnsignedInteger())) {
+      iLit->setType(lhsVal->getType()->asMaybe()->getSubType());
+    } else if (lhsVal->getType()->isReference() && lhsVal->getType()->asReference()->getSubType()->isMaybe() &&
+               (lhsVal->getType()->asReference()->getSubType()->asMaybe()->getSubType()->isInteger() ||
+                lhsVal->getType()->asReference()->getSubType()->asMaybe()->getSubType()->isUnsignedInteger())) {
+      iLit->setType(lhsVal->getType()->asReference()->getSubType()->asMaybe()->getSubType());
     } else {
       ctx->Error("Type of LHS is not compatible with the RHS", fileRange);
     }
   }
   SHOW("Emitted lhs and rhs of Assignment")
-  if (lhsVal->isVariable() ||
-      (lhsVal->getType()->isReference() && lhsVal->getType()->asReference()->isSubtypeVariable())) {
+  if (lhsVal->getType()->isMaybe() ||
+      (lhsVal->getType()->isReference() && lhsVal->getType()->asReference()->getSubType()->isMaybe())) {
+    IR::MaybeType* mTy = nullptr;
+    if (lhsVal->getType()->isMaybe()) {
+      mTy = lhsVal->getType()->asMaybe();
+      if (lhsVal->isImplicitPointer()) {
+        if (!lhsVal->isVariable()) {
+          ctx->Error("Left hand side does not have variability and hence cannot be assigned to", fileRange);
+        }
+      } else {
+        ctx->Error(
+            "The expression is a value and hence cannot be reassigned. Assignment requires an address on the left hand side",
+            fileRange);
+      }
+    } else {
+      mTy = lhsVal->getType()->asReference()->getSubType()->asMaybe();
+      if (lhsVal->getType()->asReference()->isSubtypeVariable()) {
+        lhsVal->loadImplicitPointer(ctx->builder);
+      } else {
+        ctx->Error("The left hand side is a reference without variability and hence cannot be assigned to", fileRange);
+      }
+    }
+    llvm::Value* maybeTagLhsRef   = ctx->builder.CreateStructGEP(mTy->getLLVMType(), lhsVal->getLLVM(), 0u);
+    llvm::Value* maybeValueLhsRef = ctx->builder.CreateStructGEP(mTy->getLLVMType(), lhsVal->getLLVM(), 1u);
+    auto*        subTy            = mTy->getSubType();
+    if (value->nodeType() == NodeType::none || value->nodeType() == NodeType::Default) {
+      if (value->nodeType() == NodeType::none) {
+        auto* noneAST = ((ast::NoneExpression*)value);
+        if (noneAST->type) {
+          auto* noneTy = noneAST->type->emit(ctx);
+          if (!subTy->isSame(noneTy)) {
+            ctx->Error("The type of the none expression is " + ctx->highlightError(noneTy->toString()) +
+                           " but the left hand side is maybe type, with " + ctx->highlightError(subTy->toString()) +
+                           " as the sub type",
+                       value->fileRange);
+          }
+        }
+      }
+      if (subTy->isCoreType() && subTy->asCore()->hasDestructor()) {
+        auto* tagTrueBlock = new IR::Block(ctx->fn, ctx->fn->getBlock());
+        auto* restBlock    = new IR::Block(ctx->fn, ctx->fn->getBlock()->getParent());
+        restBlock->linkPrevBlock(ctx->fn->getBlock());
+        ctx->builder.CreateCondBr(
+            ctx->builder.CreateICmpEQ(ctx->builder.CreateLoad(llvm::Type::getInt1Ty(ctx->llctx), maybeTagLhsRef),
+                                      llvm::ConstantInt::get(llvm::Type::getInt1Ty(ctx->llctx), 1u)),
+            tagTrueBlock->getBB(), restBlock->getBB());
+        tagTrueBlock->setActive(ctx->builder);
+        auto* dstrFn = subTy->asCore()->getDestructor();
+        (void)dstrFn->call(ctx, {maybeValueLhsRef}, ctx->getMod());
+        ctx->builder.CreateBr(restBlock->getBB());
+        restBlock->setActive(ctx->builder);
+      }
+      ctx->builder.CreateStore(llvm::ConstantInt::get(llvm::Type::getInt1Ty(ctx->llctx), 0u), maybeTagLhsRef);
+      // FIXME - Storing null for the value is necessary only if there is no destructor
+      ctx->builder.CreateStore(llvm::Constant::getNullValue(subTy->getLLVMType()), maybeValueLhsRef);
+      return nullptr;
+    } else {
+      auto* exp = value->emit(ctx);
+      if ((exp->getType()->isMaybe() && mTy->getSubType()->isSame(exp->getType()->asMaybe()->getSubType()) &&
+           exp->isImplicitPointer()) ||
+          (exp->getType()->isReference() && exp->getType()->asReference()->getSubType()->isMaybe() &&
+           mTy->getSubType()->isSame(exp->getType()->asReference()->getSubType()->asMaybe()->getSubType()))) {
+        // NOTE - Both are references
+        if (subTy->isCoreType()) {
+          auto* cTy         = subTy->asCore();
+          auto* falseBool   = llvm::ConstantInt::get(llvm::Type::getInt1Ty(ctx->llctx), 0u);
+          auto* trueBool    = llvm::ConstantInt::get(llvm::Type::getInt1Ty(ctx->llctx), 1u);
+          auto* maybeTagLhs = ctx->builder.CreateLoad(llvm::Type::getInt1Ty(ctx->llctx), maybeTagLhsRef);
+          auto* maybeTagRhs = ctx->builder.CreateLoad(
+              llvm::Type::getInt1Ty(ctx->llctx), ctx->builder.CreateStructGEP(mTy->getLLVMType(), exp->getLLVM(), 0u));
+          auto* maybeValueRhsRef = ctx->builder.CreateStructGEP(mTy->getLLVMType(), exp->getLLVM(), 1u);
+          auto* currBlock        = ctx->fn->getBlock();
+          auto* condBothTrue     = ctx->builder.CreateAnd(
+              {ctx->builder.CreateICmpEQ(maybeTagLhs, trueBool), ctx->builder.CreateICmpEQ(maybeTagRhs, trueBool)});
+          auto* firstTrueBlock  = new IR::Block(ctx->fn, currBlock);
+          auto* firstFalseBlock = new IR::Block(ctx->fn, currBlock);
+          ctx->builder.CreateCondBr(condBothTrue, firstTrueBlock->getBB(), firstFalseBlock->getBB());
+          firstTrueBlock->setActive(ctx->builder);
+          if (cTy->hasCopyAssignment() || cTy->hasMoveAssignment()) {
+            IR::Function* candFn = nullptr;
+            if (cTy->hasCopyAssignment()) {
+              candFn = cTy->getCopyAssignment();
+            } else {
+              candFn = cTy->getMoveAssignment();
+            }
+            (void)candFn->call(ctx, {maybeValueLhsRef, maybeValueRhsRef}, ctx->getMod());
+          } else {
+            ctx->builder.CreateStore(ctx->builder.CreateLoad(cTy->getLLVMType(), maybeValueRhsRef), maybeValueLhsRef);
+          }
+          (void)IR::addBranch(ctx->builder, firstFalseBlock->getBB());
+          firstFalseBlock->setActive(ctx->builder);
+          auto* condTrueAndFalse = ctx->builder.CreateAnd(
+              {ctx->builder.CreateICmpEQ(maybeTagLhs, trueBool), ctx->builder.CreateICmpEQ(maybeTagRhs, falseBool)});
+          auto* secTrueBlock  = new IR::Block(ctx->fn, currBlock);
+          auto* secFalseBlock = new IR::Block(ctx->fn, currBlock);
+          ctx->builder.CreateCondBr(condTrueAndFalse, secTrueBlock->getBB(), secFalseBlock->getBB());
+          secTrueBlock->setActive(ctx->builder);
+          if (cTy->hasDestructor()) {
+            (void)cTy->getDestructor()->call(ctx, {maybeTagLhsRef}, ctx->getMod());
+          }
+          ctx->builder.CreateStore(falseBool, maybeTagLhsRef);
+          // FIXME - Storing null for the value is necessary only if there is no destructor
+          ctx->builder.CreateStore(llvm::Constant::getNullValue(cTy->getLLVMType()), maybeValueLhsRef);
+          (void)IR::addBranch(ctx->builder, secFalseBlock->getBB());
+          secFalseBlock->setActive(ctx->builder);
+          auto* condFalseAndTrue = ctx->builder.CreateAnd(
+              {ctx->builder.CreateICmpEQ(maybeTagLhs, falseBool), ctx->builder.CreateICmpEQ(maybeTagRhs, trueBool)});
+          auto* thirdTrueBlock = new IR::Block(ctx->fn, currBlock);
+          auto* restBlock      = new IR::Block(ctx->fn, currBlock->getParent());
+          restBlock->linkPrevBlock(currBlock);
+          ctx->builder.CreateCondBr(condFalseAndTrue, thirdTrueBlock->getBB(), restBlock->getBB());
+          thirdTrueBlock->setActive(ctx->builder);
+          if (cTy->hasCopyConstructor() || cTy->hasMoveConstructor()) {
+            IR::Function* candFn = nullptr;
+            if (cTy->hasCopyConstructor()) {
+              candFn = cTy->getCopyConstructor();
+            } else {
+              candFn = cTy->getMoveConstructor();
+            }
+            (void)candFn->call(ctx, {maybeValueLhsRef, maybeValueRhsRef}, ctx->getMod());
+          } else {
+            ctx->builder.CreateStore(ctx->builder.CreateLoad(cTy->getLLVMType(), maybeValueRhsRef), maybeValueLhsRef);
+          }
+          ctx->builder.CreateStore(trueBool, maybeTagLhsRef);
+          (void)IR::addBranch(ctx->builder, restBlock->getBB());
+          restBlock->setActive(ctx->builder);
+        } else {
+          if (exp->isReference()) {
+            exp->loadImplicitPointer(ctx->builder);
+            exp = new IR::Value(
+                ctx->builder.CreateLoad(exp->getType()->asReference()->getSubType()->getLLVMType(), exp->getLLVM()),
+                exp->getType()->asReference()->getSubType(), exp->getType()->asReference()->isSubtypeVariable(),
+                IR::Nature::temporary);
+          } else {
+            exp->loadImplicitPointer(ctx->builder);
+          }
+          ctx->builder.CreateStore(exp->getLLVM(), lhsVal->getLLVM());
+        }
+        return nullptr;
+      } else if (exp->getType()->isMaybe() && exp->getType()->asMaybe()->getSubType()->isSame(mTy->getSubType())) {
+        // NOTE - Assigning absolute value to the reference
+        if (subTy->isCoreType() && subTy->asCore()->hasDestructor()) {
+          auto* condLhsTrue =
+              ctx->builder.CreateICmpEQ(ctx->builder.CreateLoad(llvm::Type::getInt1Ty(ctx->llctx), maybeTagLhsRef),
+                                        llvm::ConstantInt::get(llvm::Type::getInt1Ty(ctx->llctx), 1u));
+          auto* trueBlock = new IR::Block(ctx->fn, ctx->fn->getBlock());
+          auto* restBlock = new IR::Block(ctx->fn, ctx->fn->getBlock()->getParent());
+          restBlock->linkPrevBlock(ctx->fn->getBlock());
+          ctx->builder.CreateCondBr(condLhsTrue, trueBlock->getBB(), restBlock->getBB());
+          trueBlock->setActive(ctx->builder);
+          (void)subTy->asCore()->getDestructor()->call(
+              ctx, {ctx->builder.CreateStructGEP(mTy->getLLVMType(), lhsVal->getLLVM(), 1u)}, ctx->getMod());
+          (void)IR::addBranch(ctx->builder, restBlock->getBB());
+          restBlock->setActive(ctx->builder);
+        }
+        // FIXME - Automatically allocate the value and perform copy/move?
+        ctx->builder.CreateStore(exp->getLLVM(), lhsVal->getLLVM());
+      } else if (exp->getType()->isSame(mTy->getSubType()) ||
+                 (exp->getType()->isReference() &&
+                  exp->getType()->asReference()->getSubType()->isSame(mTy->getSubType()))) {
+        if (exp->getType()->isReference()) {
+          exp->loadImplicitPointer(ctx->builder);
+        }
+        if (subTy->isCoreType() &&
+            ((exp->getType()->isSame(mTy->getSubType()) && exp->isImplicitPointer()) || exp->isReference()) &&
+            (subTy->asCore()->hasCopy() || subTy->asCore()->hasMove() || subTy->asCore()->hasDestructor())) {
+          auto* cTy = subTy->asCore();
+          auto* condLhsTrue =
+              ctx->builder.CreateICmpEQ(ctx->builder.CreateLoad(llvm::Type::getInt1Ty(ctx->llctx), maybeTagLhsRef),
+                                        llvm::ConstantInt::get(llvm::Type::getInt1Ty(ctx->llctx), 1u));
+          auto* trueBlock  = new IR::Block(ctx->fn, ctx->fn->getBlock());
+          auto* falseBlock = new IR::Block(ctx->fn, ctx->fn->getBlock());
+          auto* restBlock  = new IR::Block(ctx->fn, ctx->fn->getBlock());
+          restBlock->linkPrevBlock(ctx->fn->getBlock());
+          ctx->builder.CreateCondBr(condLhsTrue, trueBlock->getBB(), falseBlock->getBB());
+          trueBlock->setActive(ctx->builder);
+          if (cTy->hasCopyAssignment() || cTy->hasMoveAssignment()) {
+            auto* candFn = cTy->hasCopyAssignment() ? cTy->getCopyAssignment() : cTy->getMoveAssignment();
+            (void)candFn->call(ctx, {maybeValueLhsRef, exp->getLLVM()}, ctx->getMod());
+          } else {
+            if (cTy->hasDestructor()) {
+              auto* dstrFn = cTy->getDestructor();
+              (void)dstrFn->call(ctx, {maybeValueLhsRef}, ctx->getMod());
+            }
+            ctx->builder.CreateStore(ctx->builder.CreateLoad(cTy->getLLVMType(), exp->getLLVM()), maybeValueLhsRef);
+          }
+          (void)IR::addBranch(ctx->builder, restBlock->getBB());
+          falseBlock->setActive(ctx->builder);
+          if (cTy->hasCopyConstructor() || cTy->hasMoveConstructor()) {
+            auto* constrFn = cTy->hasCopyConstructor() ? cTy->getCopyConstructor() : cTy->getMoveConstructor();
+            (void)constrFn->call(ctx, {maybeValueLhsRef, exp->getLLVM()}, ctx->getMod());
+          } else {
+            ctx->builder.CreateStore(ctx->builder.CreateLoad(cTy->getLLVMType(), exp->getLLVM()), maybeValueLhsRef);
+          }
+          ctx->builder.CreateStore(llvm::ConstantInt::get(llvm::Type::getInt1Ty(ctx->llctx), 1u), maybeTagLhsRef);
+          (void)IR::addBranch(ctx->builder, restBlock->getBB());
+          restBlock->setActive(ctx->builder);
+        } else {
+          if (exp->isReference()) {
+            exp->loadImplicitPointer(ctx->builder);
+            exp = new IR::Value(ctx->builder.CreateLoad(subTy->getLLVMType(), exp->getLLVM()), subTy, false,
+                                IR::Nature::temporary);
+          } else if (exp->isImplicitPointer()) {
+            exp = new IR::Value(ctx->builder.CreateLoad(subTy->getLLVMType(), exp->getLLVM()), subTy, false,
+                                IR::Nature::temporary);
+          }
+          ctx->builder.CreateStore(exp->getLLVM(), maybeValueLhsRef);
+          ctx->builder.CreateStore(llvm::ConstantInt::get(llvm::Type::getInt1Ty(ctx->llctx), 1u), maybeTagLhsRef);
+        }
+      } else {
+        ctx->Error("Type of the right hand side of the assignment is not compatible with the left hand side",
+                   fileRange);
+      }
+    }
+  } else if (lhsVal->isVariable() ||
+             (lhsVal->getType()->isReference() && lhsVal->getType()->asReference()->isSubtypeVariable())) {
     SHOW("Is variable nature")
     if (lhsVal->isImplicitPointer() || lhsVal->getType()->isReference()) {
       if (value->nodeType() == NodeType::copyExpression || value->nodeType() == NodeType::moveExpression) {
@@ -224,6 +465,7 @@ IR::Value* Assignment::emit(IR::Context* ctx) {
                  lhs->fileRange);
     }
   }
+  return nullptr;
 }
 
 Json Assignment::toJson() const {

28b26a26f703dbdf1f8e93e96281d07b17255cc5
Aldrin Mathew <admin@aldrinmathew.com>
2022-11-10 14:52:38 +0530

    Added subscript operator support for cstring
    - For some reason, the subscript operator / index operator support for
      cstring was absent, so that has been added

--- src/ast/expressions/index_access.cpp
+++ src/ast/expressions/index_access.cpp
@@ -6,15 +6,14 @@
 
 namespace qat::ast {
 
-IndexAccess::IndexAccess(Expression *_instance, Expression *_index,
-                         utils::FileRange _fileRange)
+IndexAccess::IndexAccess(Expression* _instance, Expression* _index, utils::FileRange _fileRange)
     : Expression(std::move(_fileRange)), instance(_instance), index(_index) {}
 
-IR::Value *IndexAccess::emit(IR::Context *ctx) {
-  auto *inst     = instance->emit(ctx);
-  auto *instType = inst->getType();
-  auto *ind      = index->emit(ctx);
-  auto *indType  = ind->getType();
+IR::Value* IndexAccess::emit(IR::Context* ctx) {
+  auto* inst     = instance->emit(ctx);
+  auto* instType = inst->getType();
+  auto* ind      = index->emit(ctx);
+  auto* indType  = ind->getType();
   if (inst->getType()->isReference()) {
     instType = instType->asReference()->getSubType();
   }
@@ -28,97 +27,68 @@ IR::Value *IndexAccess::emit(IR::Context *ctx) {
       indType = indType->asReference()->getSubType();
     }
     if (ind->getType()->isUnsignedInteger() || ind->getType()->isInteger() ||
-        (ind->getType()->isReference() &&
-         (ind->getType()->asReference()->getSubType()->isUnsignedInteger() ||
-          ind->getType()->asReference()->getSubType()->isInteger()))) {
+        (ind->getType()->isReference() && (ind->getType()->asReference()->getSubType()->isUnsignedInteger() ||
+                                           ind->getType()->asReference()->getSubType()->isInteger()))) {
       if (inst->getType()->isReference()) {
-        SHOW("Instance for member access is a Reference to: "
-             << inst->getType()->toString())
-        inst = new IR::Value(
-            ctx->builder.CreateLoad(instType->getLLVMType(), inst->getLLVM()),
-            instType, inst->getType()->asReference()->isSubtypeVariable(),
-            inst->getType()->asReference()->isSubtypeVariable()
-                ? IR::Nature::assignable
-                : IR::Nature::temporary);
+        SHOW("Instance for member access is a Reference to: " << inst->getType()->toString())
+        inst = new IR::Value(ctx->builder.CreateLoad(instType->getLLVMType(), inst->getLLVM()), instType,
+                             inst->getType()->asReference()->isSubtypeVariable(),
+                             inst->getType()->asReference()->isSubtypeVariable() ? IR::Nature::assignable
+                                                                                 : IR::Nature::temporary);
       }
       if (ind->getType()->isReference()) {
-        ind = new IR::Value(
-            ctx->builder.CreateLoad(indType->getLLVMType(), ind->getLLVM()),
-            indType, ind->getType()->asReference()->isSubtypeVariable(),
-            ind->getType()->asReference()->isSubtypeVariable()
-                ? IR::Nature::assignable
-                : IR::Nature::temporary);
+        ind = new IR::Value(ctx->builder.CreateLoad(indType->getLLVMType(), ind->getLLVM()), indType,
+                            ind->getType()->asReference()->isSubtypeVariable(),
+                            ind->getType()->asReference()->isSubtypeVariable() ? IR::Nature::assignable
+                                                                               : IR::Nature::temporary);
       }
       if (!instType->isTuple()) {
         if (instType->isPointer()) {
-          Vec<llvm::Value *> idxs;
+          Vec<llvm::Value*> idxs;
           idxs.push_back(ind->getLLVM());
           return new IR::Value(
-              ctx->builder.CreateInBoundsGEP(
-                  instType->asPointer()->getSubType()->getLLVMType(),
-                  inst->getLLVM(), idxs),
-              IR::ReferenceType::get(inst->isVariable(),
-                                     instType->asPointer()->getSubType(),
-                                     ctx->llctx),
+              ctx->builder.CreateInBoundsGEP(instType->asPointer()->getSubType()->getLLVMType(), inst->getLLVM(), idxs),
+              IR::ReferenceType::get(inst->isVariable(), instType->asPointer()->getSubType(), ctx->llctx),
               instType->asPointer()->isSubtypeVariable(),
-              instType->asPointer()->isSubtypeVariable()
-                  ? IR::Nature::assignable
-                  : IR::Nature::temporary);
+              instType->asPointer()->isSubtypeVariable() ? IR::Nature::assignable : IR::Nature::temporary);
         } else {
           SHOW("Getting first element")
-          auto *firstElem = ctx->builder.CreateInBoundsGEP(
-              inst->getType()->getLLVMType(), inst->getLLVM(),
-              {llvm::ConstantInt::get(llvm::Type::getInt64Ty(ctx->llctx), 0u),
-               llvm::ConstantInt::get(llvm::Type::getInt64Ty(ctx->llctx), 0u)});
+          auto* firstElem =
+              ctx->builder.CreateInBoundsGEP(inst->getType()->getLLVMType(), inst->getLLVM(),
+                                             {llvm::ConstantInt::get(llvm::Type::getInt64Ty(ctx->llctx), 0u),
+                                              llvm::ConstantInt::get(llvm::Type::getInt64Ty(ctx->llctx), 0u)});
           if (llvm::isa<llvm::ConstantInt>(ind->getLLVM())) {
             SHOW("Index Access : Index is a constant")
-            auto indConst = *(llvm::dyn_cast<llvm::ConstantInt>(ind->getLLVM())
-                                  ->getValue()
-                                  .getRawData());
+            auto indConst = *(llvm::dyn_cast<llvm::ConstantInt>(ind->getLLVM())->getValue().getRawData());
             if (indConst == 0) {
               SHOW("Returning the first element from inbounds")
               return new IR::Value(
                   firstElem,
-                  IR::ReferenceType::get(inst->isVariable(),
-                                         instType->asArray()->getElementType(),
-                                         ctx->llctx),
-                  inst->isVariable(),
-                  inst->isVariable() ? IR::Nature::assignable
-                                     : IR::Nature::temporary);
+                  IR::ReferenceType::get(inst->isVariable(), instType->asArray()->getElementType(), ctx->llctx),
+                  inst->isVariable(), inst->isVariable() ? IR::Nature::assignable : IR::Nature::temporary);
             }
           }
           SHOW("Got first element, getting specific element")
           return new IR::Value(
-              ctx->builder.CreateInBoundsGEP(
-                  inst->getType()->getLLVMType()->getArrayElementType(),
-                  firstElem, ind->getLLVM()),
-              IR::ReferenceType::get(inst->isVariable(),
-                                     instType->asArray()->getElementType(),
-                                     ctx->llctx),
-              inst->isVariable(),
-              inst->isVariable() ? IR::Nature::assignable
-                                 : IR::Nature::temporary);
+              ctx->builder.CreateInBoundsGEP(inst->getType()->getLLVMType()->getArrayElementType(), firstElem,
+                                             ind->getLLVM()),
+              IR::ReferenceType::get(inst->isVariable(), instType->asArray()->getElementType(), ctx->llctx),
+              inst->isVariable(), inst->isVariable() ? IR::Nature::assignable : IR::Nature::temporary);
         }
       } else {
         if (llvm::isa<llvm::ConstantInt>(ind->getLLVM())) {
           // NOLINTBEGIN(clang-analyzer-core.CallAndMessage)
-          auto constantIndex =
-              *(llvm::dyn_cast<llvm::ConstantInt>(ind->getLLVM())
-                    ->getValue()
-                    .getRawData());
+          auto constantIndex = *(llvm::dyn_cast<llvm::ConstantInt>(ind->getLLVM())->getValue().getRawData());
           // NOLINTEND(clang-analyzer-core.CallAndMessage)
           if (constantIndex < instType->asTuple()->getSubTypeCount()) {
-            return new IR::Value(
-                ctx->builder.CreateStructGEP(instType->getLLVMType(),
-                                             inst->getLLVM(), constantIndex),
-                instType->asTuple()->getSubtypeAt(constantIndex),
-                inst->isVariable(), inst->getNature());
+            return new IR::Value(ctx->builder.CreateStructGEP(instType->getLLVMType(), inst->getLLVM(), constantIndex),
+                                 instType->asTuple()->getSubtypeAt(constantIndex), inst->isVariable(),
+                                 inst->getNature());
           } else {
             ctx->Error("The provided index exceeds the maximum value for this "
                        "tuple type. Remember that tuples are zero indexed. The "
                        "index value starts at " +
-                           ctx->highlightError("0") + " and ends at " +
-                           ctx->highlightError("length - 1"),
+                           ctx->highlightError("0") + " and ends at " + ctx->highlightError("length - 1"),
                        index->fileRange);
           }
         } else {
@@ -139,50 +109,48 @@ IR::Value *IndexAccess::emit(IR::Context *ctx) {
     }
     // FIXME - POSSIBLY THROW ERROR AT RUNTIME IF BOUNDS ARE EXCEEDED
     if (inst->isImplicitPointer() || inst->isReference()) {
-      auto *zero64 =
-          llvm::ConstantInt::get(llvm::Type::getInt64Ty(ctx->llctx), 0u);
-      auto *strData = ctx->builder.CreateStructGEP(
-          IR::StringSliceType::get(ctx->llctx)->getLLVMType(), inst->getLLVM(),
-          0);
+      auto* zero64 = llvm::ConstantInt::get(llvm::Type::getInt64Ty(ctx->llctx), 0u);
+      auto* strData =
+          ctx->builder.CreateStructGEP(IR::StringSliceType::get(ctx->llctx)->getLLVMType(), inst->getLLVM(), 0);
       SHOW("Got string data")
       SHOW("Got first element")
-      return new IR::Value(
-          ctx->builder.CreateInBoundsGEP(
-              llvm::Type::getInt8Ty(ctx->llctx),
-              ctx->builder.CreateLoad(
-                  llvm::Type::getInt8Ty(ctx->llctx)->getPointerTo(), strData),
-              Vec<llvm::Value *>({ind->getLLVM()})),
-          IR::ReferenceType::get(
-              inst->isReference()
-                  ? inst->getType()->asReference()->isSubtypeVariable()
-                  : inst->isVariable(),
-              IR::UnsignedType::get(8, ctx->llctx), ctx->llctx),
-          false, IR::Nature::temporary);
+      return new IR::Value(ctx->builder.CreateInBoundsGEP(
+                               llvm::Type::getInt8Ty(ctx->llctx),
+                               ctx->builder.CreateLoad(llvm::Type::getInt8Ty(ctx->llctx)->getPointerTo(), strData),
+                               Vec<llvm::Value*>({ind->getLLVM()})),
+                           IR::ReferenceType::get(inst->isReference()
+                                                      ? inst->getType()->asReference()->isSubtypeVariable()
+                                                      : inst->isVariable(),
+                                                  IR::UnsignedType::get(8, ctx->llctx), ctx->llctx),
+                           false, IR::Nature::temporary);
     } else {
-      ctx->Error(
-          "Invalid value for string slice and hence string cannot be indexed",
-          fileRange);
+      ctx->Error("Invalid value for string slice and hence string cannot be indexed", fileRange);
     }
+  } else if (instType->isCString()) {
+    ind->loadImplicitPointer(ctx->builder);
+    inst->loadImplicitPointer(ctx->builder);
+    auto* instVal = inst->getLLVM();
+    if (inst->isReference()) {
+      instVal = ctx->builder.CreateLoad(instType->getLLVMType(), inst->getLLVM());
+    }
+    bool isVarExp = inst->isReference() ? inst->getType()->asReference()->isSubtypeVariable() : inst->isVariable();
+    return new IR::Value(ctx->builder.CreateInBoundsGEP(llvm::Type::getInt8Ty(ctx->llctx), instVal, {ind->getLLVM()}),
+                         IR::ReferenceType::get(isVarExp, IR::UnsignedType::get(8, ctx->llctx), ctx->llctx), false,
+                         IR::Nature::temporary);
   } else if (instType->isCoreType()) {
-    auto               *cTy     = instType->asCore();
-    IR::MemberFunction *opFn    = nullptr;
-    IR::Value          *operand = nullptr;
+    auto*               cTy     = instType->asCore();
+    IR::MemberFunction* opFn    = nullptr;
+    IR::Value*          operand = nullptr;
     if (cTy->hasBinaryOperator("[]", indType)) {
       opFn    = cTy->getBinaryOperator("[]", indType);
       operand = ind;
-    } else if (indType->isReference() &&
-               cTy->hasBinaryOperator("[]",
-                                      indType->asReference()->getSubType())) {
-      opFn = cTy->getBinaryOperator("[]", indType->asReference()->getSubType());
-      operand =
-          new IR::Value(ind->getLLVM(), indType->asReference()->getSubType(),
-                        false, IR::Nature::temporary);
-    } else if (cTy->hasBinaryOperator(
-                   "[]", IR::ReferenceType::get(true, indType, ctx->llctx))) {
+    } else if (indType->isReference() && cTy->hasBinaryOperator("[]", indType->asReference()->getSubType())) {
+      opFn    = cTy->getBinaryOperator("[]", indType->asReference()->getSubType());
+      operand = new IR::Value(ind->getLLVM(), indType->asReference()->getSubType(), false, IR::Nature::temporary);
+    } else if (cTy->hasBinaryOperator("[]", IR::ReferenceType::get(true, indType, ctx->llctx))) {
       if (ind->isImplicitPointer()) {
         if (!ind->isVariable()) {
-          ctx->Error("The [] operator of core type " +
-                         ctx->highlightError(cTy->getFullName()) +
+          ctx->Error("The [] operator of core type " + ctx->highlightError(cTy->getFullName()) +
                          " expects a variable reference, but the provided "
                          "expression is not a variable",
                      index->fileRange);
@@ -196,8 +164,7 @@ IR::Value *IndexAccess::emit(IR::Context *ctx) {
     }
 
   } else {
-    ctx->Error("The expression cannot be used for index access",
-               instance->fileRange);
+    ctx->Error("The expression cannot be used for index access", instance->fileRange);
   }
 }
 

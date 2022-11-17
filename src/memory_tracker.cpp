02135f3602baed74a6e8b85749d1234e67dad173
Aldrin Mathew <admin@aldrinmathew.com>
2022-11-18 01:04:37 +0530

    Added `MemoryTracker`
    - MemoryTracker keeps track of the number of objects allocated on the heap
      and the total size allocated

---
+++ src/memory_tracker.cpp
@@ -0,0 +1,49 @@
+#include "./memory_tracker.hpp"
+#include "cli/color.hpp"
+#include "cli/version.hpp"
+#include "show.hpp"
+#include <iostream>
+
+namespace qat {
+
+u64 MemoryTracker::objectsOnHeap = 0;
+
+u64 MemoryTracker::totalSize = 0;
+
+u64 MemoryTracker::negativeCounter = 0;
+
+u64 MemoryTracker::getHeapCounter() { return objectsOnHeap; }
+
+u64 MemoryTracker::getTotalSize() { return totalSize; }
+
+void MemoryTracker::addHeapCounter() { ++objectsOnHeap; }
+
+void MemoryTracker::incrementSize(usize size) { totalSize += size; }
+
+void MemoryTracker::subtractHeapCounter() {
+  if (objectsOnHeap > 0u) {
+    --objectsOnHeap;
+  } else {
+    ++negativeCounter;
+  }
+}
+
+void MemoryTracker::report() {
+  SHOW("\nMemory Tracker")
+  SHOW("Objects in heap memory :: " << objectsOnHeap)
+  SHOW("Total heap memory      :: " << totalSize)
+  SHOW("Unbalanced deletes      :: " << negativeCounter << "\n")
+}
+
+} // namespace qat
+
+void* operator new(qat::usize size) _GLIBCXX_THROW(std::bad_alloc) {
+  qat::MemoryTracker::addHeapCounter();
+  qat::MemoryTracker::incrementSize(size);
+  return malloc(size);
+}
+
+void operator delete(void* ptr) noexcept {
+  qat::MemoryTracker::subtractHeapCounter();
+  free(ptr);
+}
---
+++ src/memory_tracker.hpp
@@ -0,0 +1,31 @@
+#ifndef QAT_MEMORY_TRACKER_HPP
+#define QAT_MEMORY_TRACKER_HPP
+
+#include "./utils/helpers.hpp"
+#include "./utils/macros.hpp"
+#include <map>
+
+namespace qat {
+
+class MemoryTracker {
+private:
+  static u64 objectsOnHeap;
+  static u64 totalSize;
+  static u64 negativeCounter;
+
+public:
+  useit static u64 getHeapCounter();
+  useit static u64 getTotalSize();
+  static void      addHeapCounter();
+  static void      incrementSize(usize size);
+  static void      subtractHeapCounter();
+  static void      report();
+};
+
+} // namespace qat
+
+void* operator new(qat::usize size) _GLIBCXX_THROW(std::bad_alloc); // NOLINT(readability-redundant-declaration)
+
+void operator delete(qat::VoidPtr ptr) noexcept; // NOLINT(readability-redundant-declaration)
+
+#endif

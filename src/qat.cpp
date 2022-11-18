b5eae3acf3f12db8d0ddfb1129830bd8e87db579
Aldrin Mathew <admin@aldrinmathew.com>
2022-11-18 15:07:34 +0530

    `setTerminalColors` now reads cli config
    - setTerminalColors now checks cli config and see if the user has opted for the
      no color mode

--- src/qat.cpp
+++ src/qat.cpp
@@ -7,27 +7,28 @@
 #include <windows.h>
 #endif
 
-void setTerminalColors() {
-  HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
-  DWORD  consoleMode;
-  GetConsoleMode(handle, &consoleMode);
-  consoleMode |= 0x0004;
-  consoleMode |= 0x0008;
-  SetConsoleMode(handle, consoleMode);
+void setTerminalColors(qat::cli::Config* cfg) {
+  if (cfg->noColorMode()) {
+    HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD  consoleMode;
+    GetConsoleMode(handle, &consoleMode);
+    consoleMode |= 0x0004;
+    consoleMode |= 0x0008;
+    SetConsoleMode(handle, consoleMode);
+  }
 }
+#else
+void setTerminalColors(qat::cli::Config* cfg) {}
 #endif
 
 int main(int count, const char** args) {
   using namespace qat;
 
-#if PLATFORM_IS_WINDOWS
-  setTerminalColors();
-#endif
-
   auto* cli = cli::Config::init(count, args);
   if (cli->shouldExit()) {
     return 0;
   }
+  setTerminalColors(cli);
   auto* sitter = new QatSitter();
   sitter->init();
   SHOW("QatSitter initted")

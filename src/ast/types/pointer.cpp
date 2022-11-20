10577e31e6a662d13105f6ed7c2e2205979f0dc0
Aldrin Mathew <admin@aldrinmathew.com>
2022-11-20 15:48:37 +0530

    Updated `ast::PointerType` to support ownership
    - ast::PointerType has been updated to support ownership, following the
      updates in IR::PointerType
    
    - The ownership types supported in the AST is different from that in the
      IR, mostly since the underlying ownership types are not reflected as
      it is in some syntaxes

--- src/ast/types/pointer.cpp
+++ src/ast/types/pointer.cpp
@@ -4,12 +4,45 @@
 
 namespace qat::ast {
 
-PointerType::PointerType(QatType *_type, const bool _variable,
-                         const utils::FileRange _fileRange)
-    : type(_type), QatType(_variable, _fileRange) {}
+PointerType::PointerType(QatType* _type, bool _variable, PtrOwnType ownTy, Maybe<QatType*> _ownTyTy,
+                         utils::FileRange _fileRange)
+    : QatType(_variable, std::move(_fileRange)), type(_type), ownTyp(ownTy), ownerTyTy(_ownTyTy) {}
 
-IR::QatType *PointerType::emit(IR::Context *ctx) {
-  return IR::PointerType::get(type->isVariable(), type->emit(ctx), ctx->llctx);
+IR::PointerOwner PointerType::getPointerOwner(IR::Context* ctx) const {
+  switch (ownTyp) {
+    case PtrOwnType::type:
+      return IR::PointerOwner::OfType(ownerTyTy.value()->emit(ctx));
+    case PtrOwnType::typeParent:
+      return IR::PointerOwner::OfType(ctx->activeType);
+    case PtrOwnType::anonymous:
+      return IR::PointerOwner::OfAnonymous();
+    case PtrOwnType::heap:
+      return IR::PointerOwner::OfHeap();
+    case PtrOwnType::parent: {
+      if (ctx->fn) {
+        return IR::PointerOwner::OfFunction(ctx->fn);
+      } else {
+        return IR::PointerOwner::OfType(ctx->activeType);
+      }
+    }
+    case PtrOwnType::region:
+      return IR::PointerOwner::OfRegion();
+  }
+}
+
+IR::QatType* PointerType::emit(IR::Context* ctx) {
+  if (ownTyp == PtrOwnType::parent) {
+    if (!ctx->fn && !ctx->activeType) {
+      ctx->Error(
+          "This pointer type is not inside a function or type and hence the pointer cannot have parent ownership",
+          fileRange);
+    }
+  } else if (ownTyp == PtrOwnType::typeParent) {
+    if (!ctx->activeType) {
+      ctx->Error("This pointer type is not inside a type and hence the pointer cannot be owned by type", fileRange);
+    }
+  }
+  return IR::PointerType::get(type->isVariable(), type->emit(ctx), getPointerOwner(ctx), ctx->llctx);
 }
 
 TypeKind PointerType::typeKind() const { return TypeKind::pointer; }
@@ -22,8 +55,6 @@ Json PointerType::toJson() const {
       ._("fileRange", fileRange);
 }
 
-String PointerType::toString() const {
-  return (isVariable() ? "var #[" : "#[") + type->toString() + "]";
-}
+String PointerType::toString() const { return (isVariable() ? "var #[" : "#[") + type->toString() + "]"; }
 
 } // namespace qat::ast
--- src/ast/types/pointer.hpp
+++ src/ast/types/pointer.hpp
@@ -1,37 +1,38 @@
 #ifndef QAT_TYPES_POINTER_HPP
 #define QAT_TYPES_POINTER_HPP
 
+#include "../../IR/types/pointer.hpp"
 #include "./qat_type.hpp"
 #include "./void.hpp"
 #include <string>
 
 namespace qat::ast {
 
-/**
- *  A pointer type in the language
- *
- */
+enum class PtrOwnType {
+  heap,
+  type,
+  typeParent,
+  parent,
+  anonymous,
+  region,
+};
+
 class PointerType : public QatType {
 private:
-  QatType *type;
+  QatType*             type;
+  PtrOwnType           ownTyp;
+  Maybe<ast::QatType*> ownerTyTy;
+
+  useit IR::PointerOwner getPointerOwner(IR::Context* ctx) const;
 
 public:
-  /**
-   *  Create a pointer to the provided datatype
-   *
-   * @param _type Datatype to which the pointer is pointing to
-   * @param _fileRange
-   */
-  PointerType(QatType *_type, const bool _variable,
-              const utils::FileRange _fileRange);
+  PointerType(QatType* _type, bool _variable, PtrOwnType _ownTy, Maybe<QatType*> _ownerTyTy,
+              utils::FileRange _fileRange);
 
-  IR::QatType *emit(IR::Context *ctx);
-
-  TypeKind typeKind() const;
-
-  Json toJson() const;
-
-  String toString() const;
+  useit IR::QatType* emit(IR::Context* ctx) final;
+  useit TypeKind     typeKind() const final;
+  useit Json         toJson() const final;
+  useit String       toString() const final;
 };
 
 } // namespace qat::ast

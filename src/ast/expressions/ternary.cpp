0c8fed3559a878d11e29e2e5e442e6e6169b6c6f
Aldrin Mathew <admin@aldrinmathew.com>
2022-12-03 16:48:37 +0530

    Fixed ternary expression issue & Type Inference
    - Type Inference has been added to `ast::TernaryExpression` and it
      supports type inference for signed and unsigned integer literals,
      null pointers, default expression & none expression
    
    - Fixed issue that if either true or false expression is a local value,
      then it is not loaded as a value thereby causing problems in
      compilation

--- src/ast/expressions/ternary.cpp
+++ src/ast/expressions/ternary.cpp
@@ -1,5 +1,11 @@
 #include "./ternary.hpp"
 #include "../../IR/control_flow.hpp"
+#include "../../IR/types/maybe.hpp"
+#include "../constants/integer_literal.hpp"
+#include "../constants/null_pointer.hpp"
+#include "../constants/unsigned_literal.hpp"
+#include "./default.hpp"
+#include "./none.hpp"
 
 namespace qat::ast {
 
@@ -25,14 +31,91 @@ IR::Value* TernaryExpression::emit(IR::Context* ctx) {
                                 IR::Nature::temporary);
       }
       ctx->builder.CreateCondBr(genCond->getLLVM(), trueBlock->getBB(), falseBlock->getBB());
+      IR::Value* trueVal     = nullptr;
+      IR::Value* falseVal    = nullptr;
+      auto       trueNodeTy  = trueExpr->nodeType();
+      auto       falseNodeTy = falseExpr->nodeType();
+      if (trueNodeTy == NodeType::integerLiteral) {
+        falseBlock->setActive(ctx->builder);
+        falseVal = falseExpr->emit(ctx);
+        ((IntegerLiteral*)trueExpr)
+            ->setType(falseVal->isReference() ? falseVal->getType()->asReference()->getSubType() : falseVal->getType());
+      } else if (trueNodeTy == NodeType::unsignedLiteral) {
+        falseBlock->setActive(ctx->builder);
+        falseVal = falseExpr->emit(ctx);
+        ((UnsignedLiteral*)trueExpr)
+            ->setType(falseVal->isReference() ? falseVal->getType()->asReference()->getSubType() : falseVal->getType());
+      } else if (trueNodeTy == NodeType::nullPointer) {
+        falseBlock->setActive(ctx->builder);
+        falseVal = falseExpr->emit(ctx);
+        if (falseVal->isPointer() ||
+            (falseVal->isReference() && falseVal->getType()->asReference()->getSubType()->isPointer())) {
+          ((NullPointer*)trueExpr)
+              ->setType(falseVal->isReference() ? falseVal->getType()->asReference()->getSubType()->asPointer()
+                                                : falseVal->getType()->asPointer());
+        }
+      } else if (trueNodeTy == NodeType::none) {
+        falseBlock->setActive(ctx->builder);
+        falseVal = falseExpr->emit(ctx);
+        if (falseVal->getType()->isReference() ? falseVal->getType()->asReference()->getSubType()->isMaybe()
+                                               : falseVal->getType()->isMaybe()) {
+          ((NoneExpression*)trueExpr)
+              ->setType(falseVal->isReference()
+                            ? falseVal->getType()->asReference()->getSubType()->asMaybe()->getSubType()
+                            : falseVal->getType()->asMaybe()->getSubType());
+        }
+      } else if (trueNodeTy == NodeType::Default) {
+        falseBlock->setActive(ctx->builder);
+        falseVal = falseExpr->emit(ctx);
+        ((Default*)trueExpr)
+            ->setType(falseVal->isReference() ? falseVal->getType()->asReference()->getSubType() : falseVal->getType());
+      } else if (falseNodeTy == NodeType::integerLiteral) {
+        trueBlock->setActive(ctx->builder);
+        trueVal = trueExpr->emit(ctx);
+        ((IntegerLiteral*)falseExpr)
+            ->setType(trueVal->isReference() ? trueVal->getType()->asReference()->getSubType() : trueVal->getType());
+      } else if (falseNodeTy == NodeType::unsignedLiteral) {
+        trueBlock->setActive(ctx->builder);
+        trueVal = trueExpr->emit(ctx);
+        ((UnsignedLiteral*)falseExpr)
+            ->setType(trueVal->isReference() ? trueVal->getType()->asReference()->getSubType() : trueVal->getType());
+      } else if (falseNodeTy == NodeType::nullPointer) {
+        trueBlock->setActive(ctx->builder);
+        trueVal = trueExpr->emit(ctx);
+        if (trueVal->isPointer() ||
+            (trueVal->isReference() && trueVal->getType()->asReference()->getSubType()->isPointer())) {
+          ((NullPointer*)falseExpr)
+              ->setType(trueVal->isReference() ? trueVal->getType()->asReference()->getSubType()->asPointer()
+                                               : trueVal->getType()->asPointer());
+        }
+      } else if (falseNodeTy == NodeType::none) {
+        trueBlock->setActive(ctx->builder);
+        trueVal = trueExpr->emit(ctx);
+        if (trueVal->getType()->isReference() ? trueVal->getType()->asReference()->getSubType()->isMaybe()
+                                              : trueVal->getType()->isMaybe()) {
+          ((NoneExpression*)falseExpr)
+              ->setType(trueVal->isReference()
+                            ? trueVal->getType()->asReference()->getSubType()->asMaybe()->getSubType()
+                            : trueVal->getType()->asMaybe()->getSubType());
+        }
+      } else if (falseNodeTy == NodeType::Default) {
+        trueBlock->setActive(ctx->builder);
+        trueVal = trueExpr->emit(ctx);
+        ((Default*)falseExpr)
+            ->setType(trueVal->isReference() ? trueVal->getType()->asReference()->getSubType() : trueVal->getType());
+      }
 
       /* true case */
-      trueBlock->setActive(ctx->builder);
-      auto* trueVal = trueExpr->emit(ctx);
+      if (!trueVal) {
+        trueBlock->setActive(ctx->builder);
+        trueVal = trueExpr->emit(ctx);
+      }
 
       /* false case */
-      falseBlock->setActive(ctx->builder);
-      auto* falseVal = falseExpr->emit(ctx);
+      if (!falseVal) {
+        falseBlock->setActive(ctx->builder);
+        falseVal = falseExpr->emit(ctx);
+      }
 
       /* rest block */
       llvm::PHINode* phiNode = nullptr;
@@ -43,32 +126,43 @@ IR::Value* TernaryExpression::emit(IR::Context* ctx) {
             (trueType->isReference() && trueType->asReference()->getSubType()->isSame(falseType)) ||
             (falseType->isReference() && falseType->asReference()->getSubType()->isSame(trueType))) {
           IR::QatType* resultType = nullptr;
-          if (trueType->isReference() && (!falseType->isReference() && !falseVal->isImplicitPointer())) {
-            resultType = trueType->asReference()->getSubType();
+          if ((trueType->isReference() || trueVal->isImplicitPointer()) &&
+              (!falseType->isReference() && !falseVal->isImplicitPointer())) {
             trueBlock->setActive(ctx->builder);
             trueVal->loadImplicitPointer(ctx->builder);
-            trueVal = new IR::Value(
-                ctx->builder.CreateLoad(trueType->asReference()->getSubType()->getLLVMType(), trueVal->getLLVM()),
-                trueType->asReference()->getSubType(), false, IR::Nature::temporary);
-          } else if (falseType->isReference() && (!trueType->isReference() && !trueVal->isImplicitPointer())) {
-            resultType = falseType->asReference()->getSubType();
+            if (trueType->isReference()) {
+              resultType = trueType->asReference()->getSubType();
+              trueVal    = new IR::Value(
+                  ctx->builder.CreateLoad(trueType->asReference()->getSubType()->getLLVMType(), trueVal->getLLVM()),
+                  trueType->asReference()->getSubType(), false, IR::Nature::temporary);
+            } else {
+              resultType = trueType;
+            }
+          } else if ((falseType->isReference() || falseVal->isImplicitPointer()) &&
+                     (!trueType->isReference() && !trueVal->isImplicitPointer())) {
             falseBlock->setActive(ctx->builder);
             falseVal->loadImplicitPointer(ctx->builder);
-            falseVal = new IR::Value(
-                ctx->builder.CreateLoad(falseType->asReference()->getSubType()->getLLVMType(), falseVal->getLLVM()),
-                falseType->asReference()->getSubType(), false, IR::Nature::temporary);
+            if (falseType->isReference()) {
+              resultType = falseType->asReference()->getSubType();
+              falseVal   = new IR::Value(
+                  ctx->builder.CreateLoad(falseType->asReference()->getSubType()->getLLVMType(), falseVal->getLLVM()),
+                  falseType->asReference()->getSubType(), false, IR::Nature::temporary);
+            } else {
+              resultType = falseType;
+            }
           } else {
             resultType = trueType;
           }
           trueBlock->setActive(ctx->builder);
-          trueBlock->destroyLocals(ctx);
           (void)IR::addBranch(ctx->builder, mergeBlock->getBB());
           falseBlock->setActive(ctx->builder);
-          falseBlock->destroyLocals(ctx);
           (void)IR::addBranch(ctx->builder, mergeBlock->getBB());
           mergeBlock->setActive(ctx->builder);
-          phiNode = ctx->builder.CreatePHI(resultType->getLLVMType(), 2, utils::unique_id());
+          SHOW("Creating PhiNode, resultType address: " << resultType)
+          phiNode = ctx->builder.CreatePHI(resultType->getLLVMType(), 2);
+          SHOW("Getting true value")
           phiNode->addIncoming(trueVal->getLLVM(), trueBlock->getBB());
+          SHOW("Getting false value")
           phiNode->addIncoming(falseVal->getLLVM(), falseBlock->getBB());
           SHOW("Phinode type is: " << phiNode->getType()->getTypeID());
           return new IR::Value(phiNode, resultType, false, IR::Nature::temporary);

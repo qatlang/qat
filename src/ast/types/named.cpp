76ca36a0d9be19e2274c1b396f104a287a3b2df6
Aldrin Mathew <admin@aldrinmathew.com>
2022-12-07 01:08:39 +0530

    Updated `ast::NamedType` for regions
    - NamedType has been updated to detect Region types in modules and
      return them. This makes it possible to mention regions as types

--- src/ast/types/named.cpp
+++ src/ast/types/named.cpp
@@ -1,23 +1,20 @@
 #include "./named.hpp"
+#include "../../IR/types/region.hpp"
 #include "../../utils/split_string.hpp"
 
 namespace qat::ast {
 
-NamedType::NamedType(u32 _relative, String _name, const bool _variable,
-                     utils::FileRange _fileRange)
-    : QatType(_variable, std::move(_fileRange)), relative(_relative),
-      name(std::move(_name)) {}
+NamedType::NamedType(u32 _relative, String _name, const bool _variable, utils::FileRange _fileRange)
+    : QatType(_variable, std::move(_fileRange)), relative(_relative), name(std::move(_name)) {}
 
-IR::QatType *NamedType::emit(IR::Context *ctx) {
-  auto *mod     = ctx->getMod();
+IR::QatType* NamedType::emit(IR::Context* ctx) {
+  auto* mod     = ctx->getMod();
   auto  reqInfo = ctx->getReqInfo();
   if (relative != 0) {
     if (ctx->getMod()->hasNthParent(relative)) {
       mod = ctx->getMod()->getNthParent(relative);
     } else {
-      ctx->Error("The active scope does not have " + std::to_string(relative) +
-                     " parents",
-                 fileRange);
+      ctx->Error("The active scope does not have " + std::to_string(relative) + " parents", fileRange);
     }
   }
   auto entityName = name;
@@ -29,20 +26,15 @@ IR::QatType *NamedType::emit(IR::Context *ctx) {
       if (mod->hasLib(split)) {
         mod = mod->getLib(split, reqInfo);
         if (!mod->getVisibility().isAccessible(reqInfo)) {
-          ctx->Error("Lib " + ctx->highlightError(mod->getFullName()) +
-                         " is not accessible here",
-                     fileRange);
+          ctx->Error("Lib " + ctx->highlightError(mod->getFullName()) + " is not accessible here", fileRange);
         }
       } else if (mod->hasBox(split)) {
         mod = mod->getBox(split, reqInfo);
         if (!mod->getVisibility().isAccessible(reqInfo)) {
-          ctx->Error("Box " + ctx->highlightError(mod->getFullName()) +
-                         " is not accessible here",
-                     fileRange);
+          ctx->Error("Box " + ctx->highlightError(mod->getFullName()) + " is not accessible here", fileRange);
         }
       } else {
-        ctx->Error("No box or lib named " + ctx->highlightError(split) +
-                       " found inside " +
+        ctx->Error("No box or lib named " + ctx->highlightError(split) + " found inside " +
                        ctx->highlightError(mod->getFullName()),
                    fileRange);
       }
@@ -50,51 +42,49 @@ IR::QatType *NamedType::emit(IR::Context *ctx) {
   }
   if (mod->hasCoreType(entityName) || mod->hasBroughtCoreType(entityName) ||
       mod->hasAccessibleCoreTypeInImports(entityName, reqInfo).first) {
-    auto *cTy = mod->getCoreType(entityName, reqInfo);
+    auto* cTy = mod->getCoreType(entityName, reqInfo);
     if (!cTy->getVisibility().isAccessible(reqInfo)) {
-      ctx->Error("Core type " + ctx->highlightError(cTy->getFullName()) +
-                     " inside module " +
-                     ctx->highlightError(mod->getFullName()) +
-                     " is not accessible here",
+      ctx->Error("Core type " + ctx->highlightError(cTy->getFullName()) + " inside module " +
+                     ctx->highlightError(mod->getFullName()) + " is not accessible here",
                  fileRange);
     }
     return cTy;
-  } else if (mod->hasTypeDef(entityName) ||
-             mod->hasBroughtTypeDef(entityName) ||
+  } else if (mod->hasTypeDef(entityName) || mod->hasBroughtTypeDef(entityName) ||
              mod->hasAccessibleTypeDefInImports(entityName, reqInfo).first) {
-    auto *dTy = mod->getTypeDef(entityName, reqInfo);
+    auto* dTy = mod->getTypeDef(entityName, reqInfo);
     if (!dTy->getVisibility().isAccessible(reqInfo)) {
-      ctx->Error("Type definition " + ctx->highlightError(dTy->getFullName()) +
-                     " inside module " +
-                     ctx->highlightError(mod->getFullName()) +
-                     " is not accessible here",
+      ctx->Error("Type definition " + ctx->highlightError(dTy->getFullName()) + " inside module " +
+                     ctx->highlightError(mod->getFullName()) + " is not accessible here",
                  fileRange);
     }
     return dTy;
-  } else if (mod->hasMixType(entityName) ||
-             mod->hasBroughtMixType(entityName) ||
+  } else if (mod->hasMixType(entityName) || mod->hasBroughtMixType(entityName) ||
              mod->hasAccessibleMixTypeInImports(entityName, reqInfo).first) {
-    auto *mTy = mod->getMixType(entityName, reqInfo);
+    auto* mTy = mod->getMixType(entityName, reqInfo);
     if (!mTy->getVisibility().isAccessible(reqInfo)) {
-      ctx->Error("Mix type " + ctx->highlightError(mTy->getFullName()) +
-                     " inside module " +
-                     ctx->highlightError(mod->getFullName()) +
-                     " is not accessible here",
+      ctx->Error("Mix type " + ctx->highlightError(mTy->getFullName()) + " inside module " +
+                     ctx->highlightError(mod->getFullName()) + " is not accessible here",
                  fileRange);
     }
     return mTy;
-  } else if (mod->hasChoiceType(entityName) ||
-             mod->hasBroughtChoiceType(entityName) ||
+  } else if (mod->hasChoiceType(entityName) || mod->hasBroughtChoiceType(entityName) ||
              mod->hasAccessibleChoiceTypeInImports(entityName, reqInfo).first) {
-    auto *chTy = mod->getChoiceType(entityName, reqInfo);
+    auto* chTy = mod->getChoiceType(entityName, reqInfo);
     if (!chTy->getVisibility().isAccessible(reqInfo)) {
-      ctx->Error("Choice type " + ctx->highlightError(chTy->getFullName()) +
-                     " inside module " +
-                     ctx->highlightError(mod->getFullName()) +
-                     " is not accessible here",
+      ctx->Error("Choice type " + ctx->highlightError(chTy->getFullName()) + " inside module " +
+                     ctx->highlightError(mod->getFullName()) + " is not accessible here",
                  fileRange);
     }
     return chTy;
+  } else if (mod->hasRegion(entityName) || mod->hasBroughtRegion(entityName) ||
+             mod->hasAccessibleRegionInImports(entityName, reqInfo).first) {
+    auto* reg = mod->getRegion(entityName, reqInfo);
+    if (!reg->getVisibility().isAccessible(reqInfo)) {
+      ctx->Error("Region " + ctx->highlightError(reg->getFullName()) + " inside module " +
+                     ctx->highlightError(mod->getFullName()) + " is not accessible here",
+                 fileRange);
+    }
+    return reg;
   } else {
     ctx->Error("No type named " + name + " found in scope", fileRange);
   }
@@ -108,15 +98,9 @@ u32 NamedType::getRelative() const { return relative; }
 TypeKind NamedType::typeKind() const { return TypeKind::Float; }
 
 Json NamedType::toJson() const {
-  return Json()
-      ._("typeKind", "named")
-      ._("name", name)
-      ._("isVariable", isVariable())
-      ._("fileRange", fileRange);
+  return Json()._("typeKind", "named")._("name", name)._("isVariable", isVariable())._("fileRange", fileRange);
 }
 
-String NamedType::toString() const {
-  return (isVariable() ? "var " : "") + name;
-}
+String NamedType::toString() const { return (isVariable() ? "var " : "") + name; }
 
 } // namespace qat::ast

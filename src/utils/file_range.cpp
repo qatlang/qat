f9c1c39666e624945cd83fda4f5c86c8563a75d8
Aldrin Mathew <admin@aldrinmathew.com>
2022-12-10 23:22:57 +0530

    Lexically normalise path in `FileRange`
    - Now path in `FileRange` is first converted to absolute path and then
      lexically normalised during construction. This is so that there is
      less manual work to do this in later stages

--- src/utils/file_range.cpp
+++ src/utils/file_range.cpp
@@ -1,5 +1,6 @@
 #include "./file_range.hpp"
 #include "./json.hpp"
+#include <filesystem>
 namespace qat {
 
 FilePos::FilePos(uint64_t _line, uint64_t _character) : line(_line), character(_character) {}
@@ -12,9 +13,11 @@ FilePos::operator Json() const {
   return Json()._("line", (unsigned long long)line)._("char", (unsigned long long)character);
 }
 
-FileRange::FileRange(fs::path _filePath) : file(std::move(_filePath)), start({0u, 0u}), end({0u, 0u}) {}
+FileRange::FileRange(const fs::path& _filePath)
+    : file(fs::absolute(_filePath).lexically_normal()), start({0u, 0u}), end({0u, 0u}) {}
 
-FileRange::FileRange(fs::path _file, FilePos _start, FilePos _end) : file(std::move(_file)), start(_start), end(_end) {}
+FileRange::FileRange(const fs::path& _file, FilePos _start, FilePos _end)
+    : file(fs::absolute(_file).lexically_normal()), start(_start), end(_end) {}
 
 FileRange::FileRange(const FileRange& first, const FileRange& second)
     : file(first.file), start(first.start), end((first.file == second.file) ? second.end : first.end) {}

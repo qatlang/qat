b0a9c8ee9d38275020524632d0fb4fe91353708a
Aldrin Mathew <admin@aldrinmathew.com>
2023-01-09 11:24:20 +0530

    Added `IR::ExpandedType`
    - IR::ExpandedType now represents a type that can have member functions
    
    - Now, IR::CoreType & IR::MixType inherits from ExpandedType. So,
      technically now mix types can also have constructors, destructors,
      member functions and more.
    
    - Added `needsImplicitDestructor` & `hasDefinedDestructor` that is used
      to detect if types already have a destructor or if the type is going
      to have a destructor. This is so that the cross-dependency of types
      will not cause the destructor of any type to not be implicitly defined

---
+++ src/IR/types/expanded_type.cpp
@@ -0,0 +1,269 @@
+#include "./expanded_type.hpp"
+#include "../../show.hpp"
+#include "../../utils/split_string.hpp"
+#include "../context.hpp"
+#include "../qat_module.hpp"
+#include "./function.hpp"
+#include "./reference.hpp"
+
+namespace qat::IR {
+
+ExpandedType::ExpandedType(Identifier _name, IR::QatModule* _parent, const utils::VisibilityInfo& _visib)
+    : name(std::move(_name)), parent(_parent), visibility(_visib) {}
+
+String ExpandedType::getFullName() const { return parent->getFullNameWithChild(name.value); }
+
+Identifier ExpandedType::getName() const { return name; }
+
+bool ExpandedType::hasMemberFunction(const String& fnName) const {
+  for (auto* memberFunction : memberFunctions) {
+    SHOW("Found member function: " << memberFunction->getName().value)
+    if (memberFunction->getName().value == fnName) {
+      return true;
+    }
+  }
+  return false;
+}
+
+MemberFunction* ExpandedType::getMemberFunction(const String& fnName) const {
+  for (auto* memberFunction : memberFunctions) {
+    if (memberFunction->getName().value == fnName) {
+      return memberFunction;
+    }
+  }
+  return nullptr;
+}
+
+bool ExpandedType::hasStaticFunction(const String& fnName) const {
+  for (const auto& fun : staticFunctions) {
+    if (fun->getName().value == fnName) {
+      return true;
+    }
+  }
+  return false;
+}
+
+MemberFunction* ExpandedType::getStaticFunction(const String& fnName) const {
+  for (auto* staticFunction : staticFunctions) {
+    if (staticFunction->getName().value == fnName) {
+      return staticFunction;
+    }
+  }
+  return nullptr;
+}
+
+bool ExpandedType::hasBinaryOperator(const String& opr, IR::QatType* type) const {
+  for (auto* bin : binaryOperators) {
+    if (utils::splitString(bin->getName().value, "'")[1] == opr) {
+      auto* binArgTy = bin->getType()->asFunction()->getArgumentTypeAt(1)->getType();
+      if (binArgTy->isSame(type) || (binArgTy->isReference() && binArgTy->asReference()->getSubType()->isSame(type))) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+MemberFunction* ExpandedType::getBinaryOperator(const String& opr, IR::QatType* type) const {
+  for (auto* bin : binaryOperators) {
+    if (utils::splitString(bin->getName().value, "'")[1] == opr) {
+      auto* binArgTy = bin->getType()->asFunction()->getArgumentTypeAt(1)->getType();
+      if (binArgTy->isSame(type) || (binArgTy->isReference() && binArgTy->asReference()->getSubType()->isSame(type))) {
+        return bin;
+      }
+    }
+  }
+  return nullptr;
+}
+
+bool ExpandedType::hasUnaryOperator(const String& opr) const {
+  for (auto* unr : unaryOperators) {
+    if (utils::splitString(unr->getName().value, "'")[1] == opr) {
+      return true;
+    }
+  }
+  return false;
+}
+
+MemberFunction* ExpandedType::getUnaryOperator(const String& opr) const {
+  for (auto* unr : unaryOperators) {
+    if (utils::splitString(unr->getName().value, "'")[1] == opr) {
+      return unr;
+    }
+  }
+  return nullptr;
+}
+
+u64 ExpandedType::getOperatorVariantIndex(const String& opr) const {
+  u64 index = 0;
+  for (auto* bin : binaryOperators) {
+    if (utils::splitString(bin->getName().value, "'")[1] == opr) {
+      index++;
+    }
+  }
+  return index;
+}
+
+bool ExpandedType::hasDefaultConstructor() const { return defaultConstructor != nullptr; }
+
+MemberFunction* ExpandedType::getDefaultConstructor() const { return defaultConstructor; }
+
+bool ExpandedType::hasAnyConstructor() const { return (!constructors.empty()) || (defaultConstructor != nullptr); }
+
+bool ExpandedType::hasAnyFromConvertor() const { return !fromConvertors.empty(); }
+
+bool ExpandedType::hasConstructorWithTypes(Vec<IR::QatType*> types) const {
+  for (auto* con : constructors) {
+    auto argTys = con->getType()->asFunction()->getArgumentTypes();
+    if (types.size() == (argTys.size() - 1)) {
+      bool result = true;
+      for (usize i = 1; i < argTys.size(); i++) {
+        auto* argType = argTys.at(i)->getType();
+        if (!argType->isSame(types.at(i - 1)) && !argType->isCompatible(types.at(i - 1)) &&
+            (!(argType->isReference() && (argType->asReference()->getSubType()->isSame(types.at(i - 1)) ||
+                                          argType->asReference()->getSubType()->isCompatible(types.at(i - 1))))) &&
+            (!(types.at(i - 1)->isReference() &&
+               (types.at(i - 1)->asReference()->getSubType()->isSame(argType) ||
+                types.at(i - 1)->asReference()->getSubType()->isCompatible(argType))))) {
+          result = false;
+          break;
+        }
+      }
+      if (result) {
+        return true;
+      }
+    } else {
+      continue;
+    }
+  }
+  return false;
+}
+
+MemberFunction* ExpandedType::getConstructorWithTypes(Vec<IR::QatType*> types) const {
+  for (auto* con : constructors) {
+    auto argTys = con->getType()->asFunction()->getArgumentTypes();
+    if (types.size() == (argTys.size() - 1)) {
+      bool result = true;
+      for (usize i = 1; i < argTys.size(); i++) {
+        auto* argType = argTys.at(i)->getType();
+        if (!argType->isSame(types.at(i - 1)) && !argType->isCompatible(types.at(i - 1)) &&
+            (!(argType->isReference() && (argType->asReference()->getSubType()->isSame(types.at(i - 1)) ||
+                                          argType->asReference()->getSubType()->isCompatible(types.at(i - 1))))) &&
+            (!(types.at(i - 1)->isReference() && types.at(i - 1)->asReference()->getSubType()->isSame(argType)))) {
+          result = false;
+          break;
+        }
+      }
+      if (result) {
+        return con;
+      }
+    } else {
+      continue;
+    }
+  }
+  return nullptr;
+}
+
+bool ExpandedType::hasFromConvertor(IR::QatType* typ) const {
+  for (auto* fconv : fromConvertors) {
+    auto* argTy = fconv->getType()->asFunction()->getArgumentTypeAt(1)->getType();
+    if (argTy->isSame(typ) || argTy->isCompatible(typ) ||
+        (argTy->isReference() &&
+         (argTy->asReference()->getSubType()->isSame(typ) || argTy->asReference()->getSubType()->isCompatible(typ))) ||
+        (typ->isReference() && typ->asReference()->getSubType()->isSame(argTy))) {
+      return true;
+    }
+  }
+  return false;
+}
+
+MemberFunction* ExpandedType::getFromConvertor(IR::QatType* typ) const {
+  for (auto* fconv : fromConvertors) {
+    auto* argTy = fconv->getType()->asFunction()->getArgumentTypeAt(1)->getType();
+    if (argTy->isSame(typ) || argTy->isCompatible(typ) ||
+        (argTy->isReference() &&
+         (argTy->asReference()->getSubType()->isSame(typ) || argTy->asReference()->getSubType()->isCompatible(typ))) ||
+        (typ->isReference() && typ->asReference()->getSubType()->isSame(argTy))) {
+      return fconv;
+    }
+  }
+  return nullptr;
+}
+
+bool ExpandedType::hasToConvertor(IR::QatType* typ) const {
+  for (auto* fconv : fromConvertors) {
+    auto* retTy = fconv->getType()->asFunction()->getReturnType();
+    if (retTy->isSame(typ) || (retTy->isReference() && retTy->asReference()->getSubType()->isSame(typ)) ||
+        (typ->isReference() && typ->asReference()->getSubType()->isSame(retTy))) {
+      return true;
+    }
+  }
+  return false;
+}
+
+MemberFunction* ExpandedType::getToConvertor(IR::QatType* typ) const {
+  for (auto* tconv : fromConvertors) {
+    auto* retTy = tconv->getType()->asFunction()->getReturnType();
+    if (retTy->isSame(typ) || (retTy->isReference() && retTy->asReference()->getSubType()->isSame(typ)) ||
+        (typ->isReference() && typ->asReference()->getSubType()->isSame(retTy))) {
+      return tconv;
+    }
+  }
+  return nullptr;
+}
+
+bool ExpandedType::hasCopyConstructor() const { return copyConstructor.has_value(); }
+
+MemberFunction* ExpandedType::getCopyConstructor() const { return copyConstructor.value_or(nullptr); }
+
+bool ExpandedType::hasMoveConstructor() const { return moveConstructor.has_value(); }
+
+MemberFunction* ExpandedType::getMoveConstructor() const { return moveConstructor.value_or(nullptr); }
+
+bool ExpandedType::hasCopyAssignment() const { return copyAssignment.has_value(); }
+
+MemberFunction* ExpandedType::getCopyAssignment() const { return copyAssignment.value_or(nullptr); }
+
+bool ExpandedType::hasMoveAssignment() const { return moveAssignment.has_value(); }
+
+MemberFunction* ExpandedType::getMoveAssignment() const { return moveAssignment.value_or(nullptr); }
+
+bool ExpandedType::hasCopy() const { return hasCopyConstructor() && hasCopyAssignment(); }
+
+bool ExpandedType::hasMove() const { return hasMoveConstructor() && hasMoveAssignment(); }
+
+bool ExpandedType::hasDestructor() const { return destructor != nullptr; }
+
+IR::MemberFunction* ExpandedType::getDestructor() const { return destructor; }
+
+utils::VisibilityInfo ExpandedType::getVisibility() const { return visibility; }
+
+bool ExpandedType::isAccessible(const utils::RequesterInfo& reqInfo) const { return visibility.isAccessible(reqInfo); }
+
+QatModule* ExpandedType::getParent() { return parent; }
+
+bool ExpandedType::isTriviallyCopyable() const {
+  return (constructors.empty() && fromConvertors.empty() && !hasDestructor() && !hasCopyConstructor() &&
+          !hasMoveConstructor());
+}
+
+bool ExpandedType::isExpanded() const { return true; }
+
+bool ExpandedType::isDestructible() const {
+  SHOW("ExpandedType isDestructible: " << ((hasDefinedDestructor || needsImplicitDestructor || (destructor != nullptr))
+                                               ? "true"
+                                               : "false"))
+  return hasDefinedDestructor || needsImplicitDestructor || (destructor != nullptr);
+}
+
+void ExpandedType::destroyValue(IR::Context* ctx, Vec<IR::Value*> vals, IR::Function* fun) {
+  if (destructor && !vals.empty()) {
+    auto* inst = vals.front();
+    if (inst->isReference()) {
+      inst->loadImplicitPointer(ctx->builder);
+    }
+    (void)destructor->call(ctx, {inst->getLLVM()}, ctx->getMod());
+  }
+}
+
+} // namespace qat::IR
---
+++ src/IR/types/expanded_type.hpp
@@ -0,0 +1,93 @@
+#ifndef QAT_IR_TYPES_EXPANDED_TYPE_HPP
+#define QAT_IR_TYPES_EXPANDED_TYPE_HPP
+
+#include "../member_function.hpp"
+#include "qat_type.hpp"
+
+namespace qat::ast {
+class DefineCoreType;
+class DefineMixType;
+} // namespace qat::ast
+
+namespace qat::IR {
+
+class ExpandedType : public QatType {
+  friend class MemberFunction;
+  friend class ast::DefineCoreType;
+  friend class ast::DefineMixType;
+
+protected:
+  Identifier             name;
+  QatModule*             parent             = nullptr;
+  MemberFunction*        defaultConstructor = nullptr;
+  Vec<MemberFunction*>   memberFunctions; // Normal
+  Vec<MemberFunction*>   binaryOperators; //
+  Vec<MemberFunction*>   unaryOperators;  //
+  Vec<MemberFunction*>   constructors;    // Constructors
+  Vec<MemberFunction*>   fromConvertors;  // From Convertors
+  Vec<MemberFunction*>   toConvertors;    // To Convertors
+  Vec<MemberFunction*>   staticFunctions; // Static
+  Maybe<MemberFunction*> copyConstructor; // Copy constructor
+  Maybe<MemberFunction*> moveConstructor; // Move constructor
+  Maybe<MemberFunction*> copyAssignment;  // Copy assignment operator
+  Maybe<MemberFunction*> moveAssignment;  // Move assignment operator
+
+  bool            needsImplicitDestructor = false;
+  bool            hasDefinedDestructor    = false;
+  MemberFunction* destructor              = nullptr; // Destructor
+
+  bool explicitCopy = false;
+  bool explicitMove = false;
+
+  utils::VisibilityInfo visibility;
+
+  ExpandedType(Identifier _name, QatModule* _parent, const utils::VisibilityInfo& _visib);
+
+public:
+  useit String          getFullName() const;
+  useit Identifier      getName() const;
+  useit bool            hasMemberFunction(const String& fnName) const;
+  useit MemberFunction* getMemberFunction(const String& fnName) const;
+  useit bool            hasStaticFunction(const String& fnName) const;
+  useit MemberFunction* getStaticFunction(const String& fnName) const;
+  useit bool            hasBinaryOperator(const String& opr, IR::QatType* type) const;
+  useit MemberFunction* getBinaryOperator(const String& opr, IR::QatType* type) const;
+  useit bool            hasUnaryOperator(const String& opr) const;
+  useit MemberFunction* getUnaryOperator(const String& opr) const;
+  useit u64             getOperatorVariantIndex(const String& opr) const;
+  useit bool            hasFromConvertor(IR::QatType* type) const;
+  useit MemberFunction* getFromConvertor(IR::QatType* type) const;
+  useit bool            hasToConvertor(IR::QatType* type) const;
+  useit MemberFunction* getToConvertor(IR::QatType* type) const;
+  useit bool            hasConstructorWithTypes(Vec<IR::QatType*> types) const;
+  useit MemberFunction* getConstructorWithTypes(Vec<IR::QatType*> types) const;
+  useit bool            hasDefaultConstructor() const;
+  useit MemberFunction* getDefaultConstructor() const;
+  useit bool            hasAnyFromConvertor() const;
+  useit bool            hasAnyConstructor() const;
+  useit bool            hasCopyConstructor() const;
+  useit MemberFunction* getCopyConstructor() const;
+  useit bool            hasMoveConstructor() const;
+  useit MemberFunction* getMoveConstructor() const;
+  useit bool            hasCopyAssignment() const;
+  useit MemberFunction* getCopyAssignment() const;
+  useit bool            hasMoveAssignment() const;
+  useit MemberFunction* getMoveAssignment() const;
+  useit bool            hasCopy() const;
+  useit bool            hasMove() const;
+  useit bool            hasDestructor() const;
+  useit MemberFunction* getDestructor() const;
+  useit QatModule*      getParent();
+
+  useit bool isTriviallyCopyable() const;
+
+  useit bool isAccessible(const utils::RequesterInfo& reqInfo) const;
+  useit utils::VisibilityInfo getVisibility() const;
+  useit bool                  isExpanded() const final;
+  useit bool                  isDestructible() const final;
+  void                        destroyValue(IR::Context* ctx, Vec<IR::Value*> vals, IR::Function* fun) final;
+};
+
+} // namespace qat::IR
+
+#endif

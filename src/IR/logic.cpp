2674ff8b0415ceaada6dce872bebc4142c3adedf
Aldrin Mathew <admin@aldrinmathew.com>
2023-01-09 12:41:47 +0530

    Updated empty string slice comparison
    - If the length of both constant string slices are zero, there is no
      further conversion and the result is returned as true

--- src/IR/logic.cpp
+++ src/IR/logic.cpp
@@ -1,14 +1,8 @@
 #include "./logic.hpp"
 #include "../show.hpp"
-#include "context.hpp"
 #include "function.hpp"
-#include "types/core_type.hpp"
-#include "types/reference.hpp"
-#include "value.hpp"
-#include "llvm/IR/ConstantFold.h"
 #include "llvm/IR/Constants.h"
 #include "llvm/IR/GlobalVariable.h"
-#include "llvm/IR/InstrTypes.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/LLVMContext.h"
 
@@ -61,6 +55,9 @@ bool Logic::compareConstantStrings(llvm::Constant* lhsBuff, llvm::Constant* lhsC
   if ((*llvm::cast<llvm::ConstantInt>(lhsCount)->getValue().getRawData()) ==
       (*llvm::cast<llvm::ConstantInt>(rhsCount)->getValue().getRawData())) {
     SHOW("Constant string length matches")
+    if ((*llvm::cast<llvm::ConstantInt>(lhsCount)->getValue().getRawData()) == 0u) {
+      return true;
+    }
     return llvm::cast<llvm::ConstantDataArray>(
                llvm::cast<llvm::GlobalVariable>(lhsBuff->getOperand(0u))->getInitializer())
                ->getRawDataValues()

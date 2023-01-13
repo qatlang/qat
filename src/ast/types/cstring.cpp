f84827d5c8f9a6010ad72f60704ecd6a5e720461
Aldrin Mathew <admin@aldrinmathew.com>
2023-01-13 17:07:20 +0530

    Added `typeSizeInBits` for `ast::CStringType`
    - `typeSizeInBits` has been implemented for `CStringType`. The size is
      same the size of a pointer

--- src/ast/types/cstring.cpp
+++ src/ast/types/cstring.cpp
@@ -7,6 +7,11 @@ namespace qat::ast {
 
 CStringType::CStringType(bool _variable, FileRange _fileRange) : QatType(_variable, std::move(_fileRange)) {}
 
+Maybe<usize> CStringType::getTypeSizeInBits(IR::Context* ctx) const {
+  return (usize)(ctx->getMod()->getLLVMModule()->getDataLayout().getTypeAllocSizeInBits(
+      llvm::PointerType::get(llvm::Type::getInt8Ty(ctx->llctx), 0u)));
+}
+
 IR::QatType* CStringType::emit(IR::Context* ctx) { return IR::CStringType::get(ctx->llctx); }
 
 TypeKind CStringType::typeKind() const { return TypeKind::cstring; }
--- src/ast/types/cstring.hpp
+++ src/ast/types/cstring.hpp
@@ -10,6 +10,7 @@ class CStringType : public QatType {
 public:
   CStringType(bool _variable, FileRange _fileRange);
 
+  useit Maybe<usize> getTypeSizeInBits(IR::Context* ctx) const final;
   useit IR::QatType* emit(IR::Context* ctx) final;
   useit TypeKind     typeKind() const final;
   useit Json         toJson() const final;

f11fbedf6b15d75995fa82038a01390a2cc9fbea
Aldrin Mathew <admin@aldrinmathew.com>
2023-01-30 15:23:36 +0530

    Updated `GenericEntity` for the new generic system
    - GenericEntity has been updated so that it makes uses of the new
      generic system
    
    - Added type inference for constant expressions used in the generic
      parameter values

--- src/ast/expressions/generic_entity.cpp
+++ src/ast/expressions/generic_entity.cpp
@@ -1,9 +1,13 @@
 #include "./generic_entity.hpp"
 #include "../../utils/split_string.hpp"
+#include "../constants/default.hpp"
+#include "../constants/integer_literal.hpp"
+#include "../constants/unsigned_literal.hpp"
+#include "../types/const_generic.hpp"
 
 namespace qat::ast {
 
-GenericEntity::GenericEntity(u32 _relative, Vec<Identifier> _names, Vec<ast::QatType*> _types, FileRange _fileRange)
+GenericEntity::GenericEntity(u32 _relative, Vec<Identifier> _names, Vec<FillGeneric*> _types, FileRange _fileRange)
     : Expression(std::move(_fileRange)), relative(_relative), names(std::move(_names)),
       genericTypes(std::move(_types)) {}
 
@@ -45,25 +49,54 @@ IR::Value* GenericEntity::emit(IR::Context* ctx) {
   auto* curr = fun ? ctx->fn->getBlock() : nullptr;
   if (mod->hasGenericFunction(entityName.value) || mod->hasBroughtGenericFunction(entityName.value) ||
       mod->hasAccessibleGenericFunctionInImports(entityName.value, ctx->getReqInfo()).first) {
-    auto* tempFn = mod->getGenericFunction(entityName.value, ctx->getReqInfo());
-    if (!tempFn->getVisibility().isAccessible(ctx->getReqInfo())) {
+    auto* genericFn = mod->getGenericFunction(entityName.value, ctx->getReqInfo());
+    if (!genericFn->getVisibility().isAccessible(ctx->getReqInfo())) {
       auto fullName = Identifier::fullName(names);
       ctx->Error("Generic function " + ctx->highlightError(fullName.value) + " is not accessible here", fullName.range);
     }
-    ctx->mod = tempFn->getModule();
-    if (tempFn->getTypeCount() != genericTypes.size()) {
-      ctx->Error("Generic function " + ctx->highlightError(tempFn->getName().value) + " has " +
-                     ctx->highlightError(std::to_string(tempFn->getTypeCount())) + " type parameters. But " +
-                     ((tempFn->getTypeCount() > genericTypes.size()) ? "only " : "") +
-                     ctx->highlightError(std::to_string(genericTypes.size())) + " types were provided",
-                 fileRange);
+    ctx->mod = genericFn->getModule();
+    SHOW("Got module of generic function")
+    if (genericTypes.empty()) {
+      SHOW("Checking if all generic abstracts have defaults")
+      if (!genericFn->allTypesHaveDefaults()) {
+        ctx->Error(
+            "Not all generic parameters in this function have a default type associated with it, and hence the generic values can't be empty. Use " +
+                ctx->highlightError("default") + " to use the default type or value of the generic parameter.",
+            fileRange);
+      }
+      SHOW("Check complete")
+    } else if (genericFn->getTypeCount() != genericTypes.size()) {
+      ctx->Error(
+          "Generic function " + ctx->highlightError(genericFn->getName().value) + " has " +
+              ctx->highlightError(std::to_string(genericFn->getTypeCount())) + " generic parameters. But " +
+              ((genericFn->getTypeCount() > genericTypes.size()) ? "only " : "") +
+              ctx->highlightError(std::to_string(genericTypes.size())) +
+              " values were provided. Not all generic parameters have default values, and hence the number of values provided must match. Use " +
+              ctx->highlightError("default") + " to use the default type or value of the generic parameter.",
+          fileRange);
     }
-    Vec<IR::QatType*> types;
-    for (auto* typ : genericTypes) {
-      types.push_back(typ->emit(ctx));
+    SHOW("About to create toFill")
+    Vec<IR::GenericToFill*> types;
+    for (usize i = 0; i < genericTypes.size(); i++) {
+      auto* gen = genericTypes.at(i);
+      if (gen->isConst() && (gen->asConst()->nodeType() == NodeType::constantDefault)) {
+        SHOW("Generic is const and const generic is default expression")
+        ((ast::ConstantDefault*)(genericTypes.at(i)->asConst()))->setGenericAbstract(genericFn->getGenericAt(i));
+      } else if (genericFn->getGenericAt(i)->isConst() &&
+                 (genericFn->getGenericAt(i)->asConst()->getType() != nullptr)) {
+        SHOW("Generic abstract is const and has valid type")
+        if (gen->asConst()->nodeType() == NodeType::integerLiteral) {
+          ((ast::IntegerLiteral*)(gen->asConst()))->setType(genericFn->getGenericAt(i)->asConst()->getType());
+        } else if (gen->asConst()->nodeType() == NodeType::unsignedLiteral) {
+          ((ast::UnsignedLiteral*)(gen->asConst()))->setType(genericFn->getGenericAt(i)->asConst()->getType());
+        }
+      }
+      types.push_back(genericTypes.at(i)->toFill(ctx));
     }
-    auto* fnRes = tempFn->fillGenerics(types, ctx, fileRange);
-    ctx->fn     = fun;
+    SHOW("Calling fillGenerics")
+    auto* fnRes = genericFn->fillGenerics(types, ctx, fileRange);
+    SHOW("fillGenerics completed")
+    ctx->fn = fun;
     if (curr) {
       curr->setActive(ctx->builder);
     }
--- src/ast/expressions/generic_entity.hpp
+++ src/ast/expressions/generic_entity.hpp
@@ -2,18 +2,19 @@
 #define QAT_AST_EXPRESSIONS_GENERIC_ENTITY_HPP
 
 #include "../expression.hpp"
+#include "../generics.hpp"
 #include "../types/qat_type.hpp"
 
 namespace qat::ast {
 
 class GenericEntity : public Expression {
 private:
-  u32             relative;
-  Vec<Identifier> names;
-  Vec<QatType*>   genericTypes;
+  u32               relative;
+  Vec<Identifier>   names;
+  Vec<FillGeneric*> genericTypes;
 
 public:
-  GenericEntity(u32 _relative, Vec<Identifier> _names, Vec<QatType*> _genericTypes, FileRange _fileRange);
+  GenericEntity(u32 _relative, Vec<Identifier> _names, Vec<FillGeneric*> _genericTypes, FileRange _fileRange);
 
   useit IR::Value* emit(IR::Context* ctx) final;
   useit Json       toJson() const final;

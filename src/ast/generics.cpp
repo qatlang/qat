48805b8571b7e4c8240f2d1767cdf19352cab797
Aldrin Mathew <admin@aldrinmathew.com>
2023-01-30 14:13:53 +0530

    Added `ast::FillGeneric`
    - `ast::FillGeneric` is used to represent a value that is supposed to be
      set for a generic parameter
    
    - It can represent either a type, or a constant expression
    
    - It is also convertible to IR::GenericToFill which is used in the
      creation of variants for generic entities

---
+++ src/ast/generics.cpp
@@ -0,0 +1,49 @@
+#include "./generics.hpp"
+#include "expression.hpp"
+#include "node.hpp"
+
+namespace qat::ast {
+
+FillGeneric::FillGeneric(QatType* type)
+    : data(type), kind(FillGenericKind::typed){SHOW("FillGeneric created for type " << type->toString())}
+
+      FillGeneric::FillGeneric(ConstantExpression * constant)
+    : data(constant), kind(FillGenericKind::constant) {
+  SHOW("FillGeneric created for const" << constant->toString())
+}
+
+bool FillGeneric::isType() const { return kind == FillGenericKind::typed; }
+
+bool FillGeneric::isConst() const { return kind == FillGenericKind::constant; }
+
+QatType* FillGeneric::asType() const { return (QatType*)data; }
+
+ConstantExpression* FillGeneric::asConst() const { return (ConstantExpression*)data; }
+
+FileRange const& FillGeneric::getRange() const { return asType()->fileRange; }
+
+IR::GenericToFill* FillGeneric::toFill(IR::Context* ctx) const {
+  SHOW("ToFill called")
+  if (isType()) {
+    return IR::GenericToFill::get(asType()->emit(ctx), asType()->fileRange);
+  } else {
+    return IR::GenericToFill::get(asConst()->emit(ctx), asConst()->fileRange);
+  }
+}
+
+String FillGeneric::toString() const {
+  if (isType()) {
+    return asType()->toString();
+  } else {
+    return asConst()->toString();
+  }
+}
+
+Json FillGeneric::toJson() const {
+  return Json()
+      ._("kind", (kind == FillGenericKind::typed) ? "typed" : "const")
+      ._("type", isType() ? asType()->toJson() : Json())
+      ._("constant", isConst() ? asConst()->toJson() : Json());
+}
+
+} // namespace qat::ast
---
+++ src/ast/generics.hpp
@@ -0,0 +1,39 @@
+#ifndef QAT_AST_GENERICS_HPP
+#define QAT_AST_GENERICS_HPP
+
+#include "./types/qat_type.hpp"
+#include "expression.hpp"
+#include "node.hpp"
+
+namespace qat::ast {
+
+enum class FillGenericKind {
+  typed,
+  constant,
+};
+
+class FillGeneric {
+  void*           data;
+  FillGenericKind kind;
+
+public:
+  explicit FillGeneric(QatType* type);
+  explicit FillGeneric(ConstantExpression* expression);
+
+  useit bool isType() const;
+  useit bool isConst() const;
+
+  useit QatType*            asType() const;
+  useit ConstantExpression* asConst() const;
+
+  useit FileRange const& getRange() const;
+
+  useit IR::GenericToFill* toFill(IR::Context* ctx) const;
+
+  useit Json   toJson() const;
+  useit String toString() const;
+};
+
+} // namespace qat::ast
+
+#endif

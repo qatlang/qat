89fb1ce2395af6d3f3b69b5036bb0a7f36d723f0
Aldrin Mathew <admin@aldrinmathew.com>
2023-11-29 08:33:33 +0530

    Added `IR::LinkNames` and `IR::LinkNameUnit`
    - Added LinkNames and LinkNameUnit to determine the linking names for
      entities
    
    - Completed the unified naming system for entities during linking

---
+++ src/IR/link_names.cpp
@@ -0,0 +1,197 @@
+#include "./link_names.hpp"
+#include "./qat_module.hpp"
+
+namespace qat {
+
+LinkNames::LinkNames() : units(), foreignID(), parentMod(nullptr) {}
+
+LinkNames::LinkNames(Vec<LinkNameUnit> _units, Maybe<String> _foreignID, IR::QatModule* _mod)
+    : units(_units), foreignID(_foreignID), parentMod(_mod) {}
+
+void LinkNames::addUnit(LinkNameUnit unit, Maybe<String> entityForeignID) {
+  units.push_back(unit);
+  if (entityForeignID) {
+    foreignID = entityForeignID;
+  }
+}
+
+LinkNames LinkNames::newWith(LinkNameUnit unit, Maybe<String> entityForeignID) {
+  auto result = *this;
+  result.addUnit(unit, entityForeignID);
+  return result;
+}
+
+String LinkNames::toName() const {
+  auto isForeign = [&](String const& id) {
+    if (foreignID.has_value()) {
+      return (foreignID.value() == id);
+    } else {
+      return parentMod && parentMod->isInForeignModuleOfType(id);
+    }
+  };
+  if (isForeign("C")) {
+    return units.back().getUsableName();
+  } else if (isForeign("C++")) {
+    String result("");
+    // FIXME - Implement C++ name mangling
+    return result;
+  } else {
+    String result = ((!units.empty()) && (units.front().unitType == LinkUnitType::name ||
+                                          units.front().unitType == LinkUnitType::genericTypeValue ||
+                                          units.front().unitType == LinkUnitType::genericPrerunValue))
+                        ? ""
+                        : "qat'";
+    for (usize i = 0; i < units.size(); i++) {
+      auto& unit = units.at(i);
+      if (i != 0 && unit.unitType != LinkUnitType::genericList) {
+        result += ":";
+      }
+      switch (unit.unitType) {
+        case LinkUnitType::box: {
+          result += "box_";
+          result += unit.getUsableName();
+          break;
+        }
+        case LinkUnitType::lib: {
+          result += "lib_";
+          result += unit.getUsableName();
+          break;
+        }
+        case LinkUnitType::type: {
+          result += "type_";
+          result += unit.getUsableName();
+          break;
+        }
+        case LinkUnitType::choice: {
+          result += "type_choice_";
+          result += unit.getUsableName();
+          break;
+        }
+        case LinkUnitType::mix: {
+          result += "type_mix_";
+          result += unit.getUsableName();
+          break;
+        }
+        case LinkUnitType::staticField: {
+          result += "static_";
+          result += unit.getUsableName();
+          break;
+        }
+        case LinkUnitType::region: {
+          result += "region_";
+          result += unit.getUsableName();
+          break;
+        }
+        case LinkUnitType::doType: {
+          result += "do_type:[" + unit.subNames.front().toName() + "]";
+          break;
+        }
+        case LinkUnitType::skill: {
+          result += "skill_";
+          result += unit.getUsableName();
+          break;
+        }
+        case LinkUnitType::doSkill: {
+          result += "do_skill:[" + unit.getUsableName() + "]:for_type:[" + unit.subNames.front().toName() + "]";
+          break;
+        }
+        case LinkUnitType::function: {
+          result += "fn_" + unit.getUsableName();
+          break;
+        }
+        case LinkUnitType::staticFunction: {
+          result += "staticfn_" + unit.getUsableName();
+          break;
+        }
+        case LinkUnitType::variationMethod: {
+          result += "variation_" + unit.getUsableName();
+          break;
+        }
+        case LinkUnitType::method: {
+          result += "method_" + unit.getUsableName();
+          break;
+        }
+        case LinkUnitType::defaultConstructor: {
+          result += "constructor_default";
+          break;
+        }
+        case LinkUnitType::fromConvertor: {
+          result += "convertor_from:[" + unit.getUsableName() + "]";
+          break;
+        }
+        case LinkUnitType::toConvertor: {
+          result += "convertor_to:[" + unit.getUsableName() + "]";
+          break;
+        }
+        case LinkUnitType::constructor: {
+          result += "constructor_from:[";
+          for (usize j = 0; j < unit.subNames.size(); j++) {
+            result += unit.subNames.at(j).toName();
+            if (j != (unit.subNames.size() - 1)) {
+              result += ",";
+            }
+          }
+          result += "]";
+          break;
+        }
+        case LinkUnitType::copyConstructor: {
+          result += "constructor_copy";
+          break;
+        }
+        case LinkUnitType::copyAssignment: {
+          result += "operator_copy";
+          break;
+        }
+        case LinkUnitType::moveConstructor: {
+          result += "constructor_move";
+          break;
+        }
+        case LinkUnitType::moveAssignment: {
+          result += "operator_move";
+          break;
+        }
+        case LinkUnitType::binaryOperator: {
+          result += "operator_binary(" + unit.getUsableName() + "):[" + unit.subNames.front().toName() + "]";
+          break;
+        }
+        case LinkUnitType::unaryOperator: {
+          result += "operator_unary(" + unit.getUsableName() + ")";
+        }
+        case LinkUnitType::destructor: {
+          result += "destructor";
+          break;
+        }
+        case LinkUnitType::global: {
+          result += "global_" + unit.getUsableName();
+          break;
+        }
+        case LinkUnitType::genericList: {
+          result += ":[";
+          for (usize j = 0; j < unit.subNames.size(); j++) {
+            result += unit.subNames.at(j).toName();
+            if (j != (unit.subNames.size() - 1)) {
+              result += ",";
+            }
+          }
+          result += "]";
+          break;
+        }
+        case LinkUnitType::name:
+        case LinkUnitType::typeName:
+        case LinkUnitType::genericPrerunValue:
+        case LinkUnitType::genericTypeValue: {
+          result += unit.getUsableName();
+          break;
+        }
+      }
+    }
+    return result;
+  }
+}
+
+LinkNameUnit::LinkNameUnit(String _name, LinkUnitType _namingType, Maybe<String> _alias, Vec<LinkNames> _subNames)
+    : name(_name), unitType(_namingType), alias(_alias), subNames(_subNames) {}
+
+String LinkNameUnit::getUsableName() const { return alias.value_or(name); }
+
+} // namespace qat
---
+++ src/IR/link_names.hpp
@@ -0,0 +1,77 @@
+#ifndef QAT_IR_LINK_NAMES_HPP
+#define QAT_IR_LINK_NAMES_HPP
+
+#include "../utils/helpers.hpp"
+#include "../utils/macros.hpp"
+
+namespace qat::IR {
+class QatModule;
+}
+
+namespace qat {
+
+enum class LinkUnitType {
+  lib,
+  box,
+  type,
+  function,
+  method,
+  variationMethod,
+  staticFunction,
+  constructor,
+  fromConvertor,
+  toConvertor,
+  defaultConstructor,
+  copyConstructor,
+  copyAssignment,
+  moveConstructor,
+  moveAssignment,
+  binaryOperator,
+  unaryOperator,
+  destructor,
+  staticField,
+  choice,
+  mix,
+  genericList,
+  genericPrerunValue,
+  genericTypeValue,
+  region,
+  skill,
+  doType,
+  doSkill,
+  name,
+  typeName,
+  global,
+};
+
+class LinkNameUnit;
+
+class LinkNames {
+public:
+  Vec<LinkNameUnit> units;
+  Maybe<String>     foreignID;
+  IR::QatModule*    parentMod;
+
+  LinkNames();
+  LinkNames(Vec<LinkNameUnit> _units, Maybe<String> _foreignID, IR::QatModule* _mod);
+
+  void            addUnit(LinkNameUnit unit, Maybe<String> foreignID);
+  useit LinkNames newWith(LinkNameUnit unit, Maybe<String> foreignID);
+  useit String    toName() const;
+};
+
+class LinkNameUnit {
+public:
+  String         name;
+  LinkUnitType   unitType;
+  Maybe<String>  alias;
+  Vec<LinkNames> subNames;
+
+  LinkNameUnit(String _name, LinkUnitType _namingType, Maybe<String> _alias = None, Vec<LinkNames> subNames = {});
+
+  useit String getUsableName() const;
+};
+
+} // namespace qat
+
+#endif

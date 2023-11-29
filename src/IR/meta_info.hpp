c6d10e5861259b2686bff075bdbbc3776c44dbc8
Aldrin Mathew <admin@aldrinmathew.com>
2023-11-29 11:17:33 +0530

    Added `IR::MetaInfo`
    - Added IR::MetaInfo which is used for storing metadata information
      about functions and types in the language
    
    - As of now, this is intended to be used for foreign entities

---
+++ src/IR/meta_info.hpp
@@ -0,0 +1,51 @@
+#ifndef QAT_IR_META_INFO_HPP
+#define QAT_IR_META_INFO_HPP
+
+#include "../utils/identifier.hpp"
+#include "./types/string_slice.hpp"
+#include "value.hpp"
+
+namespace qat::IR {
+
+struct MetaInfo {
+  explicit MetaInfo(Vec<Pair<Identifier, IR::PrerunValue*>> keyValues) {
+    for (auto& kv : keyValues) {
+      keys.push_back(kv.first);
+      values.push_back(kv.second);
+    }
+  }
+
+  Vec<Identifier>       keys;
+  Vec<IR::PrerunValue*> values;
+
+  useit bool hasKey(String const& name) const {
+    for (auto& k : keys) {
+      if (k.value == name) {
+        return true;
+      }
+    }
+    return false;
+  }
+  useit IR::PrerunValue* getValueFor(String const& name) const {
+    usize ind = 0;
+    for (auto& k : keys) {
+      if (k.value == name) {
+        return values.at(ind);
+      }
+      ind++;
+    }
+    return nullptr;
+  }
+  useit Maybe<String> getForeignID() const {
+    if (hasKey("foreign")) {
+      auto* val = getValueFor("foreign");
+      return val->getType()->asStringSlice()->toPrerunGenericString(val);
+    } else {
+      return None;
+    }
+  }
+};
+
+} // namespace qat::IR
+
+#endif

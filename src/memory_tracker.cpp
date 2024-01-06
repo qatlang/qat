39ff4c7daa0c4729e22a5af4decf3a8f389fbf01
Aldrin Mathew <admin@aldrinmathew.com>
2024-01-06 16:06:35 +0530

    Updated memory tracker
    - Since static and global objects are not destroyed when the memory
      report is done, there will always be some objects in memory
    
    - So now MemoryTracker only tracks the total memory used, and not
      anything else
    
    - The codebase has been reviewed and there are no undeleted instances to
      be found. There has also not been unbalanced deletes for a long time
      now

--- src/memory_tracker.cpp
+++ src/memory_tracker.cpp
@@ -2,52 +2,33 @@
 #include "cli/color.hpp"
 #include "cli/version.hpp"
 #include "show.hpp"
+#include "utils/logger.hpp"
+#include "utils/qat_region.hpp"
 #include <iostream>
 
 namespace qat {
 
-u64 MemoryTracker::objectsOnHeap = 0;
-
 u64 MemoryTracker::totalSize = 0;
-
-u64 MemoryTracker::negativeCounter = 0;
-
-u64 MemoryTracker::getHeapCounter() { return objectsOnHeap; }
 
 u64 MemoryTracker::getTotalSize() { return totalSize; }
 
-void MemoryTracker::addHeapCounter() { ++objectsOnHeap; }
-
 void MemoryTracker::incrementSize(usize size) { totalSize += size; }
 
-void MemoryTracker::subtractHeapCounter() {
-  if (objectsOnHeap > 0u) {
-    --objectsOnHeap;
-  } else {
-    ++negativeCounter;
-  }
-}
-
 void MemoryTracker::report() {
-  SHOW("\nMemory Tracker")
-  SHOW("Objects in heap memory :: " << objectsOnHeap)
-  SHOW("Total heap memory      :: " << totalSize)
-  SHOW("Unbalanced deletes     :: " << negativeCounter << "\n")
+  auto& log = qat::Logger::get();
+  log->diagnostic("Memory Used   -> " + (totalSize > 1048576
+                                             ? (std::to_string(((double)totalSize) / 1048576) + " MB")
+                                             : (totalSize > 1024 ? (std::to_string((double)totalSize / 1024) + " KB")
+                                                                 : (std::to_string(totalSize) + " bytes"))));
 }
 
 } // namespace qat
 
 void* operator new(qat::usize size)
 #if PlatformIsLinux
-_GLIBCXX_THROW(std::bad_alloc) 
+    _GLIBCXX_THROW(std::bad_alloc)
 #endif
 {
-  qat::MemoryTracker::addHeapCounter();
   qat::MemoryTracker::incrementSize(size);
   return malloc(size);
-}
-
-void operator delete(void* ptr) noexcept {
-  qat::MemoryTracker::subtractHeapCounter();
-  free(ptr);
 }
--- src/memory_tracker.hpp
+++ src/memory_tracker.hpp
@@ -9,16 +9,11 @@ namespace qat {
 
 class MemoryTracker {
 private:
-  static u64 objectsOnHeap;
   static u64 totalSize;
-  static u64 negativeCounter;
 
 public:
-  useit static u64 getHeapCounter();
   useit static u64 getTotalSize();
-  static void      addHeapCounter();
   static void      incrementSize(usize size);
-  static void      subtractHeapCounter();
   static void      report();
 };
 
@@ -26,10 +21,7 @@ public:
 
 void* operator new(qat::usize size)
 #if PlatformIsLinux
-_GLIBCXX_THROW(std::bad_alloc)
+    _GLIBCXX_THROW(std::bad_alloc)
 #endif
-; // NOLINT(readability-redundant-declaration)
-
-void operator delete(void* ptr) noexcept; // NOLINT(readability-redundant-declaration)
-
+        ; // NOLINT(readability-redundant-declaration)
 #endif

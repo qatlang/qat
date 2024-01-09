780f4863a4fa9dd688a27d1c465a73fa8d0f124e
Aldrin Mathew <admin@aldrinmathew.com>
2024-01-09 11:33:25 +0530

    `say` now uses IR::Logic::formatValues
    - All formatting logic has been removed from ast::Say and reimplemented
      in IR::Logic::formatValues, which can also handle prerun expressions
      and register values very well

--- src/ast/sentences/say_sentence.cpp
+++ src/ast/sentences/say_sentence.cpp
@@ -1,5 +1,6 @@
 #include "./say_sentence.hpp"
-#include "../../IR/types/string_slice.hpp"
+#include "../../IR/logic.hpp"
+#include "../../IR/stdlib.hpp"
 #include "../../cli/config.hpp"
 
 namespace qat::ast {
@@ -14,118 +15,22 @@ IR::Value* SayLike::emit(IR::Context* ctx) {
     auto* mod = ctx->getMod();
     SHOW("Current block is: " << ctx->getActiveFunction()->getBlock()->getName())
     mod->linkNative(IR::NativeUnit::printf);
-    auto*             printfFn = mod->getLLVMModule()->getFunction("printf");
-    String            formatStr;
-    Vec<IR::Value*>   valuesIR;
-    Vec<llvm::Value*> llvmValues;
-    for (auto* exp : expressions) {
-      valuesIR.push_back(exp->emit(ctx));
-      SHOW("Say sentence member type: " << valuesIR.back()->getType()->toString());
+    auto*           printfFn = mod->getLLVMModule()->getFunction("printf");
+    Vec<IR::Value*> valuesIR;
+    Vec<FileRange>  valuesRange;
+    for (usize i = 0; i < expressions.size(); i++) {
+      valuesRange.push_back(expressions[i]->fileRange);
+      valuesIR.push_back(expressions[i]->emit(ctx));
     }
-    std::function<void(IR::Value*, usize)> formatter = [&](IR::Value* value, usize index) {
-      SHOW("Say sentence value type is: " << value->getType()->toString())
-      auto* val = value;
-      auto* typ = val->getType();
-      if (typ->isReference()) {
-        val->loadImplicitPointer(ctx->builder);
-        val = new IR::Value(ctx->builder.CreateLoad(typ->asReference()->getSubType()->getLLVMType(), val->getLLVM()),
-                            typ->asReference()->getSubType(), false, IR::Nature::temporary);
-        typ = typ->asReference()->getSubType();
-        SHOW("Reference in say sentence of type " << typ->toString())
-      }
-      if (typ->isStringSlice()) {
-        formatStr += "%.*s";
-        if (llvm::isa<llvm::Constant>(val->getLLVM())) {
-          auto* strStruct = (llvm::ConstantStruct*)val->getLLVM();
-          llvmValues.push_back(strStruct->getAggregateElement(1u));
-          llvmValues.push_back(strStruct->getAggregateElement(0u));
-        } else if (llvm::isa<llvm::AllocaInst>(val->getLLVM())) {
-          auto* strStruct = val->getLLVM();
-          llvmValues.push_back(ctx->builder.CreateLoad(
-              llvm::Type::getInt64Ty(ctx->llctx),
-              ctx->builder.CreateStructGEP(IR::StringSliceType::get(ctx)->getLLVMType(), strStruct, 1)));
-          llvmValues.push_back(ctx->builder.CreateLoad(
-              llvm::Type::getInt8Ty(ctx->llctx)->getPointerTo(),
-              ctx->builder.CreateStructGEP(IR::StringSliceType::get(ctx)->getLLVMType(), strStruct, 0)));
-        } else {
-          auto* strStruct = val->getLLVM();
-          auto* tempAlloc = ctx->builder.CreateAlloca(IR::StringSliceType::get(ctx)->getLLVMType());
-          ctx->builder.CreateStore(strStruct, tempAlloc);
-          llvmValues.push_back(ctx->builder.CreateLoad(
-              llvm::Type::getInt64Ty(ctx->llctx),
-              ctx->builder.CreateStructGEP(IR::StringSliceType::get(ctx)->getLLVMType(), tempAlloc, 1)));
-          llvmValues.push_back(ctx->builder.CreateLoad(
-              llvm::Type::getInt8Ty(ctx->llctx)->getPointerTo(),
-              ctx->builder.CreateStructGEP(IR::StringSliceType::get(ctx)->getLLVMType(), tempAlloc, 0)));
-        }
-      } else if (typ->isCType() && typ->asCType()->isCString()) {
-        formatStr += "%s";
-        val->loadImplicitPointer(ctx->builder);
-        llvmValues.push_back(val->getLLVM());
-      } else if (typ->isInteger()) {
-        formatStr += "%d";
-        val->loadImplicitPointer(ctx->builder);
-        llvmValues.push_back(val->getLLVM());
-      } else if (typ->isUnsignedInteger()) {
-        formatStr += "%u";
-        val->loadImplicitPointer(ctx->builder);
-        llvmValues.push_back(val->getLLVM());
-      } else if (typ->isFloat()) {
-        formatStr += "%f";
-        val->loadImplicitPointer(ctx->builder);
-        if (typ->asFloat()->getKind() != IR::FloatTypeKind::_64) {
-          llvmValues.push_back(ctx->builder.CreateFPCast(val->getLLVM(), llvm::Type::getDoubleTy(ctx->llctx)));
-        } else {
-          llvmValues.push_back(val->getLLVM());
-        }
-      } else if (typ->isPointer()) {
-        formatStr += "%p";
-        val->loadImplicitPointer(ctx->builder);
-        llvmValues.push_back(val->getLLVM());
-      } else if (typ->isTuple()) {
-        formatStr += "(";
-        auto subTypes = typ->asTuple()->getSubTypes();
-        for (usize i = 0; i < subTypes.size(); i++) {
-          auto* subVal = val->getLLVM();
-          subVal       = ctx->builder.CreateGEP(subTypes.at(i)->getLLVMType(), subVal,
-                                                llvm::ConstantInt::get(llvm::Type::getInt64Ty(ctx->llctx), i));
-          subVal       = ctx->builder.CreateLoad(subTypes.at(i)->getLLVMType(), subVal);
-          formatter(new IR::Value(subVal, subTypes.at(i), val->isVariable(), val->getNature()), index);
-          if (i != (subTypes.size() - 1)) {
-            formatStr += "; ";
-          }
-        }
-        formatStr += ")";
-      } else if (typ->isArray()) {
-        formatStr += "[";
-        auto* subType = typ->asArray()->getElementType();
-        for (usize i = 0; i < typ->asArray()->getLength(); i++) {
-          auto* subVal = ctx->builder.CreateGEP(subType->getLLVMType(), val->getLLVM(),
-                                                // TODO - Change index type to usize llvm equivalent
-                                                llvm::ConstantInt::get(llvm::Type::getInt64Ty(ctx->llctx), i));
-          subVal       = ctx->builder.CreateLoad(subType->getLLVMType(), subVal);
-          formatter(new IR::Value(subVal, subType, val->isVariable(), val->getNature()), i);
-          if (i != (typ->asArray()->getLength() - 1)) {
-            formatStr += ", ";
-          }
-        }
-        formatStr += "]";
-      } else {
-        ctx->Error("Cannot convert this value to a string", expressions.at(index)->fileRange);
-      }
-    };
-    for (usize i = 0; i < valuesIR.size(); i++) {
-      formatter(valuesIR.at(i), i);
-    }
+    auto fmtRes = IR::Logic::formatValues(ctx, valuesIR, {}, fileRange);
     if (sayType != SayType::only) {
-      formatStr += "\n";
+      fmtRes.first += "\n";
     }
     Vec<llvm::Value*> values;
-    values.push_back(ctx->builder.CreateGlobalStringPtr(formatStr, ctx->getGlobalStringName()));
-    for (auto* val : llvmValues) {
+    values.push_back(ctx->builder.CreateGlobalStringPtr(fmtRes.first, ctx->getGlobalStringName()));
+    for (auto* val : fmtRes.second) {
       values.push_back(val);
     }
-    llvmValues.clear();
     ctx->builder.CreateCall(printfFn->getFunctionType(), printfFn, values);
     return nullptr;
   } else {

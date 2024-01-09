11d300fda686d983eb704522f88b7f4beb85ec67
Aldrin Mathew <admin@aldrinmathew.com>
2024-01-09 11:31:33 +0530

    Added formatValues & panicInFunction to IR::Logic
    - formatValues can be used to format a list of values and then return a
      string and list of llvm values
    
    - panicInFunction is used internally to add panics during bounds check
      fails and so on

--- src/IR/logic.hpp
+++ src/IR/logic.hpp
@@ -8,10 +8,15 @@ namespace qat::IR {
 
 class Logic {
 public:
-  useit static llvm::AllocaInst* newAlloca(IR::Function* fun, const String& name, llvm::Type* type);
+  useit static llvm::AllocaInst* newAlloca(IR::Function* fun, Maybe<String> name, llvm::Type* type);
   useit static String            getGenericVariantName(String mainName, Vec<IR::GenericToFill*>& types);
   useit static bool compareConstantStrings(llvm::Constant* lhsBuff, llvm::Constant* lhsCount, llvm::Constant* rhsBuff,
                                            llvm::Constant* rhsCount, llvm::LLVMContext& llCtx);
+
+  static Pair<String, Vec<llvm::Value*>> formatValues(IR::Context* ctx, Vec<IR::Value*> values, Vec<FileRange> ranges,
+                                                      FileRange fileRange);
+  static void panicInFunction(IR::Function* fun, Vec<IR::Value*> values, Vec<FileRange> ranges, FileRange fileRange,
+                              IR::Context* ctx);
 };
 
 } // namespace qat::IR

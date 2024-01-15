3d485482e10c30202bc03ab4812ff31402757c30
Aldrin Mathew <admin@aldrinmathew.com>
2024-01-15 22:48:11 +0530

    Added simple `WorkGroup` for multithreading
    - Added WorkGroup that can temporarily hold a list of threads that are
      active, and when required wait on them and clear the list

---
+++ src/utils/workgroup.hpp
@@ -0,0 +1,24 @@
+#ifndef QAT_WORKGROUP_HPP
+#define QAT_WORKGROUP_HPP
+
+#include "helpers.hpp"
+#include <thread>
+
+namespace qat {
+
+class Workgroup {
+  Vec<std::thread> workers;
+
+public:
+  void inline add(std::thread thr) { workers.push_back(std::move(thr)); }
+  void inline wait() {
+    for (auto& thr : workers) {
+      thr.join();
+    }
+    workers.clear();
+  }
+};
+
+} // namespace qat
+
+#endif

1c46a4e357fb4c6c29801930c5328ac85ca8186f
Aldrin Mathew <admin@aldrinmathew.com>
2024-01-15 22:51:55 +0530

    Debug build logs now use Logger's osyncstream
    - If normal std::cout is used, then those outputs are shown first and at
      the end of the program, the output from the logger is shown
    
    - So debug logs use the same stream as the logger

--- src/show.hpp
+++ src/show.hpp
@@ -2,6 +2,7 @@
 #define QAT_SHOW_HPP
 
 #include "./cli/color.hpp"
+#include "./utils/logger.hpp"
 #include <iostream>
 
 #define HLIGHT(col, val) colors::col << val << colors::reset
@@ -9,8 +10,8 @@
 #define SHOW(val)
 #define HIGHLIGHT(val)
 #else
-#define SHOW(val)           std::cout << colors::blue << val << colors::reset << "\n";
-#define HIGHLIGHT(col, val) std::cout << colors::col << val << colors::reset << "\n";
+#define SHOW(val)           qat::Logger::get()->out << colors::blue << val << colors::reset << "\n";
+#define HIGHLIGHT(col, val) qat::Logger::get()->out << colors::col << val << colors::reset << "\n";
 #endif
 
 #endif

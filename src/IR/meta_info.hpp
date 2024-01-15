88ba368ae84a905debe038734fb7742fb12cf028
Aldrin Mathew <admin@aldrinmathew.com>
2024-01-15 19:02:23 +0530

    IR::MetaInfo now supports ranges for values
    - The field value can now have a value associated with it

--- src/IR/meta_info.hpp
+++ src/IR/meta_info.hpp
@@ -8,7 +8,8 @@
 namespace qat::IR {
 
 struct MetaInfo {
-  MetaInfo(Vec<Pair<Identifier, IR::PrerunValue*>> keyValues, FileRange _fileRange) : fileRange(_fileRange) {
+  MetaInfo(Vec<Pair<Identifier, IR::PrerunValue*>> keyValues, Vec<FileRange> _valueRanges, FileRange _fileRange)
+      : fileRange(_fileRange), valueRanges(_valueRanges) {
     for (auto& kv : keyValues) {
       keys.push_back(kv.first);
       values.push_back(kv.second);
@@ -17,6 +18,7 @@ struct MetaInfo {
 
   Vec<Identifier>       keys;
   Vec<IR::PrerunValue*> values;
+  Vec<FileRange>        valueRanges;
   FileRange             fileRange;
 
   useit bool hasKey(String const& name) const {
@@ -37,6 +39,17 @@ struct MetaInfo {
     }
     return nullptr;
   }
+
+  useit FileRange getValueRangeFor(String const& name) const {
+    usize ind = 0;
+    for (auto& k : keys) {
+      if (k.value == name) {
+        return valueRanges.at(ind);
+      }
+      ind++;
+    }
+  }
+
   useit Maybe<String> getForeignID() const {
     if (hasKey("foreign")) {
       auto* val = getValueFor("foreign");

15f3ba00987bb047bb176c0122a98ea21aa46bdd
Aldrin Mathew <admin@aldrinmathew.com>
2024-01-24 15:48:01 +0530

    Improved logic for `DefineChoiceType`
    - If the value is provided for a field mid-way, the values for the
      previous fields are now calculated
    
    - During calculation if the value of any field is the minimum possible
      then error is shown to the user about that
    
    - While calculating the values for fields after a field with value, the
      check is done for the maximum value as well
    
    - Added improved type checking for values of choice variants

--- src/ast/define_choice_type.cpp
+++ src/ast/define_choice_type.cpp
@@ -32,8 +32,20 @@ void DefineChoiceType::createType(IR::Context* ctx) {
       if (providedType && fields.at(i).second.value()->hasTypeInferrance()) {
         fields.at(i).second.value()->asTypeInferrable()->setInferenceType(providedType.value());
       }
-      auto iVal = fields.at(i).second.value()->emit(ctx);
-      if (providedType && !iVal->getType()->isSame(providedType.value())) {
+      auto iVal   = fields.at(i).second.value()->emit(ctx);
+      auto iValTy = iVal->getType();
+      if (!iValTy->isInteger() && !iValTy->isUnsignedInteger() &&
+          !(iValTy->isCType() &&
+            (iValTy->asCType()->getSubType()->isUnsignedInteger() || iValTy->asCType()->getSubType()->isInteger()))) {
+        ctx->Error(
+            "The value for a variant of this choice type should be of integer or unsigned integer type. Got an expression of type " +
+                ctx->highlightError(iValTy->toString()),
+            fields.at(i).second.value()->fileRange);
+      }
+      if (iValTy->isCType()) {
+        iValTy = iValTy->asCType()->getSubType();
+      }
+      if (providedType.has_value() && !iVal->getType()->isSame(providedType.value())) {
         ctx->Error("The provided value of the variant of this choice type has type " +
                        ctx->highlightError(iVal->getType()->toString()) +
                        ", which does not match the provided underlying type " +
@@ -43,23 +55,58 @@ void DefineChoiceType::createType(IR::Context* ctx) {
         ctx->Error("Value for variant for choice type should either be a signed or unsigned integer type",
                    fields.at(i).second.value()->fileRange);
       }
-      if (variantValueType && variantValueType->isSame(iVal->getType())) {
+      if (variantValueType && !variantValueType->isSame(iVal->getType())) {
         ctx->Error("Value provided for this variant does not match the type of the previous values",
                    fields.at(i).second.value()->fileRange);
+      } else if (!variantValueType) {
+        variantValueType = iVal->getType();
       }
-      variantValueType = iVal->getType();
-      lastVal =
+      llvm::ConstantInt* fieldResult =
           llvm::cast<llvm::ConstantInt>(llvm::ConstantFoldConstant(iVal->getLLVMConstant(), ctx->dataLayout.value()));
-      SHOW("Index for field " << fields.at(i).first.value << " is " << iVal)
-      if (!fieldValues.has_value()) {
-        fieldValues = {};
+      if (!lastVal.has_value() && i > 0) {
+        Vec<llvm::ConstantInt*> prevVals; // In reverse
+        for (usize j = i - 1; j >= 0; j--) {
+          if ((prevVals.empty() ? fieldResult : prevVals.back())->isMinValue(iValTy->isInteger()) && j != 0) {
+            ctx->Error("Tried to calculate values for variants before " +
+                           ctx->highlightError(fields.at(i).first.value) +
+                           ". Could not calculate underlying value for the variant " +
+                           ctx->highlightError(fields.at(j).first.value) + " as the variant " +
+                           ctx->highlightError(fields.at(j + 1).first.value) +
+                           " right after this variant has the minimum value possible for the underlying type " +
+                           ctx->highlightError(providedType.has_value() ? providedType.value()->toString()
+                                                                        : iVal->getType()->toString()) +
+                           " of this choice type.",
+                       fields.at(j).first.range);
+          }
+          prevVals.push_back(llvm::cast<llvm::ConstantInt>(llvm::ConstantFoldConstant(
+              llvm::ConstantExpr::getSub(fieldResult,
+                                         llvm::ConstantInt::get(fieldResult->getType(), 1u, iValTy->isInteger())),
+              ctx->dataLayout.value())));
+        }
+        fieldValues = Vec<llvm::ConstantInt*>{};
+        fieldValues.value().insert(fieldValues.value().end(), prevVals.end(), prevVals.begin());
       }
-      fieldValues->push_back(
-          llvm::cast<llvm::ConstantInt>(llvm::ConstantFoldConstant(iVal->getLLVMConstant(), ctx->dataLayout.value())));
+      lastVal = fieldResult;
+      SHOW("Index for field " << fields.at(i).first.value << " is " << *lastVal.value()->getValue().getRawData())
+      if (!fieldValues.has_value()) {
+        fieldValues = Vec<llvm::ConstantInt*>{};
+      }
+      SHOW("Pushing field value")
+      fieldValues->push_back(lastVal.value());
     } else if (lastVal.has_value()) {
+      if (lastVal.value()->isMaxValue(variantValueType->isInteger())) {
+        ctx->Error("Could not calculate value for the variant " + ctx->highlightError(fields[i].first.value) +
+                       " as the variant " + ctx->highlightError(fields[i - 1].first.value) +
+                       " before this has the maximum value possible for the underlying type " +
+                       ctx->highlightError(providedType.has_value() ? providedType.value()->toString()
+                                                                    : variantValueType->toString()) +
+                       " of this choice type",
+                   fields[i].first.range);
+      }
+      SHOW("Last value has value")
       auto newVal = llvm::cast<llvm::ConstantInt>(llvm::ConstantFoldConstant(
-          llvm::ConstantExpr::getAdd(lastVal.value(), llvm::ConstantInt::get(variantValueType->getLLVMType(), 1u,
-                                                                             variantValueType->isInteger())),
+          llvm::ConstantExpr::getAdd(
+              lastVal.value(), llvm::ConstantInt::get(lastVal.value()->getType(), 1u, variantValueType->isInteger())),
           ctx->dataLayout.value()));
       SHOW("Index for field " << fields.at(i).first.value << " is " << newVal)
       fieldValues->push_back(newVal);

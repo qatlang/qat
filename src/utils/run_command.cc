e7b21e1a5127b69246f4ce976094d1c8f849fbbc
Aldrin Mathew <admin@aldrinmathew.com>
2024-03-28 17:03:43 +0530

    Added functions to run commands & get output
    - Added functions to run commands and get status code, stdout and stderr
    
    - These are variations returning slightly different things about the
      created process
    
    - These functions make use of `boost::process`

---
+++ src/utils/run_command.cc
@@ -0,0 +1,64 @@
+#include "./run_command.hpp"
+#include "boost/process/child.hpp"
+#include "boost/process/io.hpp"
+
+namespace qat {
+
+int run_command_get_code(String command, Vec<String> args) {
+  boost::process::ipstream outStream;
+  boost::process::child    childProcess(command, args, boost::process::std_out > boost::process::null);
+  childProcess.wait();
+  return childProcess.exit_code();
+}
+
+Pair<int, String> run_command_get_stdout(String command, Vec<String> args) {
+  boost::process::ipstream outStream;
+  boost::process::child    childProcess(command, args, boost::process::std_out > outStream);
+  childProcess.wait();
+  String output;
+  for (String line; std::getline(outStream, line);) {
+    output += line + "\n";
+  }
+  return Pair<int, String>(childProcess.exit_code(), output);
+}
+
+Pair<int, String> run_command_get_output(String command, Vec<String> args) {
+  boost::process::ipstream outStream;
+  boost::process::child    childProcess(command, args, (boost::process::std_out & boost::process::std_err) > outStream);
+  childProcess.wait();
+  String output;
+  for (String line; std::getline(outStream, line);) {
+    output += line + "\n";
+  }
+  return Pair<int, String>(childProcess.exit_code(), output);
+}
+
+Pair<int, String> run_command_get_stderr(String command, Vec<String> args) {
+  boost::process::ipstream errorStream;
+  boost::process::child    childProcess(command, args, boost::process::std_err > errorStream);
+  childProcess.wait();
+  String errorOutput;
+  for (String line; !std::getline(errorStream, line).eof();) {
+    errorOutput += line + "\n";
+  }
+  return Pair<int, String>(childProcess.exit_code(), errorOutput);
+}
+
+std::tuple<int, String, String> run_command_get_stdout_and_stderr(String command, Vec<String> args) {
+  boost::process::ipstream errorStream;
+  boost::process::ipstream outStream;
+  boost::process::child    childProcess(command, args, boost::process::std_out > outStream,
+                                        boost::process::std_err > errorStream);
+  childProcess.wait();
+  String output;
+  String errorOutput;
+  for (String line; std::getline(outStream, line);) {
+    output += line + "\n";
+  }
+  for (String line; std::getline(errorStream, line);) {
+    errorOutput += line + "\n";
+  }
+  return std::tuple<int, String, String>(childProcess.exit_code(), output, errorOutput);
+}
+
+} // namespace qat
---
+++ src/utils/run_command.hpp
@@ -0,0 +1,21 @@
+#ifndef QAT_UTILS_LAUNCH_PROCESS_HPP
+#define QAT_UTILS_LAUNCH_PROCESS_HPP
+
+#include "helpers.hpp"
+#include "macros.hpp"
+
+namespace qat {
+
+useit int run_command_get_code(String command, Vec<String> args);
+
+useit Pair<int, String> run_command_get_stdout(String command, Vec<String> args);
+
+useit Pair<int, String> run_command_get_output(String command, Vec<String> args);
+
+useit Pair<int, String> run_command_get_stderr(String command, Vec<String> args);
+
+useit std::tuple<int, String, String> run_command_get_stdout_and_stderr(String command, Vec<String> args);
+
+} // namespace qat
+
+#endif

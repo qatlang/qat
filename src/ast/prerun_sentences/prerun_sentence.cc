34cf9b859c60d6c587cbb8a0193a14c31e0ef68a
Aldrin Mathew <admin@aldrinmathew.com>
2024-03-30 12:08:49 +0530

    Fixed logic in `emit_prerun_sentences`
    - `emit_prerun_sentences` had logic to update the level of iteration of
      emission
    
    - The logic has been updated so that the change reflects in early
      returns as well

--- src/ast/prerun_sentences/prerun_sentence.cc
+++ src/ast/prerun_sentences/prerun_sentence.cc
@@ -1,16 +1,17 @@
 #include "./prerun_sentence.hpp"
+#include "../expression.hpp"
 
 namespace qat::ast {
 
 bool emit_prerun_sentences(Vec<PrerunSentence*>& sentences, EmitCtx* ctx) {
   ctx->prerunCallState->increment_emit_nesting();
+  FnAtEnd defer([&]() { ctx->prerunCallState->decrement_emit_nesting(); });
   for (auto snt : sentences) {
     snt->emit(ctx);
     if (ctx->get_pre_call_state()->has_given_value()) {
       return true;
     }
   }
-  ctx->prerunCallState->decrement_emit_nesting();
   return ctx->get_pre_call_state()->has_given_value();
 }
 

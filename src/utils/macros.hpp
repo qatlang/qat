521250f9de93437a63871171ada7096fe451f44e
Aldrin Mathew <admin@aldrinmathew.com>
2024-11-13 20:38:31 +0530

    Added macros to track the runtime in windows builds
    - Added `RUNTIME_IS_MINGW` and `RUNTIME_IS_MSVC` macros to track the
      runtime for the windows platform builds

--- src/utils/macros.hpp
+++ src/utils/macros.hpp
@@ -4,4 +4,12 @@
 #define useit  [[nodiscard]]
 #define exitFn [[noreturn]]
 
+#if defined __MINGW32__ || defined __MINGW64__
+#define RUNTIME_IS_MINGW 1
+#define RUNTIME_IS_MSVC  0
+#elif defined _WIN32 || defined WIN32 || defined WIN64 || defined _WIN64
+#define RUNTIME_IS_MSVC  1
+#define RUNTIME_IS_MINGW 0
+#endif
+
 #endif

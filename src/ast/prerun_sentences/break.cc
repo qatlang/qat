93d1557424bcfc86328d7b57b68506d4d9b8b698
Aldrin Mathew <admin@aldrinmathew.com>
2024-12-03 23:22:38 +0530

    Added `ast::PrerunBreak` for prerun loops
    - AST now supports `break` sentences for prerun loops
    
    - This throws the `InternalPrerunBreak` exception to handle over the
      control flow to the loop handler

---
+++ src/ast/prerun_sentences/break.cc
@@ -0,0 +1,23 @@
+#include "./break.hpp"
+#include "./internal_exceptions.hpp"
+
+namespace qat::ast {
+
+void PrerunBreak::emit(EmitCtx* ctx) {
+  if (ctx->get_pre_call_state()->loopsInfo.empty()) {
+    ctx->Error("This " + ctx->color("break") + " is not inside a loop. Please check your logic", fileRange);
+  }
+  if (tag.has_value()) {
+    for (auto& inf : ctx->get_pre_call_state()->loopsInfo) {
+      if (inf.tag.has_value() && inf.tag->value == tag->value) {
+        throw InternalPrerunBreak{tag->value};
+      }
+    }
+  } else if (ctx->get_pre_call_state()->loopsInfo.size() == 1) {
+    throw InternalPrerunBreak();
+  } else {
+    ctx->Error("Found multiple nested loops. Please provide the tag of the loop to break from", fileRange);
+  }
+}
+
+} // namespace qat::ast
---
+++ src/ast/prerun_sentences/break.hpp
@@ -0,0 +1,24 @@
+#ifndef QAT_AST_PRERUN_SENTENCES_BREAK_HPP
+#define QAT_AST_PRERUN_SENTENCES_BREAK_HPP
+
+#include "prerun_sentence.hpp"
+
+namespace qat::ast {
+
+class PrerunBreak final : public PrerunSentence {
+  Maybe<Identifier> tag;
+
+public:
+  PrerunBreak(Maybe<Identifier> _tag, FileRange _fileRange)
+      : PrerunSentence(std::move(_fileRange)), tag(std::move(_tag)) {}
+
+  useit static inline PrerunBreak* create(Maybe<Identifier> tag, FileRange fileRange) {
+    return std::construct_at(OwnNormal(PrerunBreak), std::move(tag), std::move(fileRange));
+  }
+
+  void emit(EmitCtx* ctx) final;
+};
+
+} // namespace qat::ast
+
+#endif

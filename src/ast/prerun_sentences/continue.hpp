0e554bbfbd82018839545935ff23c35aa81596f0
Aldrin Mathew <admin@aldrinmathew.com>
2024-12-03 23:25:13 +0530

    Added `ast::PrerunContinue`
    - AST now supports `continue` sentences for prerun loops
    
    - This throws `InternalPrerunContinue` exception to handle over the
      control flow to the loop handler

---
+++ src/ast/prerun_sentences/continue.hpp
@@ -0,0 +1,24 @@
+#ifndef QAT_AST_PRERUN_SENTENCE_CONTINUE_HPP
+#define QAT_AST_PRERUN_SENTENCE_CONTINUE_HPP
+
+#include "./prerun_sentence.hpp"
+
+namespace qat::ast {
+
+class PrerunContinue : public PrerunSentence {
+  Maybe<Identifier> tag;
+
+public:
+  PrerunContinue(Maybe<Identifier> _tag, FileRange _fileRange)
+      : PrerunSentence(std::move(_fileRange)), tag(std::move(_tag)) {}
+
+  useit static inline PrerunContinue* create(Maybe<Identifier> tag, FileRange fileRange) {
+    return std::construct_at(OwnNormal(PrerunContinue), std::move(tag), std::move(fileRange));
+  }
+
+  void emit(EmitCtx* ctx) final;
+};
+
+} // namespace qat::ast
+
+#endif

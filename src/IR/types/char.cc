b659c3ff01f96f7554a36ac4ad44c91be491ca1d
Aldrin Mathew <admin@aldrinmathew.com>
2025-04-09 23:13:17 +0530

    `ir::CharType` can now be prerun & be in generics
    - Added functions to `ir::CharType` so that it can be used in prerun
      expressions and in prerun generics
    
    - Added function to convert the constant value of this type to a
      string representation to be used for generics
    
    - Fixed bug that the implementation for `is_type_sized` was not
      present. This caused errors in local declarations.

--- src/IR/types/char.cc
+++ src/IR/types/char.cc
@@ -1,4 +1,6 @@
 #include "./char.hpp"
+#include "../../utils/utils.hpp"
+#include "../value.hpp"
 
 #include <llvm/IR/Type.h>
 
@@ -7,6 +9,25 @@ namespace qat::ir {
 CharType::CharType(llvm::LLVMContext& llctx) {
 	llvmType    = llvm::cast<llvm::Type>(llvm::Type::getIntNTy(llctx, 21u));
 	linkingName = "qat'char";
+}
+
+Maybe<String> CharType::to_prerun_generic_string(ir::PrerunValue* val) const {
+	String str;
+	str.reserve(4);
+	auto utfRes = utils::unicode_scalar_to_utf8(
+	    static_cast<u32>(*llvm::cast<llvm::ConstantInt>(val->get_llvm_constant())->getValue().getRawData()));
+	if (not utfRes.has_value()) {
+		return None;
+	}
+	for (u8 i = 0; i < utfRes->second; i++) {
+		str += utfRes->first[i];
+	}
+	return "`" + str + "`";
+}
+
+Maybe<bool> CharType::equality_of(ir::Ctx* irCtx, ir::PrerunValue* first, ir::PrerunValue* second) const {
+	return llvm::cast<llvm::ConstantInt>(llvm::ConstantFoldCompareInstruction(
+	    llvm::CmpInst::ICMP_EQ, first->get_llvm_constant(), second->get_llvm_constant()));
 }
 
 } // namespace qat::ir
--- src/IR/types/char.hpp
+++ src/IR/types/char.hpp
@@ -19,6 +19,16 @@ class CharType final : public Type {
 		return std::construct_at(OwnNormal(CharType), llctx);
 	}
 
+	useit bool can_be_prerun() const final { return true; }
+
+	useit bool can_be_prerun_generic() const final { return true; }
+
+	useit Maybe<String> to_prerun_generic_string(ir::PrerunValue* val) const final;
+
+	useit Maybe<bool> equality_of(ir::Ctx* irCtx, ir::PrerunValue* first, ir::PrerunValue* second) const final;
+
+	useit bool is_type_sized() const final { return true; }
+
 	useit TypeKind type_kind() const final { return TypeKind::CHAR; }
 
 	useit String to_string() const final { return "char"; }

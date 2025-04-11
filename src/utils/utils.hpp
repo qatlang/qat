d70c66092320d39de80a5c7e2b855db28f0ecca8
Aldrin Mathew <admin@aldrinmathew.com>
2025-04-11 21:21:34 +0530

    Added utf8-unicode conversion functions
    - Added `unicode_scalar_to_utf8` and `utf8_to_unicode_scalar` to
      convert a utf-8 sequence to a unicode scalar value and vice versa

--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -3,6 +3,8 @@
 
 #include "./helpers.hpp"
 #include "./macros.hpp"
+
+#include <array>
 #include <random>
 
 namespace qat::utils {
@@ -59,6 +61,47 @@ useit inline u64 random_number() {
 	return dist(rng);
 }
 
+useit inline Maybe<Pair<std::array<u8, 4>, u8>> unicode_scalar_to_utf8(u32 scalar) {
+	std::array<u8, 4> bytes = {0, 0, 0, 0};
+	if (scalar >= 0x0000 && scalar <= 0x007F) {
+		bytes[0] = (u8)scalar;
+		return std::make_pair(bytes, 1);
+	} else if (scalar >= 0x0080 && scalar <= 0x07FF) {
+		bytes[0] = 0b11000000 | static_cast<u8>(scalar >> 6);
+		bytes[1] = 0b10000000 | (static_cast<u8>(scalar) & 0b00111111);
+		return std::make_pair(bytes, 2);
+	} else if (scalar >= 0x0800 && scalar <= 0xFFFF) {
+		bytes[0] = 0b11100000 | (static_cast<u8>(scalar >> 12) & 0b00001111);
+		bytes[1] = 0b10000000 | (static_cast<u8>(scalar >> 6) & 0b00111111);
+		bytes[2] = 0b10000000 | (static_cast<u8>(scalar) & 0b00111111);
+		return std::make_pair(bytes, 3);
+	} else if (scalar >= 0x10000 && scalar <= 0x10FFFF) {
+		bytes[0] = 0b11110000 | (static_cast<u8>(scalar >> 18) & 0b00000111);
+		bytes[1] = 0b10000000 | (static_cast<u8>(scalar >> 12) & 0b00111111);
+		bytes[2] = 0b10000000 | (static_cast<u8>(scalar >> 6) & 0b00111111);
+		bytes[3] = 0b10000000 | (static_cast<u8>(scalar) & 0b00111111);
+		return std::make_pair(bytes, 4);
+	} else {
+		return None;
+	}
+}
+
+useit inline Maybe<u32> utf8_to_unicode_scalar(std::array<u8, 4> bytes) {
+	u32 value = 0;
+	if ((bytes[0] & 0x80) == 0) {
+		value = bytes[0];
+	} else if ((bytes[0] & 0xE0) == 0xC0) {
+		value = ((u32(bytes[0] & 0x1F)) << 6) | u32(bytes[1] & 0x3F);
+	} else if ((bytes[0] & 0xF0) == 0xE0) {
+		value = (u32(bytes[0] & 0x0F) << 12) | (u32(bytes[1] & 0x3F) << 6) | u32(bytes[2] & 0x3F);
+	} else if ((bytes[0] & 0xF8) == 0xF0) {
+		value = (u32(bytes[0] & 0x07) << 18) | (u32(bytes[1] & 0x3F) << 12) | (u32(bytes[2] & 0x3F) << 6) |
+		        u32(bytes[3] & 0x3F);
+	} else {
+		return None;
+	}
+	return value;
+}
 } // namespace qat::utils
 
 #endif

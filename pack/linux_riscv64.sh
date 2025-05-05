84db40edff8e5bfece4ec1331f35a4e0fd1f41eb
Aldrin Mathew <admin@aldrinmathew.com>
2025-05-05 23:54:28 +0530

    Fixed build config & updated pack script
    - The pack script for the linux-riscv64 platform has been fixed to
      include the `boost` library path
    
    - Added static LLVM libraries that were need in riscv64
      builds. `LLVMHipStdPar` and `LLVMFrontendOffloading`

--- pack/linux_riscv64.sh
+++ pack/linux_riscv64.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 clear
 rm -rf build/*
-cmake -DCMAKE_TOOLCHAIN_FILE="/home/aldrinmathew/toolchain/linux-riscv64.cmake" \
+cmake -DCMAKE_TOOLCHAIN_FILE="~/toolchains/linux-riscv64.cmake" \
 	-DCMAKE_BUILD_TYPE=Release -DLIBS_DIR="/mnt/main/libs/linux-riscv64" \
 	-DLLVM_DIR="/mnt/main/libs/linux-riscv64/llvm" \
+	-DBOOST_DIR="/mnt/main/libs/linux-riscv64/boost" \
 	-DCMAKE_CXX_FLAGS="-fuse-ld=lld" \
 	-DBUILD_SHARED_LIBS=false -GNinja -S src/ -B build/
 cmake --build build --config Release --parallel 24 --target package

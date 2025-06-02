0f084475b5ea7a4867bb308bdbbd671d622cc0f0
Aldrin Mathew <admin@aldrinmathew.com>
2025-06-02 23:16:38 +0530

    Added `ast::PtrOwner`
    - Previously only the IR had a representation for the owner of a
      pointer, now the AST has it as well, since it is now also used in
      polymorphs

--- src/ast/types/mark_owner.cc
+++ src/ast/types/mark_owner.cc
@@ -1,7 +1,34 @@
 #include "./mark_owner.hpp"
 #include "../emit_ctx.hpp"
+#include "./qat_type.hpp"
 
 namespace qat::ast {
+
+String PtrOwner::to_string() const {
+	switch (kind) {
+		case PtrOwnType::type:
+			return "type(" + candidate->to_string() + ")";
+		case PtrOwnType::typeParent:
+			return "''";
+		case PtrOwnType::function:
+			return "own";
+		case PtrOwnType::anonymous:
+			return "";
+		case PtrOwnType::heap:
+			return "heap";
+		case PtrOwnType::region:
+			return "region(" + candidate->to_string() + ")";
+		case PtrOwnType::anyRegion:
+			return "region";
+	}
+}
+
+Json PtrOwner::to_json() const {
+	return Json()
+	    ._("kind", ptr_owner_to_string(kind))
+	    ._("hasAssociatedType", candidate != nullptr)
+	    ._("associatedType", candidate ? candidate->to_json() : JsonValue());
+}
 
 String ptr_owner_to_string(PtrOwnType ownType) {
 	switch (ownType) {
@@ -22,10 +49,47 @@ String ptr_owner_to_string(PtrOwnType ownType) {
 	}
 }
 
-ir::PtrOwner get_ptr_owner(EmitCtx* ctx, PtrOwnType ownType, Maybe<ir::Type*> ownerVal, FileRange fileRange) {
-	switch (ownType) {
+ir::PtrOwner get_ptr_owner(EmitCtx* ctx, PtrOwner owner, FileRange fileRange) {
+
+	if (owner.kind == PtrOwnType::function) {
+		if (not ctx->get_fn()) {
+			ctx->Error("This pointer type is not inside a function and hence cannot have function ownership",
+			           fileRange);
+		}
+	} else if (owner.kind == PtrOwnType::typeParent) {
+		if (not ctx->has_member_parent()) {
+			ctx->Error("No parent type found in scope and hence the pointer "
+			           "cannot be owned by the parent type instance",
+			           fileRange);
+		}
+	}
+	ir::Type* ownerVal = nullptr;
+	if (owner.kind == PtrOwnType::type) {
+		if (not owner.candidate) {
+			ctx->Error("Expected a type to be provided for pointer ownership", fileRange);
+		}
+		auto* typVal = owner.candidate->emit(ctx);
+		if (typVal->is_region()) {
+			ctx->Error("The type provided is a region and hence pointer ownership has to be " +
+			               ctx->color("'region(" + typVal->to_string() = ")") + " or " + ctx->color("'region"),
+			           fileRange);
+		}
+		ownerVal = typVal;
+	} else if (owner.kind == PtrOwnType::region) {
+		if (owner.candidate) {
+			auto* regTy = owner.candidate->emit(ctx);
+			if (not regTy->is_region()) {
+				ctx->Error("The provided type is not a region type and hence pointer "
+				           "owner cannot be " +
+				               ctx->color("region"),
+				           fileRange);
+			}
+			ownerVal = regTy;
+		}
+	}
+	switch (owner.kind) {
 		case PtrOwnType::type:
-			return ir::PtrOwner::of_type(ownerVal.value());
+			return ir::PtrOwner::of_type(ownerVal);
 		case PtrOwnType::typeParent: {
 			if (ctx->has_member_parent()) {
 				return ir::PtrOwner::of_parent_instance(ctx->get_member_parent()->get_parent_type());
@@ -40,7 +104,7 @@ ir::PtrOwner get_ptr_owner(EmitCtx* ctx, PtrOwnType ownType, Maybe<ir::Type*> ow
 		case PtrOwnType::function:
 			return ir::PtrOwner::of_parent_function(ctx->get_fn());
 		case PtrOwnType::region:
-			return ir::PtrOwner::of_region(ownerVal.value()->as_region());
+			return ir::PtrOwner::of_region(ownerVal->as_region());
 		case PtrOwnType::anyRegion:
 			return ir::PtrOwner::of_any_region();
 	}
--- src/ast/types/mark_owner.hpp
+++ src/ast/types/mark_owner.hpp
@@ -12,6 +12,7 @@ class Type;
 namespace qat::ast {
 
 class EmitCtx;
+class Type;
 
 enum class PtrOwnType {
 	heap,
@@ -23,7 +24,45 @@ enum class PtrOwnType {
 	anyRegion,
 };
 
-useit ir::PtrOwner get_ptr_owner(EmitCtx* ctx, PtrOwnType ownType, Maybe<ir::Type*> ownerVal, FileRange fileRange);
+struct PtrOwner {
+	PtrOwnType kind;
+	ast::Type* candidate = nullptr;
+	FileRange  range;
+
+	useit static PtrOwner of_function(FileRange range) {
+		return PtrOwner{.kind = PtrOwnType::function, .candidate = nullptr, .range = std::move(range)};
+	}
+
+	useit static PtrOwner of_heap(FileRange range) {
+		return PtrOwner{.kind = PtrOwnType::heap, .candidate = nullptr, .range = std::move(range)};
+	}
+
+	useit static PtrOwner of_type(ast::Type* candidate, FileRange range) {
+		return PtrOwner{.kind = PtrOwnType::type, .candidate = candidate, .range = std::move(range)};
+	}
+
+	useit static PtrOwner of_any_region(FileRange range) {
+		return PtrOwner{.kind = PtrOwnType::region, .candidate = nullptr, .range = std::move(range)};
+	}
+
+	useit static PtrOwner of_region(ast::Type* region, FileRange range) {
+		return PtrOwner{.kind = PtrOwnType::region, .candidate = region, .range = std::move(range)};
+	}
+
+	useit static PtrOwner of_type_parent(FileRange range) {
+		return PtrOwner{.kind = PtrOwnType::typeParent, .candidate = nullptr, .range = std::move(range)};
+	}
+
+	useit static PtrOwner of_anonymous(FileRange range) {
+		return PtrOwner{.kind = PtrOwnType::anonymous, .candidate = nullptr, .range = std::move(range)};
+	}
+
+	useit Json to_json() const;
+
+	useit String to_string() const;
+};
+
+useit ir::PtrOwner get_ptr_owner(EmitCtx* ctx, PtrOwner owner, FileRange fileRange);
 
 useit String ptr_owner_to_string(PtrOwnType ownType);
 

422432431955b1273f917ab22aa809ef369f022f
Aldrin Mathew <admin@aldrinmathew.com>
2025-06-05 18:38:31 +0530

    Updated platform-specific build scripts
    - Updated build scripts for linux-aarch64 and windows

--- pack/linux_cross_arm64.sh
+++ pack/linux_cross_arm64.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 clear
 rm -rf build/*
-cmake -DCMAKE_TOOLCHAIN_FILE="/home/aldrinmathew/toolchain/linux-aarch64.cmake" \
-   -DCMAKE_BUILD_TYPE=Release -DLIBS_DIR="/mnt/main/libs/linux-aarch64" \
-	-DLLVM_DIR="/mnt/main/libs/linux-aarch64/llvm" \
+cmake -DCMAKE_TOOLCHAIN_FILE="~/toolchains/linux-aarch64.cmake" \
+	-DCMAKE_BUILD_TYPE=Release -DLIBS_DIR="/mnt/Core/libs/linux-aarch64" \
+	-DLLVM_DIR="/mnt/Core/libs/linux-aarch64/llvm" \
+	-DBOOST_DIR="/mnt/Core/libs/linux-aarch64/boost" \
+	-DCMAKE_CXX_FLAGS="-fuse-ld=lld" \
 	-DBUILD_SHARED_LIBS=false -GNinja -S src/ -B build/
 cmake --build build --config Release --parallel 24 --target package

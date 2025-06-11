2e5e045a1773bbcfb52192e65517c05c3d7ca06a
Aldrin Mathew <admin@aldrinmathew.com>
2025-06-11 11:53:26 +0530

    Updated windows build script
    - The windows build script has been modified to work with the latest
      build configuration

--- pack/windows_x64.bat
+++ pack/windows_x64.bat
@@ -1,5 +1,7 @@
 cls
-cmake --build build/ --target clean
+rmdir /s /q build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="C:/Users/aldri/Development/qat" ^
-	-DLLVM_DIR="H:/LIBS/llvm-win-release/" -G"Visual Studio 17 2022" -S ./src/ -B ./build/
+	-DLLVM_DIR="D:/libs/windows-x64/llvm" -DBOOST_DIR="D:/libs/windows-x64/boost" ^
+	-DLIBS_DIR="D:/libs/windows-x64" -G"Visual Studio 17 2022" -DTARGET_ARCHITECTURE="x64" ^
+	-S ./src/ -B ./build/
 cmake --build build --config Release --parallel 24 --target package

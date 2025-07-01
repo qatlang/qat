92d5f218b967c10740c4c925b28a345c27b4909f
Aldrin Mathew <admin@aldrinmathew.com>
2025-07-01 12:46:49 +0530

    Added `ast::InlineMatch`
    - Added AST representation for inline matching expressions which
      provides better ergonomics for matching in cases where there doesn't
      have to be any block-level syntax
    
    - Added implementations to match `bool` and `choice` type values
    
    - This feature will be supported for `mix` types and other types as
      well in the future

---
+++ src/ast/expressions/inline_match.cc
@@ -0,0 +1,129 @@
+#include "./inline_match.hpp"
+#include "../../IR/control_flow.hpp"
+#include "../../IR/logic.hpp"
+
+namespace qat::ast {
+
+void InlineMatch::update_dependencies(ir::EmitPhase phase, Maybe<ir::DependType>, ir::EntityState* ent,
+                                      EmitCtx* ctx) {
+	for (auto val : values) {
+		UPDATE_DEPS(val);
+	}
+}
+
+ir::Value* InlineMatch::emit(EmitCtx* ctx) {
+	auto       expr  = expression->emit(ctx);
+	const bool isRef = expr->get_ir_type()->is_ref();
+	auto valTy = expr->get_ir_type()->is_ref() ? expr->get_ir_type()->as_ref()->get_subtype() : expr->get_ir_type();
+	Vec<Pair<llvm::Value*, llvm::BasicBlock*>> branchVals;
+	ir::Type*                                  resTy     = is_type_inferred() ? inferredType : nullptr;
+	auto                                       currBlock = ctx->get_fn()->get_block();
+	auto                                       resBlock  = ir::Block::create(ctx->get_fn(), currBlock->get_parent());
+	resBlock->link_previous_block(currBlock);
+	if (valTy->is_bool()) {
+		if (values.size() != 2) {
+			ctx->Error("Inline matching a " + String(isRef ? "reference " : "value ") + "of type " +
+			               ctx->color(valTy->to_string()) +
+			               " requires exactly 2 expressions to match to the two possible values " + ctx->color("true") +
+			               " and " + ctx->color("false") + ", but found " + ctx->color(std::to_string(values.size())) +
+			               " expressions instead",
+			           fileRange);
+			ctx->irCtx->builder.CreatePHI(llvm::Type::getVoidTy(ctx->irCtx->llctx), 3);
+		}
+		expr->load_ghost_ref(ctx->irCtx->builder);
+		auto cand = expr->get_llvm();
+		if (isRef) {
+			cand = ctx->irCtx->builder.CreateLoad(valTy->get_llvm_type(), cand);
+		}
+		auto trueBlock  = ir::Block::create(ctx->get_fn(), currBlock);
+		auto falseBlock = ir::Block::create(ctx->get_fn(), currBlock);
+		ctx->irCtx->builder.CreateCondBr(cand, trueBlock->get_bb(), falseBlock->get_bb());
+		trueBlock->set_active(ctx->irCtx->builder);
+		auto tVal = values[0]->emit(ctx);
+		if (resTy == nullptr) {
+			resTy = tVal->get_pass_type();
+		}
+		if (not tVal->get_pass_type()->is_same(resTy)) {
+			ctx->Error("The type inferred from scope for the result of this inline match is " +
+			               ctx->color(resTy->to_string()) + " but this expression is of type " +
+			               ctx->color(tVal->get_pass_type()->to_string()),
+			           values[0]->fileRange);
+		}
+		tVal = ir::Logic::handle_pass_semantics(ctx, tVal->get_pass_type(), tVal, values[0]->fileRange);
+		branchVals.push_back(std::make_pair(tVal->get_llvm(), trueBlock->get_bb()));
+		(void)ir::add_branch(ctx->irCtx->builder, resBlock->get_bb());
+		falseBlock->set_active(ctx->irCtx->builder);
+		if (values[1]->has_type_inferrance()) {
+			values[1]->as_type_inferrable()->set_inference_type(resTy);
+		}
+		auto fVal = values[1]->emit(ctx);
+		if (not fVal->get_pass_type()->is_same(resTy)) {
+			ctx->Error("The first expression is of type " + ctx->color(resTy->to_string()) +
+			               " but the second expression is of type " + ctx->color(fVal->get_pass_type()->to_string()),
+			           values[1]->fileRange);
+		}
+		fVal = ir::Logic::handle_pass_semantics(ctx, fVal->get_pass_type(), fVal, values[1]->fileRange);
+		branchVals.push_back(std::make_pair(fVal->get_llvm(), falseBlock->get_bb()));
+		(void)ir::add_branch(ctx->irCtx->builder, resBlock->get_bb());
+	} else if (valTy->is_choice()) {
+		auto chTy = valTy->as_choice();
+		if (values.size() != chTy->get_variant_count()) {
+			ctx->Error("The expression being matched is of type " + ctx->color(chTy->to_string()) + " which has " +
+			               ctx->color(std::to_string(chTy->get_variant_count())) + ", but only " +
+			               ctx->color(std::to_string(values.size())) + " values are provided here",
+			           fileRange);
+		}
+		expr->load_ghost_ref(ctx->irCtx->builder);
+		auto cand = expr->get_llvm();
+		if (isRef) {
+			cand = ctx->irCtx->builder.CreateLoad(chTy->get_llvm_type(), cand);
+		}
+		for (usize i = 0; i < values.size(); i++) {
+			auto* trueBlock  = ir::Block::create(ctx->fn, ctx->fn->get_block());
+			auto* falseBlock = ir::Block::create(ctx->fn, ctx->fn->get_block());
+			ctx->irCtx->builder.CreateCondBr(ctx->irCtx->builder.CreateICmpEQ(cand, chTy->get_value_at(i)),
+			                                 trueBlock->get_bb(), falseBlock->get_bb());
+			trueBlock->set_active(ctx->irCtx->builder);
+			auto itVal = values[i]->emit(ctx);
+			if (resTy == nullptr) {
+				resTy = itVal->get_pass_type();
+			}
+			if (not itVal->get_pass_type()->is_same(resTy)) {
+				ctx->Error((i == 0
+				                ? "The type inferred from scope for the result of this inline match is "
+				                : "Expressions provided for the previous variants in this inline match is of type ") +
+				               ctx->color(resTy->to_string()) + ", but this expression is of type " +
+				               ctx->color(itVal->get_pass_type()->to_string()),
+				           values[i]->fileRange);
+			}
+			itVal = ir::Logic::handle_pass_semantics(ctx, itVal->get_pass_type(), itVal, values[i]->fileRange);
+			branchVals.push_back(std::make_pair(itVal->get_llvm(), trueBlock->get_bb()));
+			(void)ir::add_branch(ctx->irCtx->builder, resBlock->get_bb());
+			falseBlock->set_active(ctx->irCtx->builder);
+		}
+		// The following will be inserted into the false block of the last variant in the choice type, which should
+		// never be reached
+		ctx->irCtx->builder.CreateUnreachable();
+	}
+	//
+	resBlock->set_active(ctx->irCtx->builder);
+	auto phi = ctx->irCtx->builder.CreatePHI(resTy->get_llvm_type(), branchVals.size());
+	for (auto& it : branchVals) {
+		phi->addIncoming(it.first, it.second);
+	}
+	return ir::Value::get(phi, resTy, not resTy->is_ref());
+}
+
+Json InlineMatch::to_json() const {
+	Vec<JsonValue> valuesJSON;
+	for (auto val : values) {
+		valuesJSON.push_back(val->to_json());
+	}
+	return Json()
+	    ._("nodeType", "inlineMatch")
+	    ._("expression", expression->to_json())
+	    ._("values", valuesJSON)
+	    ._("fileRange", fileRange);
+}
+
+} // namespace qat::ast
---
+++ src/ast/expressions/inline_match.hpp
@@ -0,0 +1,29 @@
+#ifndef QAT_AST_EXPRESSIONS_INLINE_MATCH_HPP
+#define QAT_AST_EXPRESSIONS_INLINE_MATCH_HPP
+
+#include "../expression.hpp"
+
+namespace qat::ast {
+
+class InlineMatch final : public Expression, public TypeInferrable {
+	Expression*      expression;
+	Vec<Expression*> values;
+
+  public:
+	InlineMatch(Expression* _expression, Vec<Expression*> _values, FileRange _fileRange)
+	    : Expression(std::move(_fileRange)), expression(_expression), values(std::move(_values)) {}
+
+	TYPE_INFERRABLE_FUNCTIONS
+
+	void update_dependencies(ir::EmitPhase phase, Maybe<ir::DependType> dep, ir::EntityState* ent, EmitCtx* ctx) final;
+
+	useit ir::Value* emit(EmitCtx* emitCtx) final;
+
+	useit NodeType nodeType() const final { return NodeType::INLINE_MATCH; }
+
+	useit Json to_json() const final;
+};
+
+} // namespace qat::ast
+
+#endif

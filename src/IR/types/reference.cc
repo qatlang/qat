b01d3ad54dc79f64bb48434e42ec2044510709c9
Aldrin Mathew <admin@aldrinmathew.com>
2025-09-25 11:48:54 +0530

    `ir::RefType` now supports address-spaces
    - References in qat now support address-spaces. This makes it easier
      to work with references in low-level environments like operating
      systems and embedded.

--- src/IR/types/reference.cc
+++ src/IR/types/reference.cc
@@ -8,25 +8,24 @@ namespace qat::ir {
 
 Vec<RefType*> RefType::allRefTypes = {};
 
-RefType::RefType(bool _isSubtypeVariable, Type* _type, ir::Ctx* irCtx)
-    : subType(_type), isSubVariable(_isSubtypeVariable) {
-	if (subType->is_type_sized()) {
-		llvmType = llvm::PointerType::get(subType->get_llvm_type(), irCtx->dataLayout.getProgramAddressSpace());
-	} else {
-		llvmType =
-		    llvm::PointerType::get(llvm::Type::getInt8Ty(irCtx->llctx), irCtx->dataLayout.getProgramAddressSpace());
-	}
-	linkingName = "qat'ref:[" + String(isSubVariable ? "var " : "") + subType->get_name_for_linking() + "]";
+RefType::RefType(bool _isSubtypeVariable, Type* _type, Maybe<AddressSpace> _addressSpace, ir::Ctx* irCtx)
+    : subType(_type), isSubVariable(_isSubtypeVariable), addressSpace(std::move(_addressSpace)) {
+	llvmType =
+	    llvm::PointerType::get(irCtx->llctx, addressSpace.has_value() ? addressSpace.value().get_number(irCtx)
+	                                                                  : irCtx->dataLayout.getProgramAddressSpace());
+	linkingName = "qat'ref:[" + String(isSubVariable ? "var " : "") + subType->get_name_for_linking() +
+	              (addressSpace.has_value() ? ("," + addressSpace.value().to_string()) : "") + "]";
 	allRefTypes.push_back(this);
 }
 
-RefType* RefType::get(bool isSubtypeVariable, Type* subType, ir::Ctx* irCtx) {
+RefType* RefType::get(bool isSubtypeVariable, Type* subType, Maybe<AddressSpace> addressSpace, ir::Ctx* irCtx) {
 	for (auto* typ : allRefTypes) {
-		if (typ->get_subtype()->is_same(subType) && (typ->has_variability() == isSubtypeVariable)) {
+		if (typ->get_subtype()->is_same(subType) && (typ->has_variability() == isSubtypeVariable) &&
+		    ir::AddressSpace::compare(typ->get_address_space(), addressSpace)) {
 			return typ;
 		}
 	}
-	return std::construct_at(OwnNormal(RefType), isSubtypeVariable, subType, irCtx);
+	return std::construct_at(OwnNormal(RefType), isSubtypeVariable, subType, std::move(addressSpace), irCtx);
 }
 
 Type* RefType::get_subtype() const { return subType; }
@@ -37,6 +36,9 @@ bool RefType::is_type_sized() const { return true; }
 
 TypeKind RefType::type_kind() const { return TypeKind::REFERENCE; }
 
-String RefType::to_string() const { return "ref:[" + String(isSubVariable ? "var " : "") + subType->to_string() + "]"; }
+String RefType::to_string() const {
+	return "ref:[" + String(isSubVariable ? "var " : "") + subType->to_string() +
+	       (addressSpace.has_value() ? (", " + addressSpace.value().to_string()) : "") + "]";
+}
 
 } // namespace qat::ir
--- src/IR/types/reference.hpp
+++ src/IR/types/reference.hpp
@@ -1,22 +1,26 @@
 #ifndef QAT_IR_TYPES_REFERENCE_HPP
 #define QAT_IR_TYPES_REFERENCE_HPP
 
+#include "./address_space.hpp"
 #include "./qat_type.hpp"
 
 namespace qat::ir {
 
 class RefType : public Type {
-	Type* subType;
-	bool  isSubVariable;
+	Type*               subType;
+	bool                isSubVariable;
+	Maybe<AddressSpace> addressSpace;
 
 	static Vec<RefType*> allRefTypes;
 
   public:
-	RefType(bool isSubtypeVariable, Type* _type, ir::Ctx* irCtx);
+	RefType(bool isSubtypeVariable, Type* _type, Maybe<AddressSpace> _addressSpace, ir::Ctx* irCtx);
 
-	useit static RefType* get(bool _isSubtypeVariable, Type* _subtype, ir::Ctx* irCtx);
+	useit static RefType* get(bool isSubtypeVariable, Type* subtype, Maybe<AddressSpace> addressSpace, ir::Ctx* irCtx);
 
 	useit Type* get_subtype() const;
+
+	useit Maybe<AddressSpace> const& get_address_space() const { return addressSpace; }
 
 	useit bool has_variability() const;
 

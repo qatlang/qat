3a85ca5efdbc6a48fe38352d85428c4e0e954239
Aldrin Mathew <admin@aldrinmathew.com>
2025-09-28 00:58:15 +0530

    Address-space support added in `ast::SliceType`
    - Following the updates to `ir::SliceType`, address-space has been
      added to the AST representation as well

--- src/ast/types/slice.cc
+++ src/ast/types/slice.cc
@@ -3,12 +3,27 @@
 
 namespace qat::ast {
 
-ir::Type* SliceType::emit(EmitCtx* ctx) { return ir::SliceType::get(isVar, subType->emit(ctx), ctx->irCtx); }
-
-Json SliceType::to_json() const {
-	return Json()._("nodeType", "slice")._("isVar", isVar)._("subType", subType->to_string())._("fileRange", fileRange);
+ir::Type* SliceType::emit(EmitCtx* ctx) {
+	Maybe<ir::AddressSpace> addr;
+	if (addressSpace.has_value()) {
+		addr = addressSpace.value().to_ir(ctx);
+	}
+	return ir::SliceType::get(isVar, subType->emit(ctx), std::move(addr), ctx->irCtx);
 }
 
-String SliceType::to_string() const { return "slice:[" + String(isVar ? "var " : "") + subType->to_string() + "]"; }
+Json SliceType::to_json() const {
+	return Json()
+	    ._("nodeType", "slice")
+	    ._("isVar", isVar)
+	    ._("subType", subType->to_string())
+	    ._("hasAddressSpace", addressSpace.has_value())
+	    ._("addressSpace", addressSpace.has_value() ? addressSpace.value().to_json() : JsonValue())
+	    ._("fileRange", fileRange);
+}
+
+String SliceType::to_string() const {
+	return "slice:[" + String(isVar ? "var " : "") + subType->to_string() +
+	       (addressSpace.has_value() ? (", " + addressSpace.value().to_string()) : "") + "]";
+}
 
 } // namespace qat::ast
--- src/ast/types/slice.hpp
+++ src/ast/types/slice.hpp
@@ -1,6 +1,8 @@
 #ifndef QAT_AST_TYPES_SLICE_HPP
 #define QAT_AST_TYPES_SLICE_HPP
 
+#include "../expression.hpp"
+#include "./address_space.hpp"
 #include "./qat_type.hpp"
 
 #include <clang/Basic/AddressSpaces.h>
@@ -8,24 +10,33 @@
 namespace qat::ast {
 
 class SliceType final : public Type {
-	bool  isVar;
-	Type* subType;
+	bool                isVar;
+	Type*               subType;
+	Maybe<AddressSpace> addressSpace;
 
   public:
-	SliceType(bool _isVar, Type* _subType, FileRangePtr _fileRange)
-	    : Type(_fileRange), isVar(_isVar), subType(_subType) {}
+	SliceType(bool _isVar, Type* _subType, Maybe<AddressSpace> _addressSpace, FileRangePtr _fileRange)
+	    : Type(_fileRange), isVar(_isVar), subType(_subType), addressSpace(std::move(_addressSpace)) {}
 
-	useit static SliceType* create(bool isVar, Type* subType, FileRangePtr fileRange) {
-		return std::construct_at(OwnNormal(SliceType), isVar, subType, fileRange);
+	useit static SliceType* create(bool isVar, Type* subType, Maybe<AddressSpace> addressSpace,
+	                               FileRangePtr fileRange) {
+		return std::construct_at(OwnNormal(SliceType), isVar, subType, std::move(addressSpace), fileRange);
 	}
 
 	void update_dependencies(ir::EmitPhase phase, Maybe<ir::DependType> expect, ir::EntityState* ent, EmitCtx* ctx) {
 		subType->update_dependencies(phase, expect, ent, ctx);
+		if (addressSpace.has_value() && addressSpace.value().value) {
+			UPDATE_DEPS(addressSpace.value().value);
+		}
 	}
 
 	useit Maybe<usize> get_type_bitsize(EmitCtx* ctx) const {
-		return ctx->irCtx->clangTargetInfo->getPointerWidth(
-		    clang::getLangASFromTargetAS(ctx->irCtx->dataLayout.getProgramAddressSpace()));
+		if (addressSpace.has_value() && addressSpace.value().value) {
+			return None;
+		}
+		return ctx->irCtx->clangTargetInfo->getPointerWidth(clang::getLangASFromTargetAS(
+		    addressSpace.has_value() ? addressSpace.value().to_ir(ctx).get_number(ctx->irCtx)
+		                             : ctx->irCtx->dataLayout.getProgramAddressSpace()));
 	}
 
 	useit ir::Type* emit(EmitCtx* ctx) final;

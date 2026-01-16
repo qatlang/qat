148f378a221a8e033f35b01622914855f5e44a27
Aldrin Mathew <admin@aldrinmathew.com>
2026-01-16 16:58:52 +0530

    Migration of run_command_* functions to process v2
    - run_command_* functions now make use of the boost::process:v2
      version instead of the old version. This makes it possible to
      integrate the latest boost versions into the project instead of
      being stuck on 1.86.0

--- src/utils/run_command.cc
+++ src/utils/run_command.cc
@@ -1,74 +1,229 @@
 #include "./run_command.hpp"
+#include "../cli/logger.hpp"
 
-#include <boost/process/child.hpp>
-#include <boost/process/environment.hpp>
-#include <boost/process/io.hpp>
-
-#include <iostream>
+#include <boost/asio/readable_pipe.hpp>
+#include <boost/asio/writable_pipe.hpp>
+#include <boost/process/stdio.hpp>
+#include <boost/process/v2/process.hpp>
+#include <helpers/array.hpp>
 
 namespace qat {
 
 int run_command_get_code(String command, Vec<String> const& args) {
-	boost::process::ipstream outStream;
-	boost::process::child    childProcess(command, args, boost::process::std_out > boost::process::null);
-	childProcess.wait();
-	return childProcess.exit_code();
+	boost::asio::io_context    ctx;
+	boost::process::process    proc(ctx, command, args,
+	                                boost::process::process_stdio{
+	                                    .in  = nullptr,
+	                                    .out = nullptr,
+	                                    .err = nullptr,
+                                 });
+	boost::process::error_code ec;
+	auto                       exitCode = proc.wait(ec);
+	if (ec.failed()) {
+		String message(command);
+		for (auto& arg : args) {
+			message += " " + arg;
+		}
+		auto& log = Logger::get();
+		log->fatalError("Failed waiting for the following command to finish. Could not retrieve the exit code.\n" +
+		                    log->color(message),
+		                None);
+	}
+	return exitCode;
 }
 
 Pair<int, String> run_command_get_stdout(String command, Vec<String> const& args) {
-	boost::process::ipstream outStream;
-	boost::process::child    childProcess(command, args, boost::process::std_out > outStream);
-	childProcess.wait();
-	String output;
-	for (String line; std::getline(outStream, line);) {
-		output += line + "\n";
+	boost::asio::io_context    ctx;
+	boost::asio::readable_pipe pOut(ctx);
+	boost::process::process    proc(ctx, command, args,
+	                                boost::process::process_stdio{
+	                                    .in  = nullptr,
+	                                    .out = pOut,
+	                                    .err = nullptr,
+                                 });
+	String                     result;
+	Array<char, 2048>          buffer{};
+	boost::system::error_code  sysErr;
+	while (true) {
+		auto size = pOut.read_some(boost::asio::buffer(buffer), sysErr);
+		if (sysErr == boost::asio::error::eof) {
+			break;
+		}
+		if (sysErr) {
+			String message(command);
+			for (auto& arg : args) {
+				command += " " + arg;
+			}
+			auto& log = Logger::get();
+			log->fatalError("Failed to read the standard output of the following command:\n" + log->color(message),
+			                None);
+		}
+		result.append(buffer.data(), size);
 	}
-	return Pair<int, String>(childProcess.exit_code(), output);
+	boost::process::error_code err;
+	auto                       exitCode = proc.wait(err);
+	if (err.failed()) {
+		String message(command);
+		for (auto& arg : args) {
+			command += " " + arg;
+		}
+		auto& log = Logger::get();
+		log->fatalError("Failed to wait for the following command to finish execution:\n" + log->color(message), None);
+	}
+	return {exitCode, result};
 }
 
 int run_command_with_output(String command, Vec<String> const& args) {
-	boost::process::child childProcess(command, args);
-	childProcess.wait();
-	return childProcess.exit_code();
+	boost::asio::io_context    ctx;
+	boost::process::process    proc(ctx, command, args, boost::process::process_stdio{});
+	boost::process::error_code err;
+	auto                       exitCode = proc.wait(err);
+	if (err.failed()) {
+		String message(command);
+		for (auto& arg : args) {
+			command += " " + arg;
+		}
+		auto& log = Logger::get();
+		log->fatalError("Failed to wait for the following command to finish execution:\n" + log->color(message), None);
+	}
+	return exitCode;
 }
 
 Pair<int, String> run_command_get_output(String command, Vec<String> const& args) {
-	boost::process::ipstream outStream;
-	boost::process::child childProcess(command, args, (boost::process::std_err & boost::process::std_out) > outStream);
-	childProcess.wait();
-	String output;
-	for (String line; std::getline(outStream, line);) {
-		output += line + "\n";
+	boost::asio::io_context    ctx;
+	boost::asio::readable_pipe readPipe(ctx);
+	boost::asio::writable_pipe writePipe(ctx);
+	boost::asio::connect_pipe(readPipe, writePipe);
+	boost::process::process proc(ctx, command, args,
+	                             boost::process::process_stdio{
+	                                 .in  = nullptr,
+	                                 .out = writePipe,
+	                                 .err = writePipe,
+	                             });
+	writePipe.close();
+	String                    result;
+	Array<char, 2048>         buffer;
+	boost::system::error_code sysErr;
+	while (true) {
+		auto size = readPipe.read_some(boost::asio::buffer(buffer), sysErr);
+		if (sysErr == boost::asio::error::eof) {
+			break;
+		}
+		if (sysErr) {
+			String message(command);
+			for (auto& arg : args) {
+				command += " " + arg;
+			}
+			auto& log = Logger::get();
+			log->fatalError("Failed to read the output of the following command:\n" + log->color(message), None);
+		}
+		result.append(buffer.data(), size);
 	}
-	return Pair<int, String>(childProcess.exit_code(), output);
+	boost::process::error_code err;
+	auto                       exitCode = proc.wait(err);
+	if (err.failed()) {
+		String message(command);
+		for (auto& arg : args) {
+			command += " " + arg;
+		}
+		auto& log = Logger::get();
+		log->fatalError("Failed to wait for the following command to finish execution:\n" + log->color(message), None);
+	}
+
+	return Pair<int, String>(exitCode, result);
 }
 
 Pair<int, String> run_command_get_stderr(String command, Vec<String> const& args) {
-	boost::process::ipstream errorStream;
-	boost::process::child    childProcess(command, args, boost::process::std_err > errorStream);
-	childProcess.wait();
-	String errorOutput;
-	for (String line; not std::getline(errorStream, line).eof();) {
-		errorOutput += line + "\n";
+	boost::asio::io_context    ctx;
+	boost::asio::readable_pipe pErr(ctx);
+	boost::process::process    proc(ctx, command, args,
+	                                boost::process::process_stdio{
+	                                    .in  = nullptr,
+	                                    .out = nullptr,
+	                                    .err = pErr,
+                                 });
+	String                     result;
+	Array<char, 2048>          buffer{};
+	boost::system::error_code  sysErr;
+	while (true) {
+		const auto size = pErr.read_some(boost::asio::buffer(buffer), sysErr);
+		if (sysErr == boost::asio::error::eof) {
+			break;
+		}
+		if (sysErr) {
+			String message(command);
+			for (auto& arg : args) {
+				command += " " + arg;
+			}
+			auto& log = Logger::get();
+			log->fatalError(
+			    "Failed to read the standard error output of the following command:\n" + log->color(message), None);
+		}
+		result.append(buffer.data(), size);
 	}
-	return Pair<int, String>(childProcess.exit_code(), errorOutput);
+	boost::process::error_code err;
+	auto                       exitCode = proc.wait(err);
+	if (err.failed()) {
+		String message(command);
+		for (auto& arg : args) {
+			command += " " + arg;
+		}
+		auto& log = Logger::get();
+		log->fatalError("Failed to wait for the following command to finish execution:\n" + log->color(message), None);
+	}
+	return {exitCode, result};
 }
 
-std::tuple<int, String, String> run_command_get_stdout_and_stderr(String command, Vec<String> const& args) {
-	boost::process::ipstream errorStream;
-	boost::process::ipstream outStream;
-	boost::process::child    childProcess(command, args, boost::process::std_out > outStream,
-	                                      boost::process::std_err > errorStream);
-	childProcess.wait();
-	String output;
-	String errorOutput;
-	for (String line; std::getline(outStream, line);) {
-		output += line + "\n";
+Many<int, String, String> run_command_get_stdout_and_stderr(String command, Vec<String> const& args) {
+	boost::asio::io_context    ctx;
+	boost::asio::readable_pipe out_read(ctx);
+	boost::asio::writable_pipe out_write(ctx);
+	boost::asio::connect_pipe(out_read, out_write);
+	boost::asio::readable_pipe err_read(ctx);
+	boost::asio::writable_pipe err_write(ctx);
+	boost::asio::connect_pipe(err_read, err_write);
+	auto proc = boost::process::process(ctx, command, args,
+	                                    boost::process::process_stdio{
+	                                        .in  = nullptr,
+	                                        .out = out_write,
+	                                        .err = err_write,
+	                                    });
+	out_write.close();
+	err_write.close();
+	auto read_from_pipe = [&](boost::asio::readable_pipe& pipe) -> String {
+		String                    res;
+		Array<char, 2048>         buffer;
+		boost::system::error_code ec;
+		while (true) {
+			const auto size = pipe.read_some(boost::asio::buffer(buffer), ec);
+			if (ec == boost::asio::error::eof) {
+				break;
+			} else if (ec.failed()) {
+				String message(command);
+				for (auto& arg : args) {
+					message += " " + arg;
+				}
+				auto& log = Logger::get();
+				log->fatalError("Failed to read output for the following command:\n" + log->color(message), None);
+			}
+			res.append(buffer.data(), size);
+		}
+		return res;
+	};
+
+	String                     stdOut = read_from_pipe(out_read);
+	String                     stdErr = read_from_pipe(err_read);
+	boost::process::error_code err;
+	const auto                 exitCode = proc.wait(err);
+	if (err.failed()) {
+		String message(command);
+		for (auto& arg : args) {
+			command += " " + arg;
+		}
+		auto& log = Logger::get();
+		log->fatalError("Failed to wait for the following command to finish execution:\n" + log->color(message), None);
 	}
-	for (String line; std::getline(errorStream, line);) {
-		errorOutput += line + "\n";
-	}
-	return std::tuple<int, String, String>(childProcess.exit_code(), output, errorOutput);
+	return std::make_tuple(exitCode, stdOut, stdErr);
 }
 
 } // namespace qat
--- src/utils/run_command.hpp
+++ src/utils/run_command.hpp
@@ -1,6 +1,7 @@
 #ifndef QAT_UTILS_LAUNCH_PROCESS_HPP
 #define QAT_UTILS_LAUNCH_PROCESS_HPP
 
+#include <helpers/many.hpp>
 #include <helpers/pair.hpp>
 #include <helpers/string.hpp>
 #include <helpers/vec.hpp>
@@ -17,7 +18,7 @@ int run_command_with_output(String command, Vec<String> const& args);
 
 Pair<int, String> run_command_get_stderr(String command, Vec<String> const& args);
 
-std::tuple<int, String, String> run_command_get_stdout_and_stderr(String command, Vec<String> const& args);
+Many<int, String, String> run_command_get_stdout_and_stderr(String command, Vec<String> const& args);
 
 } // namespace qat
 
